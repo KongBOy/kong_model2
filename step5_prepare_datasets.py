4ced0fbc55196c26408bbc7ee87ec2ac3dcca8a8
KongBoy <s89334roy@yahoo.com.tw>
2020-02-26 10:16:29 +0800

    改一些step5 讓他可以處理 padding dataset

--- step5_prepare_datasets.py
+++ step5_prepare_datasets.py
@@ -3,7 +3,7 @@ import shutil
 from build_dataset_combine import Check_dir_exist_and_build_new_dir
 from util import get_dir_certain_file_name
 
-dir_name = "stack_unet-easy300"
+dir_name = "stack_unet-padding2000"
 
 train_dir     = "datasets"+"/"+dir_name+"/"+"train"
 train_dis_dir = "datasets"+"/"+dir_name+"/"+"train/distorted_img"
@@ -20,9 +20,10 @@ Check_dir_exist_and_build_new_dir( test_dis_dir )
 Check_dir_exist_and_build_new_dir( test_rec_dir )
 
 # os.listdir("step3_apply_flow_result")
-src_dir = "step3_apply_flow_result"
-img_list  = get_dir_certain_file_name(  src_dir, certain_word="3a1-I1-patch.bmp")
-move_list = get_dir_certain_file_name(  src_dir, certain_word="3b-rec_mov_map.npy" )
+img_src_dir = "step3_apply_flow_result"
+img_list  = get_dir_certain_file_name(  img_src_dir, certain_word="3a1-I1-patch.bmp")
+mov_src_dir = "step2_flow_build/move_map"
+move_list = get_dir_certain_file_name(  mov_src_dir, certain_word=".npy" )
 
 data_amount = len(img_list)
 train_amount = int(data_amount*0.9)
@@ -30,20 +31,20 @@ train_amount = int(data_amount*0.9)
 
 ### train部分
 for i in range(train_amount):
-    src_img_path = src_dir       + "/" + img_list[i]
+    src_img_path = img_src_dir   + "/" + img_list[i]
     dst_img_path = train_dis_dir + "/" + img_list[i]
     shutil.copy(src=src_img_path, dst=dst_img_path)
 
-    src_mov_path = src_dir       + "/" + move_list[i]
+    src_mov_path = mov_src_dir   + "/" + move_list[i]
     dst_mov_path = train_rec_dir + "/" + move_list[i]
     shutil.copy(src=src_mov_path, dst=dst_mov_path)
 
 ### test部分
 for i in range(train_amount,data_amount):
-    src_img_path = src_dir      + "/" + img_list[i]
+    src_img_path = img_src_dir      + "/" + img_list[i]
     dst_img_path = test_dis_dir + "/" + img_list[i]
     shutil.copy(src=src_img_path, dst=dst_img_path)
 
-    src_mov_path = src_dir      + "/" + move_list[i]
+    src_mov_path = mov_src_dir      + "/" + move_list[i]
     dst_mov_path = test_rec_dir + "/" + move_list[i]
     shutil.copy(src=src_mov_path, dst=dst_mov_path)
