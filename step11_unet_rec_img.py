41308d60d90b6cacb3b2cd0d18dd8a7760ebbf29
KongBoy <s89334roy@yahoo.com.tw>
2020-03-08 13:29:19 +0800

    step11,12 加入可以生成pad的資料

--- step11_unet_rec_img.py
+++ step11_unet_rec_img.py
@@ -4,15 +4,16 @@ from util import get_dir_certain_img
 import tensorflow as tf 
 
 
-db_name = "pad2000-512to256" ### 這資料集 要搭配 512to256 的架構喔！
 model_name="model2_UNet_512to256"
+model_result_dir = access_path+"result/20200227-071341_pad2000-512to256_model2_UNet_512to256"
+pad_result = True ### pad 和 沒pad 的差別只有：1.存結果的地方不同、2.unet_rec_img 有沒有pad成 H,W 的大小
 
 #############################################################################################################
+### 讀出model的部分
 generator, generator_optimizer, discriminator, discriminator_optimizer, generate_images, train_step = step2_build_model_and_optimizer(model_name=model_name)
 ckpt  = step3_build_checkpoint (model_name=model_name, generator=generator, generator_optimizer=generator_optimizer, discriminator=discriminator, discriminator_optimizer=discriminator_optimizer)
 
-result_dir = access_path+"result/20200227-071341_pad2000-512to256_model2_UNet_512to256"
-_, ckpt_dir = step4_get_result_dir_default_logs_ckpt_dir_name(result_dir)
+_, ckpt_dir = step4_get_result_dir_default_logs_ckpt_dir_name(model_result_dir)
 manager     = tf.train.CheckpointManager (checkpoint=ckpt, directory=ckpt_dir, max_to_keep=2) ### checkpoint管理器，設定最多存2份
 ckpt.restore(manager.latest_checkpoint)     ### 從restore_ckpt_dir 抓存的model出來
 start_epoch = ckpt.epoch_log.numpy()
@@ -24,13 +25,17 @@ import cv2
 import numpy as np 
 from util import predict_unet_move_maps_back, get_max_move_xy_from_certain_move
 from build_dataset_combine import Check_dir_exist_and_build_new_dir
-from step4_apply_rec2dis_img_b_use_unet_move_map import apply_move_to_rec
+from step4_apply_rec2dis_img_b_use_move_map import apply_move_to_rec2
 from step6_data_pipline import distorted_resize_and_norm
 
-result_dir = access_path+"step11_unet_rec_img"
-Check_dir_exist_and_build_new_dir(result_dir)
+
+step11_result_dir = access_path+"step11_unet_rec_img"
+if(pad_result):
+    step11_result_dir = access_path+"step11_unet_rec_img_pad"
+Check_dir_exist_and_build_new_dir(step11_result_dir)
 
 resize_shape = (512,512)
+
 
 ### 用 dis_img 得到 unet_move_map
 dis_imgs = get_dir_certain_img(access_path+"step3_apply_flow_result","3a1-I1-patch.bmp")  ### 讀取dis_imgs 等等輸入unet
@@ -45,8 +50,22 @@ unet_move_maps = predict_unet_move_maps_back(unet_move_maps)  ### 把 unet_move_
 
 ### 用得到 unet_move_map 來還原 dis_img
 max_move_x, max_move_y = get_max_move_xy_from_certain_move(access_path+"step3_apply_flow_result","2-q") ### 注意這裡要去 step3才對！因為當初建db時是用整個db的最大移動量(step3裡的即整個db的資料)，如果去dataset/train的話只有train的資料喔
-
 import matplotlib.pyplot as plt
 for i, dis_img in enumerate(dis_imgs):
-    g_rec_img = apply_move_to_rec(dis_img, unet_move_maps[i], max_move_x, max_move_y) ### 
-    cv2.imwrite(result_dir+"/%06i_unet_rec_img.bmp"%i, g_rec_img)
+    unet_rec_img = apply_move_to_rec2(dis_img, unet_move_maps[i], max_move_x, max_move_y) ### 用unet_move_map來還原dis_img
+    if(pad_result): 
+        # unet_rec_img = np.pad( unet_rec_img, ( (max_move_y,max_move_x), (max_move_y,max_move_x), (0,0) )) ### 不精確的寫法，結果x就少1個pixel了！
+        ### 以下完全復刻 產生dis_img時的padding 方式囉！他媽的有時間一定要回去改簡單一點
+        row, col = unet_move_maps[i].shape[:2]
+        
+        ### 先建出一個大大的 unet_rec_img_pad 畫布
+        unet_rec_img_pad_h = int( np.around(max_move_y + row + max_move_y) ) ### np.around 是四捨五入，然後因為要丟到shape裡所以轉int
+        unet_rec_img_pad_w = int( np.around(max_move_x + col + max_move_x) ) ### np.around 是四捨五入，然後因為要丟到shape裡所以轉int
+        unet_rec_img_pad  = np.zeros(shape=(unet_rec_img_pad_h,unet_rec_img_pad_w,3), dtype=np.uint8)
+
+        ### 在把 unet_rec_img 畫進去 unet_rec_img_pad
+        int_max_move_x = int(max_move_x)
+        int_max_move_y = int(max_move_y)
+        unet_rec_img_pad[int_max_move_y:int_max_move_y+row, int_max_move_x:int_max_move_x+col, :] = unet_rec_img
+        unet_rec_img = unet_rec_img_pad
+    cv2.imwrite(step11_result_dir+"/%06i_unet_rec_img.bmp"%i, unet_rec_img)
--- step12_ord_pad_gt.py
+++ step12_ord_pad_gt.py
@@ -4,34 +4,38 @@ from build_dataset_combine import Check_dir_exist_and_build_new_dir
 import numpy as np 
 import cv2
 
+pad_result = True ### pad 和 沒pad 的差別只有：1.存結果的地方不同、2.unet_rec_img 有沒有pad成 H,W 的大小
 
-result_dir = access_path+"step12_ord_pad_gt"
-Check_dir_exist_and_build_new_dir( result_dir )
-
-imgs = get_dir_certain_img("step3_apply_flow_result","1-I.bmp")
+#######################################################################
+step12_result_dir = access_path+"step12_ord_pad_gt"
+if(pad_result):
+    step12_result_dir = access_path+"step12_ord_pad_gt_pad"
+Check_dir_exist_and_build_new_dir( step12_result_dir )
 
 
 ### 本來想要padding，但後來發現好像不用，所以其實直接拿step3裡的 1-I.bmp也行喔！
-### 初始化各個會用到的canvas
-row=256
-col=256
-move_x_max, move_y_max = get_max_move_xy_from_certain_move("step3_apply_flow_result","2-q.npy")
+### 但後來老師想用dis_img加入rect2訓練，所以還是需要pad一下，step3有空改寫的時候要多存pad的過程喔~
+imgs = get_dir_certain_img(access_path+"step3_apply_flow_result","1-I.bmp")
 
-dis_h = int( np.around(move_y_max + row + move_y_max) ) ### np.around 是四捨五入，然後因為要丟到shape裡所以轉int
-dis_w = int( np.around(move_x_max + col + move_x_max) ) ### np.around 是四捨五入，然後因為要丟到shape裡所以轉int
-dis_img  = np.zeros(shape=(dis_h,dis_w,3), dtype=np.uint8)
-dis_imgs = np.tile(dis_img,(2000,1,1,1))
 
-move_x_max = int(move_x_max)
-move_y_max = int(move_y_max)
-dis_imgs[:, move_y_max:move_y_max+row, move_x_max:move_x_max+col, :] = imgs
+if(pad_result):
+    ### 初始化各個會用到的canvas
+    move_x_max, move_y_max = get_max_move_xy_from_certain_move(access_path+"step3_apply_flow_result","2-q.npy")
+    row=256
+    col=256
+    img_pad_h = int( np.around(move_y_max + row + move_y_max) ) ### np.around 是四捨五入，然後因為要丟到shape裡所以轉int
+    img_pad_w = int( np.around(move_x_max + col + move_x_max) ) ### np.around 是四捨五入，然後因為要丟到shape裡所以轉int
+    img_pad  = np.zeros(shape=(img_pad_h,img_pad_w,3), dtype=np.uint8)
+    img_pads = np.tile(img_pad,(2000,1,1,1))
 
-for i, dis_img in enumerate(dis_imgs):
-    # cv2.imwrite(result_dir + "/" + "%06i_img-pad.bmp"%i, dis_img)
-    cv2.imwrite(result_dir + "/" + "%06i_img.bmp"%i, imgs[i])
+    int_move_x_max = int(move_x_max)
+    int_move_y_max = int(move_y_max)
+    img_pads[:, int_move_y_max:int_move_y_max+row, int_move_x_max:int_move_x_max+col, :] = imgs
+    imgs = img_pads
+
+for i, img in enumerate(imgs):
+    cv2.imwrite(step12_result_dir + "/" + "%06i_img.bmp"%i, img)
+    
     # cv2.waitKey()
     # cv2.destroyAllWindows()
 
-
-#dst_x = go_col + int(move_map[go_row,go_col,0] + move_x_max) ### 現在的起點是(move_x_max, move_y_max)，所以要位移一下
-#dst_y = go_row + int(move_map[go_row,go_col,1] + move_y_max) ### 現在的起點是(move_x_max, move_y_max)，所以要位移一下
