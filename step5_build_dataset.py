0b3c84b8fba4a48b0d2e4930e5cb973dc44ef203
KongBoy <s89334roy@yahoo.com.tw>
2020-03-19 21:57:23 +0800

    step5 生成資料集的名字 改成 1.pure_unet 2.pure_rect2 3.unet_rect2 之類的

--- step5_build_dataset.py
+++ step5_build_dataset.py
@@ -1,8 +1,9 @@
 from step0_access_path import access_path
 import os
 import shutil
+import numpy as np 
 from build_dataset_combine import Check_dir_exist_and_build_new_dir
-from util import get_dir_certain_file_name
+from util import get_dir_certain_file_name, get_maxmin_train_move_from_path, get_max_db_move_xy
 # access_path = "D:/Users/user/Desktop/db/" ### 後面直接補上 "/"囉，就不用再 +"/"+，自己心裡知道就好！
 
 def build_datasets(build_dir_name, in_dir_name, gt_dir_name, in_src_dir, in_src_word, gt_src_dir, gt_src_word):
@@ -57,7 +58,7 @@ def build_datasets(build_dir_name, in_dir_name, gt_dir_name, in_src_dir, in_src_
 
 
 ### 1.建立給 unet 用的 pad2000-512to256，但先不要執行喔！因為目前用的是手動複製的，然後忘記當初怎麼複製的ˊ口ˋ 咪挺完看能不能就先 生成新的DB然後再改囉！
-# build_datasets(build_dir_name="1_pad2000-512to256",
+# build_datasets(build_dir_name="1_pure_unet2000-512to256",
 #                in_dir_name   = "dis_imgs",
 #                gt_dir_name   = "move_maps",
 #                in_src_dir    = access_path+"step3_apply_flow_result",
@@ -66,7 +67,8 @@ def build_datasets(build_dir_name, in_dir_name, gt_dir_name, in_src_dir, in_src_
 #                gt_src_word   = ".npy" )
 
 
-# build_datasets(build_dir_name="1_page_h=384,w=256",
+
+# build_datasets(build_dir_name="1_pure_unet_page_h=384,w=256",
 #                in_dir_name   = "dis_imgs",
 #                gt_dir_name   = "move_maps",
 #                in_src_dir    = access_path+"step3_apply_flow_result",
@@ -84,22 +86,35 @@ def build_datasets(build_dir_name, in_dir_name, gt_dir_name, in_src_dir, in_src_
 #                in_src_word   = "3a1-I1-patch.bmp",
 #                gt_src_word   = "img.bmp" )
 
-build_datasets(build_dir_name= "2_pure_rect2_h=384,w=256",
-               in_dir_name   = "dis_img_db",
-               gt_dir_name   = "gt_ord_pad_img_db",
-               in_src_dir    = access_path+"step3_apply_flow_result",
-               gt_src_dir    = access_path+"step3_apply_flow_result",
-               in_src_word   = "3a1-I1-patch.bmp",
-               gt_src_word   = "4-gt_ord_pad.bmp" )
+
+
+# build_datasets(build_dir_name= "2_pure_rect2_h=384,w=256",
+#                in_dir_name   = "dis_img_db",
+#                gt_dir_name   = "gt_ord_pad_img_db",
+#                in_src_dir    = access_path+"step3_apply_flow_result",
+#                gt_src_dir    = access_path+"step3_apply_flow_result",
+#                in_src_word   = "3a1-I1-patch.bmp",
+#                gt_src_word   = "4-gt_ord_pad.bmp" )
 
 
 ### 3.建立給 unet+rect2 用的 rect2_2000
-# build_datasets(build_dir_name= "3_unet_rect2_h=256,w=256",
+# build_datasets(build_dir_name= "3_unet_rect2_h=384,w=256",
 #                in_dir_name   = "unet_rec_img_db",
-#                gt_dir_name   = "gt_unet_rec_img_db",
-#                in_src_dir    = access_path+"step11_unet_rec_img",
-#                gt_src_dir    = access_path+"step12_gt_ord",
+#                gt_dir_name   = "gt_ord_img_db",
+#                in_src_dir    = access_path+"result/20200316-114012_1_page_h=384,w=256_model2_UNet_512to256_127.28_finish/test_1_unet_page_h=384,w=256",
+#                gt_src_dir    = access_path+"step3_apply_flow_result",
 #                in_src_word   = "unet_rec_img.bmp",
-#                gt_src_word   = "img.bmp" )
+#                gt_src_word   = "1-I.bmp" )
 
 
+
+
+# db_name = "1_pure_unet2000-512to256_index"
+# max_train_move, min_train_move = get_maxmin_train_move_from_path(access_path+"datasets"+"/"+db_name+"/"+"train"+"/"+"move_maps")
+# np.save(access_path+"datasets"+"/"+db_name+"/"+"max_train_move",max_train_move)
+# np.save(access_path+"datasets"+"/"+db_name+"/"+"min_train_move",min_train_move)
+
+# db_name = "1_pure_unet_page_h=384,w=256"
+# max_db_move_x, max_db_move_y = get_max_db_move_xy(db_dir=access_path+"datasets", db_name=db_name)
+# np.save(access_path+"datasets"+"/"+db_name+"/"+"max_db_move_x",max_db_move_x)
+# np.save(access_path+"datasets"+"/"+db_name+"/"+"max_db_move_y",max_db_move_y)
