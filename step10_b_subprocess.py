c18ba024a39d31a7b453c1fed5e63cdea3787908
KongBoy <s89334roy@yahoo.com.tw>
2021-03-18 17:40:29 +0800

    step10b 加一些任務進去跑

--- step10_b_subprocess.py
+++ step10_b_subprocess.py
@@ -2,17 +2,17 @@ import subprocess as sb
 same_command = ["python", "step10_a_load_and_train_and_test.py"]
 
 ### hid_ch=64, 來測試 epoch系列 ##############################
-sb.run(same_command + ["blender_os_book_flow_unet_epoch050.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_epoch100.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_epoch200.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_epoch300.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_epoch500.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_epoch700.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch050.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch100.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch200.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch300.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch700.run()"])   ### 127.35  ### 正在弄 new_shuf
 
-sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_008.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_016.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_032.run()"])   ### 127.35  ### 正在弄 new_shuf
-sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_128.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_008.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_016.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_032.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch500.run()"])     ### 127.35  ### 正在弄 new_shuf  ### 跑錯重跑
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_128.run()"])   ### 127.28  ### 正在弄 new_shuf
 
 
 # sb.run(same_command + ["blender_os_book_flow_unet_bn04.run()"])   ### 127.28
