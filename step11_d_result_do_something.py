134e7066d1412839ef188325d3e025a5ce5ba9a1
KongBoy <s89334roy@yahoo.com.tw>
2021-03-18 17:39:57 +0800

    step_11_d 加些註解

--- step11_d_result_do_something.py
+++ step11_d_result_do_something.py
@@ -1,13 +1,13 @@
 from step11_c_result_instance import compress_results, rec_bm_results
 
 if(__name__ == "__main__"):
-    ### matplot_visual
+    ### matplot_visual   ### 覺得我也不會看所以就省了ˊ口ˋ
     # for result in compress_results:
     #     result.save_all_single_see_as_matplot_visual_multiprocess(add_loss=True)
 
-    ### bm, rec
+    ### bm, rec，會順便把 npy 轉 npz 喔！
     for rec_bm_result in rec_bm_results:
-        rec_bm_result.save_all_single_see_as_matplot_bm_rec_visual(start_index=0, amount=7, add_loss=False, bgr2rgb=True, single_see_multiprocess=True)
+        rec_bm_result.save_all_single_see_as_matplot_bm_rec_visual(start_index=0, amount=12, add_loss=False, bgr2rgb=True, single_see_multiprocess=True)
 
         # rec_bm_result.save_single_see_as_matplot_bm_rec_visual(see_num=0, add_loss=False, bgr2rgb=True, single_see_multiprocess=True)
         # rec_bm_result.save_single_see_as_matplot_bm_rec_visual(see_num=1, add_loss=False, bgr2rgb=True, single_see_multiprocess=True)
