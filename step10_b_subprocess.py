7fae8a7767824025d7dc01e45904d629e181b295
KongBoy <s89334roy@yahoo.com.tw>
2021-03-22 21:57:43 +0800

    step10_b 排版

--- step10_b_subprocess.py
+++ step10_b_subprocess.py
@@ -6,13 +6,13 @@ same_command = ["python", "step10_a_load_and_train_and_test.py"]
 # sb.run(same_command + ["blender_os_book_flow_unet_epoch100.run()"])   ### 127.35  ### 正在弄 new_shuf
 # sb.run(same_command + ["blender_os_book_flow_unet_epoch200.run()"])   ### 127.35  ### 正在弄 new_shuf
 # sb.run(same_command + ["blender_os_book_flow_unet_epoch300.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_epoch500.run()"])   ### 127.35  ### 正在弄 new_shuf
 # sb.run(same_command + ["blender_os_book_flow_unet_epoch700.run()"])   ### 127.35  ### 正在弄 new_shuf
 
-# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_008.run()"])   ### 127.35  ### 正在弄 new_shuf
-# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_016.run()"])   ### 127.35  ### 正在弄 new_shuf
-# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_032.run()"])   ### 127.35  ### 正在弄 new_shuf
-# sb.run(same_command + ["blender_os_book_flow_unet_epoch500.run()"])     ### 127.35  ### 正在弄 new_shuf  ### 跑錯重跑
 # sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_128.run()"])   ### 127.28  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_032.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_016.run()"])   ### 127.35  ### 正在弄 new_shuf
+# sb.run(same_command + ["blender_os_book_flow_unet_hid_ch_008.run()"])   ### 127.35  ### 正在弄 new_shuf
 
 
 # sb.run(same_command + ["blender_os_book_flow_unet_bn04.run()"])   ### 127.28
@@ -22,8 +22,8 @@ same_command = ["python", "step10_a_load_and_train_and_test.py"]
 # sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn04.run()"])   ### 127.28
 # sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn08.run()"])   ### 127.28
 # sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn16.run()"])   ### 127.28
-# sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn32.run()"])   ### 127.28  ### 失敗
-# sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn64.run()"])   ### 127.28  ### 失敗
+# # sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn32.run()"])   ### 127.28  ### 失敗
+# # sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn64.run()"])   ### 127.28  ### 失敗
 
 
 # sb.run(same_command + ["blender_os_book_flow_unet_ch32_bn04_set_arg_ok.run()"])   ### 127.35  ### 正在弄 new_shuf
