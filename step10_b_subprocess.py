18d13ff3e7ce5d917c925e345c1704856c8cf858
KongBoy <s89334roy@yahoo.com.tw>
2021-04-08 00:07:37 +0800

    step10_ab 加入 測試 range 的實驗

--- step10_b_subprocess.py
+++ step10_b_subprocess.py
@@ -63,15 +63,27 @@ same_command = ["python", "step10_a_load_and_train_and_test.py"]
 # sb.run(same_command + ["unet_5l.run()"])  ### 看看Activation 完再concat的效果
 # sb.run(same_command + ["unet_6l.run()"])  ### 看看Activation 完再concat的效果
 # sb.run(same_command + ["unet_7l.run()"])  ### 看看Activation 完再concat的效果
-sb.run(same_command + ["unet_8l.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_8l.run()"])  ### 看看Activation 完再concat的效果
 
-sb.run(same_command + ["unet_8l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-# sb.run(same_command + ["unet_7l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-# sb.run(same_command + ["unet_6l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-# sb.run(same_command + ["unet_5l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-# sb.run(same_command + ["unet_4l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-# sb.run(same_command + ["unet_3l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-# sb.run(same_command + ["unet_2l_skip_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_8l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_7l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_6l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_5l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_4l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_3l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+# sb.run(same_command + ["unet_2l_skip_use_add.run()"])  ### 看看Activation 完再concat的效果
+
+# 01_01_01 已跑完 500epoch 有lr下降
+# 01_th_th 已跑完 500epoch 有lr下降
+# 以下先跑 200epoch 無lr下降
+sb.run(same_command + ["in_01_ch64_in_epoch500_tanh____gt_tanh.run()"])  ### 看看Activation 完再concat的效果
+sb.run(same_command + ["in_01_ch64_in_epoch500_sigmoid_gt_tanh.run()"])  ### 看看Activation 完再concat的效果
+sb.run(same_command + ["in_tanh_ch64_in_epoch500_tanh____gt_01.run()"])  ### 看看Activation 完再concat的效果
+sb.run(same_command + ["in_tanh_ch64_in_epoch500_sigmoid_gt_01.run()"])  ### 看看Activation 完再concat的效果
+sb.run(same_command + ["in_tanh_ch64_in_epoch500_tanh____gt_tanh.run()"])  ### 看看Activation 完再concat的效果
+sb.run(same_command + ["in_tanh_ch64_in_epoch500_sigmoid_gt_tanh.run()"])  ### 看看Activation 完再concat的效果
+
+
 
 #######################
 # sb.run(same_command + ["rect_fk3_ch64_tfIN_resb_ok9_epoch500.run()"])
@@ -84,10 +96,9 @@ sb.run(same_command + ["unet_8l_skip_add.run()"])  ### 看看Activation 完再co
 # sb.run(same_command + ["rect_6_level_fk3.run()"])
 # sb.run(same_command + ["rect_7_level_fk3.run()"])
 
-sb.run(same_command + ["rect_2_level_fk3_ReLU.run()"])
-sb.run(same_command + ["rect_3_level_fk3_ReLU.run()"])
-sb.run(same_command + ["unet_8l_skip_add.run()"])  ### 看看Activation 完再concat的效果
-sb.run(same_command + ["rect_4_level_fk3_ReLU.run()"])
-sb.run(same_command + ["rect_5_level_fk3_ReLU.run()"])
-sb.run(same_command + ["rect_6_level_fk3_ReLU.run()"])
-sb.run(same_command + ["rect_7_level_fk3_ReLU.run()"])
+# sb.run(same_command + ["rect_2_level_fk3_ReLU.run()"])  ### 127.28跑
+# sb.run(same_command + ["rect_3_level_fk3_ReLU.run()"])  ### 127.28跑
+# sb.run(same_command + ["rect_4_level_fk3_ReLU.run()"])  ### 127.28跑
+# sb.run(same_command + ["rect_5_level_fk3_ReLU.run()"])  ### 127.28跑
+# sb.run(same_command + ["rect_6_level_fk3_ReLU.run()"])  ### 127.28跑
+# sb.run(same_command + ["rect_7_level_fk3_ReLU.run()"])  ### 127.28跑
