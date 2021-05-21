9c464596167add71487358781900d2ea34eb4112
KongBoy <s89334roy@yahoo.com.tw>
2021-05-21 11:15:53 +0800

    step8ae, 10ab 加入 sSE 模型、exp、subprocess

--- step08_a_1_UNet_IN.py
+++ step08_a_1_UNet_IN.py
@@ -13,7 +13,7 @@ import time
 ### 所有 pytorch BN 裡面有兩個參數的設定不確定～： affine=True, track_running_stats=True，目前思考覺得改道tf2全拿掉也可以
 ### 目前 總共用7層，所以size縮小 2**7 ，也就是 1/128 這樣子！例如256*256*3丟進去，最中間的feature map長寬2*2*512喔！
 class Generator(tf.keras.models.Model):
-    def __init__(self, hid_ch=64, depth_level=7, no_concat_layer=0, skip_use_add=False, skip_use_cSE=False, skip_use_cnn=False, skip_cnn_k=3, skip_use_Acti=None, out_tanh=True, out_ch=3, **kwargs):
+    def __init__(self, hid_ch=64, depth_level=7, no_concat_layer=0, skip_use_add=False, skip_use_cSE=False, skip_use_sSE=False, skip_use_cnn=False, skip_cnn_k=3, skip_use_Acti=None, out_tanh=True, out_ch=3, **kwargs):
         """
         depth_level     : 2~8, 9有點難，因為要是512的倍數，不是512就1024，只能等我研究好512的dataset才有機會式
         no_concat_layer : 2~8, 0 1 的話代表全concat，2 代表 conv2 的結果 到 decoder 不concat， 3 代表 conv2~3 的結果到 decoder 不concat， 4 代表 conv2~4 ..... 同理
@@ -24,6 +24,7 @@ class Generator(tf.keras.models.Model):
         self.depth_level = depth_level
         self.no_concat_layer = no_concat_layer
         self.skip_use_cSE = skip_use_cSE
+        self.skip_use_sSE = skip_use_sSE
         self.skip_use_add = skip_use_add
         self.skip_use_cnn  = skip_use_cnn
         self.skip_use_Acti = skip_use_Acti
@@ -36,6 +37,7 @@ class Generator(tf.keras.models.Model):
             self.conv2  = Conv2D(hid_ch * 2, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv2")  #,bias=False) ### in_channel:64
             if(self.depth_level > 2): self.in2    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in2")
             if(self.skip_use_cSE):  self.skip_cSE2  = cSE(hid_ch * 1, ratio=2 * 2, name="skip_cSE2")
+            if(self.skip_use_sSE):  self.skip_sSE2  = sSE(name="skip_sSE2")
             if(self.skip_use_cnn):  self.skip_cnn2  = Conv2D(hid_ch * 2, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn2")
             if(self.skip_use_Acti): self.skip_Acti2 = Activation(self.skip_use_Acti, name="skip_%s2" % self.skip_use_Acti.__name__)
 
@@ -44,6 +46,7 @@ class Generator(tf.keras.models.Model):
             self.conv3  = Conv2D(hid_ch * 4, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv3")  #,bias=False) ### in_channel:128
             if(self.depth_level > 3): self.in3    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in3")
             if(self.skip_use_cSE):  self.skip_cSE3  = cSE(hid_ch * 2, ratio=2 * 4, name="skip_cSE3")
+            if(self.skip_use_sSE):  self.skip_sSE3  = sSE(name="skip_sSE3")
             if(self.skip_use_cnn):  self.skip_cnn3  = Conv2D(hid_ch * 4, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn3")
             if(self.skip_use_Acti): self.skip_Acti3 = Activation(self.skip_use_Acti, name="skip_%s3" % self.skip_use_Acti.__name__)
 
@@ -52,6 +55,7 @@ class Generator(tf.keras.models.Model):
             self.conv4  = Conv2D(hid_ch * 8, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv4")  #,bias=False) ### in_channel:256
             if(self.depth_level > 4): self.in4    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in4")
             if(self.skip_use_cSE):  self.skip_cSE4  = cSE(hid_ch * 4, ratio=2 * 8, name="skip_cSE4")
+            if(self.skip_use_sSE):  self.skip_sSE4  = sSE(name="skip_sSE4")
             if(self.skip_use_cnn):  self.skip_cnn4  = Conv2D(hid_ch * 8, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn4")
             if(self.skip_use_Acti): self.skip_Acti4 = Activation(self.skip_use_Acti, name="skip_%s4" % self.skip_use_Acti.__name__)
 
@@ -60,6 +64,7 @@ class Generator(tf.keras.models.Model):
             self.conv5  = Conv2D(hid_ch * 8, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv5")  #,bias=False) ### in_channel:512
             if(self.depth_level > 5): self.in5    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in5")
             if(self.skip_use_cSE):  self.skip_cSE5  = cSE(hid_ch * 8, ratio=2 * 8, name="skip_cSE5")
+            if(self.skip_use_sSE):  self.skip_sSE5  = sSE(name="skip_sSE5")
             if(self.skip_use_cnn):  self.skip_cnn5  = Conv2D(hid_ch * 8, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn5")
             if(self.skip_use_Acti): self.skip_Acti5 = Activation(self.skip_use_Acti, name="skip_%s5" % self.skip_use_Acti.__name__)
 
@@ -68,6 +73,7 @@ class Generator(tf.keras.models.Model):
             self.conv6  = Conv2D(hid_ch * 8, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv6")  #,bias=False) ### in_channel:512
             if(self.depth_level > 6): self.in6    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in6")
             if(self.skip_use_cSE):  self.skip_cSE6  = cSE(hid_ch * 8, ratio=2 * 8, name="skip_cSE6")
+            if(self.skip_use_sSE):  self.skip_sSE6  = sSE(name="skip_sSE6")
             if(self.skip_use_cnn):  self.skip_cnn6  = Conv2D(hid_ch * 8, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn6")
             if(self.skip_use_Acti): self.skip_Acti6 = Activation(self.skip_use_Acti, name="skip_%s6" % self.skip_use_Acti.__name__)
 
@@ -76,6 +82,7 @@ class Generator(tf.keras.models.Model):
             self.conv7   = Conv2D(hid_ch * 8, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv7")  #,bias=False) ### in_channel:512
             if(self.depth_level > 7): self.in7    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in7")
             if(self.skip_use_cSE):  self.skip_cSE7  = cSE(hid_ch * 8, ratio=2 * 8, name="skip_cSE7")
+            if(self.skip_use_sSE):  self.skip_sSE7  = sSE(name="skip_sSE7")
             if(self.skip_use_cnn):  self.skip_cnn7  = Conv2D(hid_ch * 8, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn7")
             if(self.skip_use_Acti): self.skip_Acti7 = Activation(self.skip_use_Acti, name="skip_%s7" % self.skip_use_Acti.__name__)
 
@@ -84,6 +91,7 @@ class Generator(tf.keras.models.Model):
             self.conv8   = Conv2D(hid_ch * 8, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv8")  #,bias=False) ### in_channel:512
             if(self.depth_level > 8): self.in8    = InstanceNormalization(axis=3, center=True, scale=True, beta_initializer="random_uniform", gamma_initializer="random_uniform", name="in8")
             if(self.skip_use_cSE):  self.skip_cSE8  = cSE(hid_ch * 8, ratio=2 * 8, name="skip_cSE8")
+            if(self.skip_use_sSE):  self.skip_sSE8  = sSE(name="skip_sSE8")
             if(self.skip_use_cnn):  self.skip_cnn8  = Conv2D(hid_ch * 8, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn8")
             if(self.skip_use_Acti): self.skip_Acti8 = Activation(self.skip_use_Acti, name="skip_%s8" % self.skip_use_Acti.__name__)
 
@@ -93,6 +101,7 @@ class Generator(tf.keras.models.Model):
             self.lrelu9  = LeakyReLU(alpha=0.2, name="lrelu9")
             self.conv9   = Conv2D(hid_ch * 8, kernel_size=(4, 4), strides=(2, 2), padding="same", name="conv9")  #,bias=False) ### in_channel:512
             if(self.skip_use_cSE):  self.skip_cSE9  = cSE(hid_ch * 8, ratio=2 * 8, name="skip_cSE9")
+            if(self.skip_use_sSE):  self.skip_sSE9  = sSE(name="skip_sSE9")
             if(self.skip_use_cnn):  self.skip_cnn9  = Conv2D(hid_ch * 8, kernel_size=(skip_cnn_k, skip_cnn_k), strides=(1, 1), padding="same", name="skip_cnn9")
             if(self.skip_use_Acti): self.skip_Acti9 = Activation(self.skip_use_Acti, name="skip_%s9" % self.skip_use_Acti.__name__)
 
@@ -160,6 +169,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 2):
             skip2 = x
             if(self.skip_use_cSE):  skip2 = self.skip_cSE2(skip2)
+            if(self.skip_use_sSE):  skip2 = self.skip_sSE2(skip2)
             if(self.skip_use_cnn):  skip2 = self.skip_cnn2(skip2)
             if(self.skip_use_Acti): skip2 = self.skip_Acti2(skip2)
             x = self.lrelu2(x)
@@ -169,6 +179,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 3):
             skip3 = x
             if(self.skip_use_cSE):  skip3 = self.skip_cSE3(skip3)
+            # if(self.skip_use_sSE):  skip3 = self.skip_sSE3(skip3)
             if(self.skip_use_cnn):  skip3 = self.skip_cnn3(skip3)
             if(self.skip_use_Acti): skip3 = self.skip_Acti3(skip3)
             x = self.lrelu3(x)
@@ -178,6 +189,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 4):
             skip4 = x
             if(self.skip_use_cSE):  skip4 = self.skip_cSE4(skip4)
+            if(self.skip_use_sSE):  skip4 = self.skip_sSE4(skip4)
             if(self.skip_use_cnn):  skip4 = self.skip_cnn4(skip4)
             if(self.skip_use_Acti): skip4 = self.skip_Acti4(skip4)
             x = self.lrelu4(x)
@@ -187,6 +199,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 5):
             skip5 = x
             if(self.skip_use_cSE):  skip5 = self.skip_cSE5(skip5)
+            if(self.skip_use_sSE):  skip5 = self.skip_sSE5(skip5)
             if(self.skip_use_cnn):  skip5 = self.skip_cnn5(skip5)
             if(self.skip_use_Acti): skip5 = self.skip_Acti5(skip5)
             x = self.lrelu5(x)
@@ -196,6 +209,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 6):
             skip6 = x
             if(self.skip_use_cSE):  skip6 = self.skip_cSE6(skip6)
+            if(self.skip_use_sSE):  skip6 = self.skip_sSE6(skip6)
             if(self.skip_use_cnn):  skip6 = self.skip_cnn6(skip6)
             if(self.skip_use_Acti): skip6 = self.skip_Acti6(skip6)
             x = self.lrelu6(x)
@@ -205,6 +219,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 7):
             skip7 = x
             if(self.skip_use_cSE):  skip7 = self.skip_cSE7(skip7)
+            if(self.skip_use_sSE):  skip7 = self.skip_sSE7(skip7)
             if(self.skip_use_cnn):  skip7 = self.skip_cnn7(skip7)
             if(self.skip_use_Acti): skip7 = self.skip_Acti7(skip7)
             x = self.lrelu7(x)
@@ -214,6 +229,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 8):
             skip8 = x
             if(self.skip_use_cSE):  skip8 = self.skip_cSE8(skip8)
+            if(self.skip_use_sSE):  skip8 = self.skip_sSE8(skip8)
             if(self.skip_use_cnn):  skip8 = self.skip_cnn8(skip8)
             if(self.skip_use_Acti): skip8 = self.skip_Acti8(skip8)
             x = self.lrelu8(x)
@@ -224,6 +240,7 @@ class Generator(tf.keras.models.Model):
         if(self.depth_level >= 9):
             skip9 = x
             if(self.skip_use_cSE):  skip9 = self.skip_cSE9(skip9)
+            if(self.skip_use_sSE):  skip9 = self.skip_sSE9(skip9)
             if(self.skip_use_cnn):  skip9 = self.skip_cnn9(skip9)
             if(self.skip_use_Acti): skip9 = self.skip_Acti9(skip9)
             x = self.lrelu9(x)
@@ -355,7 +372,8 @@ if(__name__ == "__main__"):
     flow_unet_IN_7l_ch64_skip_use_cnn1_USErelu    = KModel_builder().set_model_name(MODEL_NAME.flow_unet).build_flow_unet(hid_ch=64, skip_use_cnn=True, skip_cnn_k=1, skip_use_Acti=tf.nn.relu, true_IN=True).build()
     flow_unet_IN_7l_ch64_skip_use_cnn1_USEsigmoid = KModel_builder().set_model_name(MODEL_NAME.flow_unet).build_flow_unet(hid_ch=64, skip_use_cnn=False, skip_cnn_k=1, skip_use_Acti=tf.nn.sigmoid, true_IN=True).build()
     flow_unet_IN_7l_ch64_skip_use_cSE             = KModel_builder().set_model_name(MODEL_NAME.flow_unet).build_flow_unet(hid_ch=64, skip_use_cSE=True, true_IN=True)
-    model_obj = flow_unet_IN_7l_ch64_skip_use_cSE.build()  ### 可替換成 上面 想測試的 model
+    flow_unet_IN_7l_ch64_skip_use_sSE             = KModel_builder().set_model_name(MODEL_NAME.flow_unet).build_flow_unet(hid_ch=64, skip_use_sSE=True, true_IN=True)
+    model_obj = flow_unet_IN_7l_ch64_skip_use_sSE.build()  ### 可替換成 上面 想測試的 model
     # flow_unet_IN_ch64 = KModel_builder().set_model_name(MODEL_NAME.flow_unet).build_flow_unet(hid_ch=64, out_ch=3, true_IN=True)
     ### 2. db_obj 和 tf_data
     db_obj = Dataset_builder().set_basic(DB_C.type8_blender_os_book                      , DB_N.blender_os_hw768      , DB_GM.in_dis_gt_flow, h=768, w=768).set_dir_by_basic().set_in_gt_format_and_range(in_format="png", gt_format="knpy").set_detail(have_train=True, have_see=True).build()
