b7d13f7815231712751fa3be6824e847a3e97986
KongBoy <s89334roy@yahoo.com.tw>
2021-08-24 15:23:35 +0800

    step6a 加入 type9_try_segmentation

--- step06_a_datas_obj.py
+++ step06_a_datas_obj.py
@@ -16,6 +16,7 @@ class DB_CATEGORY(Enum):
     type7_h472_w304_real_os_book                  = "type7_h472_w304_real_os_book"
     type7b_h500_w332_real_os_book                 = "type7b_h500_w332_real_os_book"
     type8_blender_os_book                         = "type8_blender_os_book"
+    type9_try_segmentation                        = "type9_try_segmentation"
 
 
 class DB_NAME(Enum):
@@ -53,6 +54,8 @@ class DB_NAME(Enum):
     blender_os_hw756       = "blender_os_hw756"
     blender_os_hw768       = "blender_os_hw768"
 
+    try_segmentation       = "try_segmentation"
+
 class DB_GET_METHOD(Enum):
     no_detail = ""
     in_dis_gt_move_map = "in_dis_gt_move_map"
@@ -62,6 +65,8 @@ class DB_GET_METHOD(Enum):
     test_indicate      = "test_indicate"
 
     in_dis_gt_flow     = "in_dis_gt_flow"
+
+    in_img_gt_mask   = "in_img_gt_mask"
 
 class VALUE_RANGE(Enum):
     zero_to_one = "0~1"
@@ -178,6 +183,9 @@ class Dataset_dir_builder(Dataset_basic_builder):
         elif(self.db.get_method == DB_GET_METHOD.in_dis_gt_flow)      :
             in_dir_name = "dis_imgs"
             gt_dir_name = "flows"
+        elif(self.db.get_method == DB_GET_METHOD.in_img_gt_mask)    :
+            in_dir_name = "in_imgs"
+            gt_dir_name = "gt_masks"
 
 
         self.db.train_in_dir = self.db.db_dir + "/train/" + in_dir_name
@@ -235,13 +243,14 @@ type7b_h500_w332_real_os_book_400data        = Dataset_builder().set_basic(DB_C.
 type7b_h500_w332_real_os_book_800data        = Dataset_builder().set_basic(DB_C.type7b_h500_w332_real_os_book              , DB_N.os_book_800data       , DB_GM.in_dis_gt_ord, h=500, w=332).set_dir_by_basic().set_in_gt_format_and_range(in_format="jpg", in_range="0~255", gt_format="jpg", gt_range="0~255").set_detail(have_train=True, have_see=True)
 type8_blender_os_book_756                    = Dataset_builder().set_basic(DB_C.type8_blender_os_book                      , DB_N.blender_os_hw756      , DB_GM.in_dis_gt_flow, h=756, w=756).set_dir_by_basic().set_in_gt_format_and_range(in_format="png", in_range="0~255", gt_format="knpy", gt_range="0~1").set_detail(have_train=True, have_see=True)
 type8_blender_os_book_768                    = Dataset_builder().set_basic(DB_C.type8_blender_os_book                      , DB_N.blender_os_hw768      , DB_GM.in_dis_gt_flow, h=768, w=768).set_dir_by_basic().set_in_gt_format_and_range(in_format="png", in_range="0~255", gt_format="knpy", gt_range="0~1", rec_hope_format="jpg", rec_hope_range="0~255").set_detail(have_train=True, have_see=True, have_rec_hope=True)
-
+type9_try_segmentation                       = Dataset_builder().set_basic(DB_C.type9_try_segmentation                     , DB_N.try_segmentation      , DB_GM.in_img_gt_mask, h=1280, w=1918).set_dir_by_basic().set_in_gt_format_and_range(in_format="jpg", in_range="0~255", gt_format="gif", gt_range="0~255").set_detail(have_train=True, have_see=False)
 
 if(__name__ == "__main__"):
     db = Dataset_builder().set_basic(DB_C.type5c_real_have_see_no_bg, DB_N.no_bg_gt_gray3ch, DB_GM.in_dis_gt_ord, h=472, w=304).set_dir_by_basic().set_in_gt_format_and_range(in_format="bmp", in_range="0~255", gt_format="bmp", gt_range="0~255").set_detail(have_train=True, have_see=True).build()
     db = Dataset_builder().set_basic(DB_C.type7_h472_w304_real_os_book,                DB_N.os_book_400data,  DB_GM.in_dis_gt_ord, h=472, w=304).set_dir_by_basic().set_in_gt_format_and_range(in_format="jpg", in_range="0~255", gt_format="jpg", gt_range="0~255").set_detail(have_train=True, have_see=True).build()
     db = Dataset_builder().set_basic(DB_C.type7b_h500_w332_real_os_book,               DB_N.os_book_1532data, DB_GM.in_dis_gt_ord, h=500, w=332).set_dir_by_basic().set_in_gt_format_and_range(in_format="jpg", in_range="0~255", gt_format="jpg", gt_range="0~255").set_detail(have_train=True, have_see=True).build()
     print(type8_blender_os_book_768.build())
+    print(type9_try_segmentation.build())
     # db_complex_1_pure_unet = Datasets(DB_CATEGORY.type1_h_256_w_256_complex, DB_GET_METHOD.in_dis_gt_move_map   , h=256, w=256 )
     # db_complex_2_pure_rect = Datasets(DB_CATEGORY.type1_h_256_w_256_complex, DB_GET_METHOD.in_dis_gt_ord_pad_img, h=256, w=256 )
     # db_complex_3_pure_rect = Datasets(DB_CATEGORY.type1_h_256_w_256_complex, DB_GET_METHOD.in_rec_gt_ord_img    , h=256, w=256 )
