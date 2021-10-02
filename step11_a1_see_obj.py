3a2b1408e4b7ac17634c95dff0bbc96c39885ac7
KongBoy <s89334roy@yahoo.com.tw>
2021-10-02 17:11:43 +0800

    step11a1 加入 See_mask

--- step11_a1_see_obj.py
+++ step11_a1_see_obj.py
@@ -425,7 +425,6 @@ class See_bm_rec(See_npy_to_npz):
         # move_dir_certain_file(self.old_rec_visual_read_dir,  certain_word="combine",   certain_ext=".avi", dst_dir=self.rec_visual_read_dir,  print_msg=print_msg)
         # move_dir_certain_file(self.old_rec_visual_write_dir, certain_word="combine",   certain_ext=".avi", dst_dir=self.rec_visual_write_dir, print_msg=print_msg)
 
-
     ### 給下一步 metric 用的
     def get_bm_rec_info(self):
         """
@@ -917,8 +916,47 @@ class See_rec_metric(See_bm_rec):
             single_row_imgs.Save_fig(dst_dir=self.matplot_metric_visual_write_dir, epoch=go_epoch, epoch_name="metric_epoch")  ### 話完圖就可以存了喔！
 
 
+#############################################################################################################################################################################################################################################################################################
+#############################################################################################################################################################################################################################################################################################
+#############################################################################################################################################################################################################################################################################################
+class See_mask(See_info):
+    def __init__(self, result_read_dir, result_write_dir, see_name):
+        super(See_mask, self).__init__(result_read_dir, result_write_dir, see_name)
+        """
+        __init__：放 Dir：..._read_dir
+                          ..._write_dir
+        """
+        self.old1_mask_read_dir   = self.see_read_dir
+        self.old1_mask_write_dir  = self.see_write_dir
+        ##########################################################################################
+        self.mask_read_dir      = self.see_read_dir  + "/1_mask"  ### 之後也許東西多起來會需要獨立資料夾， 先寫起來放
+        self.mask_write_dir     = self.see_write_dir + "/1_mask"  ### 之後也許東西多起來會需要獨立資料夾， 先寫起來放
+        # self.mask_read_dir      = self.see_read_dir
+        # self.mask_write_dir     = self.see_write_dir
 
-class See(See_flow_visual, See_rec_metric):
+        ### 資料夾的位置有改 保險起見加一下，久了確定 放的位置都更新了 可刪這行喔
+        # self.Change_mask_dir(print_msg=True)
+
+    ### 先寫起來放著
+    def Change_mask_dir(self, print_msg=False):  ### Change_dir 寫這 而不寫在 外面 是因為 see資訊 是要在 class See 裡面才方便看的到，所以 在這邊寫多個function 比較好寫，外面傳參數 要 exp.result.sees[]..... 很麻煩想到就不想寫ˊ口ˋ
+        print(datetime.datetime.now().strftime("%Y/%m/%d_%H:%M:%S"), f"See level: doing Change_mask_dir, Current See:{self.see_name}")
+        move_dir_certain_file(self.old1_mask_read_dir,  certain_word="epoch", certain_ext=".bmp", dst_dir=self.mask_read_dir,  print_msg=print_msg)
+        move_dir_certain_file(self.old1_mask_write_dir, certain_word="epoch", certain_ext=".bmp", dst_dir=self.mask_write_dir, print_msg=print_msg)
+
+    ### 給下一步 bm_rec 用的
+    def get_mask_info(self):
+        """
+        get_info：放 ..._names
+                      ├ ..._read_paths
+                      └ ..._write_paths
+                     file_amount
+        """
+        self.mask_names  = get_dir_certain_file_name(self.mask_read_dir , certain_word="epoch", certain_ext=".bmp")
+        self.mask_read_paths  = [self.mask_read_dir + "/" + name for name in self.mask_names]  ### 目前還沒用到～　所以也沒有寫 write_path 囉！
+
+        self.mask_amount = len(self.mask_read_paths)
+
+class See(See_flow_visual, See_rec_metric, See_mask):
     def __init__(self, result_read_dir, result_write_dir, see_name):
         super(See, self).__init__(result_read_dir, result_write_dir, see_name)
 
