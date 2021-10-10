ac34e0dcbc2cc3ef6d243750a66862137c4d9159
KongBoy <s89334roy@yahoo.com.tw>
2021-10-10 13:09:48 +0800

    step9, 10ab 加入 sobel_k5_s40

--- step09_b_loss_info_obj.py
+++ step09_b_loss_info_obj.py
@@ -206,6 +206,7 @@ G_bce_sobel_k5_loss_info_builder   = Loss_info_builder().set_loss_type("bce+mae_
 G_bce_sobel_k7_loss_info_builder   = Loss_info_builder().set_loss_type("bce+mae_sobel_k7")  #.build_gan_loss().build_gan_loss_containors()
 
 G_bce_sobel_k5_s20_loss_info_builder  = Loss_info_builder().set_loss_type("bce+mae_sobel_k5", kernel_scale=20)  #.build_gan_loss().build_gan_loss_containors()
+G_bce_sobel_k5_s40_loss_info_builder  = Loss_info_builder().set_loss_type("bce+mae_sobel_k5", kernel_scale=40)  #.build_gan_loss().build_gan_loss_containors()
 G_bce_sobel_k7_s780_loss_info_builder = Loss_info_builder().set_loss_type("bce+mae_sobel_k7", kernel_scale=780)  #.build_gan_loss().build_gan_loss_containors()
 GAN_mae_loss_info                  = Loss_info_builder().set_loss_type("justG")  #.build_gan_loss().build_gan_loss_containors()
 
