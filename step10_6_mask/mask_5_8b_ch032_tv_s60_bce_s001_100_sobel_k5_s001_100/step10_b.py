b450ab046f264f53e2d539373938929cb17daa38
KongBoy <s89334roy@yahoo.com.tw>
2021-11-02 11:01:39 +0800

    step9, 10ab 加入 8b_tv_s40, 60, 80, 100, bce, sobel

---
+++ step10_6_mask/mask_5_8b_ch032_tv_s60_bce_s001_100_sobel_k5_s001_100/step10_b.py
@@ -0,0 +1,51 @@
+'''
+目前只有 step10b 一定需要切換資料夾到 該step10b所在的資料夾 才能執行喔！
+'''
+
+import subprocess as sb
+### 沒有作用~~ 切不過去喔~~
+# sb.run(["conda.bat", "deactivate"])
+# sb.run(["conda.bat", "activate", "tensorflow_cpu"])
+
+same_command = ["python", "step10_a.py"]
+run = "build().run()"
+
+############################  have_bg  #################################
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s001_sobel_k5_s001.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s001_sobel_k5_s020.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s001_sobel_k5_s040.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s001_sobel_k5_s060.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s001_sobel_k5_s080.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s001_sobel_k5_s100.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s020_sobel_k5_s001.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s020_sobel_k5_s020.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s020_sobel_k5_s040.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s020_sobel_k5_s060.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s020_sobel_k5_s080.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s020_sobel_k5_s100.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s040_sobel_k5_s001.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s040_sobel_k5_s020.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s040_sobel_k5_s040.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s040_sobel_k5_s060.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s040_sobel_k5_s080.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s040_sobel_k5_s100.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s060_sobel_k5_s001.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s060_sobel_k5_s020.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s060_sobel_k5_s040.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s060_sobel_k5_s060.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s060_sobel_k5_s080.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s060_sobel_k5_s100.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s080_sobel_k5_s001.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s080_sobel_k5_s020.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s080_sobel_k5_s040.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s080_sobel_k5_s060.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s080_sobel_k5_s080.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s080_sobel_k5_s100.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s100_sobel_k5_s001.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s100_sobel_k5_s020.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s100_sobel_k5_s040.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s100_sobel_k5_s060.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s100_sobel_k5_s080.{run}"])  ### 127.35跑
+sb.run(same_command + [f"mask_h_bg_ch032_sig_6l_ep060_tv_s60_bce_s100_sobel_k5_s100.{run}"])  ### 127.35跑
+
+
