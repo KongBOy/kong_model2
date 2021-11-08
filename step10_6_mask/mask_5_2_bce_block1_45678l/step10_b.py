4b7b2b7d346f7ea4e2a203d3cece451e2acf446c
KongBoy <s89334roy@yahoo.com.tw>
2021-11-08 19:17:06 +0800

    step10ab 加入 bce_block1_ch_layer 各種跑得快的組合

---
+++ step10_6_mask/mask_5_2_bce_block1_45678l/step10_b.py
@@ -0,0 +1,42 @@
+'''
+目前只有 step10b 一定需要切換資料夾到 該step10b所在的資料夾 才能執行喔！
+'''
+
+import subprocess as sb
+### 沒有作用~~ 切不過去喔~~
+# sb.run(["conda.bat", "deactivate"])
+# sb.run(["conda.bat", "activate", "tensorflow_cpu"])
+
+same_command = ["python", "step10_a.py"]
+run = "build().run()"
+
+####
+sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s020.{run}"])
+sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s040.{run}"])
+sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s060.{run}"])
+sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s080.{run}"])
+sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s100.{run}"])
+####
+sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s020.{run}"])
+sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s040.{run}"])
+sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s060.{run}"])
+sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s080.{run}"])
+sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s100.{run}"])
+####
+sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s020.{run}"])
+sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s040.{run}"])
+sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s060.{run}"])
+sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s080.{run}"])
+sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s100.{run}"])
+####
+sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s020.{run}"])
+sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s040.{run}"])
+sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s060.{run}"])
+sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s080.{run}"])
+sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s100.{run}"])
+####
+sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s020.{run}"])
+sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s040.{run}"])
+sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s060.{run}"])
+sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s080.{run}"])
+sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s100.{run}"])
