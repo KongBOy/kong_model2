48b87475e20396cab1990369ab3630a4bc07c315
KongBoy <s89334roy@yahoo.com.tw>
2021-11-08 20:27:35 +0800

    step10ab tv_bce_ch032_block1_4l 加入 tv_s40, bce_s20~100

--- step10_6_mask/mask_5_6_tv_bce_block1_45678l/step10_b_tv_s01_bce_s20_100.py
+++ step10_6_mask/mask_5_6_tv_bce_block1_45678l/step10_b_tv_s01_bce_s20_100.py
@@ -44,3 +44,8 @@ run = "build().run()"
 # sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s020_bce_s060.{run}"])
 # sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s020_bce_s080.{run}"])
 # sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s020_bce_s100.{run}"])
+# sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s040_bce_s020.{run}"])
+# sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s040_bce_s040.{run}"])
+# sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s040_bce_s060.{run}"])
+# sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s040_bce_s080.{run}"])
+# sb.run(same_command + [f"ch032_sig_4l_ep060_tv_s040_bce_s100.{run}"])
