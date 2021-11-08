ef9792c2c0159b8e7ee5addda3887aaebb7fd57b
KongBoy <s89334roy@yahoo.com.tw>
2021-11-08 19:26:10 +0800

    step10ab bce_block1_45678l 多補 bce_s001進去

--- step10_6_mask/mask_5_2_bce_block1_45678l/step10_b.py
+++ step10_6_mask/mask_5_2_bce_block1_45678l/step10_b.py
@@ -10,31 +10,36 @@ import subprocess as sb
 same_command = ["python", "step10_a.py"]
 run = "build().run()"
 
-####
+#### 4l
+sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s001.{run}"])
 sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s020.{run}"])
 sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s040.{run}"])
 sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s060.{run}"])
 sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s080.{run}"])
 sb.run(same_command + [f"ch064_sig_4l_ep060_bce_s100.{run}"])
-####
+#### 5l
+sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s001.{run}"])
 sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s020.{run}"])
 sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s040.{run}"])
 sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s060.{run}"])
 sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s080.{run}"])
 sb.run(same_command + [f"ch032_sig_5l_ep060_bce_s100.{run}"])
-####
+#### 6l
+sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s001.{run}"])
 sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s020.{run}"])
 sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s040.{run}"])
 sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s060.{run}"])
 sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s080.{run}"])
 sb.run(same_command + [f"ch016_sig_6l_ep060_bce_s100.{run}"])
-####
+#### 7l
+sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s001.{run}"])
 sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s020.{run}"])
 sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s040.{run}"])
 sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s060.{run}"])
 sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s080.{run}"])
 sb.run(same_command + [f"ch008_sig_7l_ep060_bce_s100.{run}"])
-####
+#### 8l
+sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s001.{run}"])
 sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s020.{run}"])
 sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s040.{run}"])
 sb.run(same_command + [f"ch004_sig_8l_ep060_bce_s060.{run}"])
