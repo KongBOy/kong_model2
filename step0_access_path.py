dae4602a20b370323624ea5540f6de91abeff7cd
KongBoy <s89334roy@yahoo.com.tw>
2021-11-17 12:46:48 +0800

    step0 更新成127.23的狀態

--- step0_access_path.py
+++ step0_access_path.py
@@ -1,13 +1,14 @@
 ### 後面直接補上 "/"囉，就不用再 +"/"+，自己心裡知道就好！
-data_access_path    = "G:/0 data_dir/"    ### 通常是 讀取速度較快的SSD，127.35 是 400GB SSD
+data_access_path  = "data_dir/"    ### 通常是 讀取速度較快的SSD，127.35 是 400GB SSD
 
-result_read_path    = "D:/0 data_dir/"    ### 通常是 大容量的機械式硬碟，127.35 是 2T 機械式硬碟
-result_write_path   = "G:/0 data_dir/"    ### 通常是 有碎片也沒差的SSD，127.35 是 400GB SSD， 弄完再剪下貼上 到 大容量的硬碟
+# result_read_path  = "Z:/0 data_dir/"    ### 通常是 大容量的機械式硬碟，127.35 是 2T 機械式硬碟
+result_read_path  = "data_dir/"    ### 通常是 大容量的機械式硬碟，127.35 是 2T 機械式硬碟
+result_write_path = "data_dir/"    ### 通常是 有碎片也沒差的SSD，127.35 是 400GB SSD， 弄完再剪下貼上 到 大容量的硬碟
 
-analyze_write_dir = "X:/0 data_dir/"    ### 通常是 有碎片也沒差的SSD，127.35 是 400GB SSD， 弄完再剪下貼上 到 大容量的硬碟
+analyze_write_dir = "data_dir/"    ### 通常是 有碎片也沒差的SSD，127.35 是 400GB SSD， 弄完再剪下貼上 到 大容量的硬碟
 
 JPG_QUALITY = 30
-CORE_AMOUNT = 7
+CORE_AMOUNT = 6
 CORE_AMOUNT_NPY_TO_NPZ = 6
 CORE_AMOUNT_BM_REC_VISUAL = 8  ### 8  ### 14  ### 500
 CORE_AMOUNT_SAVE_AS_JPG = 6  ### 12         ### Save_as_jpg
