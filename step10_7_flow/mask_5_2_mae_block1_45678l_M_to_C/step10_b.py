de54930c5a6da35dad54569aae927b90356add60
KongBoy <s89334roy@yahoo.com.tw>
2021-11-25 09:29:08 +0800

    step10ab 127.27 L7_ch128_mae_s001/limit 填入result_name 和 compress

--- step10_7_flow/mask_5_2_mae_block1_45678l_M_to_C/step10_b.py
+++ step10_7_flow/mask_5_2_mae_block1_45678l_M_to_C/step10_b.py
@@ -72,8 +72,8 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L6_ch002_mae_s001.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch001_mae_s001.{compress_and_bm_rec_all}"])
 #### 7l ############################################################################################
-# sb.run(cmd_python_step10_a + [f"L7_ch128_mae_s001      .{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch128_mae_s001_limit.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch128_mae_s001      .{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch128_mae_s001_limit.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch064_mae_s001      .{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch064_mae_s001_limit.{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch032_mae_s001      .{run}"])
