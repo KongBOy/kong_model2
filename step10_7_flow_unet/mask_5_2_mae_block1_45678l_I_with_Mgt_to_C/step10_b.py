bae9daa80a1f0bed167569efefefe03867c15bbb
KongBoy <s89334roy@yahoo.com.tw>
2021-11-26 10:33:14 +0800

    127.29 L5 ch001 做不起來

--- step10_7_flow_unet/mask_5_2_mae_block1_45678l_I_with_Mgt_to_C/step10_b.py
+++ step10_7_flow_unet/mask_5_2_mae_block1_45678l_I_with_Mgt_to_C/step10_b.py
@@ -58,7 +58,7 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L5_ch008_mae_s001.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch004_mae_s001.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch002_mae_s001.{compress_and_bm_rec_all}"])
-# sb.run(cmd_python_step10_a + [f"L5_ch001_mae_s001.{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L5_ch001_mae_s001.{compress_and_bm_rec_all}"])  ### 做不起來
 #### 6l ############################################################################################
 # sb.run(cmd_python_step10_a + [f"L6_ch128_mae_s001      .{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch128_mae_s001_limit.{compress_and_bm_rec_all}"])
