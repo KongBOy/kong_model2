faacdcc019721555e9e1acc6ed5a756a68fc19d4
KongBoy <s89334roy@yahoo.com.tw>
2021-11-27 10:37:08 +0800

    step10ab, 11, 12 把前綴程式碼稍微統一

--- step10_7_flow_unet/mask_5_2_mae_block1_45678l_M_to_C/step12.py
+++ step10_7_flow_unet/mask_5_2_mae_block1_45678l_M_to_C/step12.py
@@ -3,18 +3,28 @@
 '''
 if(__name__ == "__main__"):
     ##########################################################################################################################################################################################################################################################################################
-    ### 把 current_dir 轉回到 kong_model 裡面
+    ### 把 kong_model2 加入 sys.path
     import os
-    import sys
-    curr_path = os.getcwd()
-    curr_layer = len(curr_path.split("\\")) - 1                ### 看 目前執行python的位置在哪一層， -1 是 因為 為了配合下面.index() 從0開始算
-    kong_layer = curr_path.split("\\").index("kong_model2")    ### 看kong_model2 在哪一層
-    back_to_kong_layer_amount = curr_layer - kong_layer        ### 看 目前執行python的位置在哪一層 到 kong_model2 差幾層
-    for _ in range(back_to_kong_layer_amount): os.chdir("..")  ### 看差幾層 往前跳 幾次dir
-    sys.path.append(".")                                         ### 把 kong_model2 加進 sys.path
+    code_exe_path = os.path.realpath(__file__)                       ### 目前執行 step10_b.py 的 path
+    code_exe_path_element = code_exe_path.split("\\")                ### 把 path 切分 等等 要找出 kong_model 在第幾層
+    kong_layer = code_exe_path_element.index("kong_model2") + 1      ### 找出 kong_model2 在第幾層
+    kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer])  ### 定位出 kong_model2 的 dir
+    import sys                                                       ### 把 kong_model2 加入 sys.path
+    sys.path.append(kong_model2_dir)
+    # print(__file__.split("\\")[-1])
+    # print("    code_exe_path:", code_exe_path)
+    # print("    code_exe_path_element:", code_exe_path_element)
+    # print("    kong_layer:", kong_layer)
+    # print("    kong_model2_dir:", kong_model2_dir)
+    ######################################################################################################################
+    ### 按F5執行時， 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～ 才可 import step10_a.py 喔！
+    code_exe_dir = os.path.dirname(code_exe_path)   ### 目前執行 step10_b.py 的 dir
+    if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～
+        os.chdir(code_exe_dir)
+    # print("current_path:", os.getcwd())
     ##########################################################################################################################################################################################################################################################################################
     from step12_result_analyzer import Col_results_analyzer, Row_col_results_analyzer, Bm_Rec_exps_analyze
-    from step11c_2c_mae_block1_45678l_M_to_C import  *
+    from step11 import  *
     ##########################################################################################################################################################################################################################################################################################
     flow_ana_dir = "flow/M_to_C_with_Mgt_to_F"
     """
