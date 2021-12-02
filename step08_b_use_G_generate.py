dc0fc74cf29b260d2403a9f8b0e922a1498d1bb1
KongBoy <s89334roy@yahoo.com.tw>
2021-12-02 17:21:01 +0800

    step8b_use_G 把 I_to_F 整理一下

--- step08_b_use_G_generate.py
+++ step08_b_use_G_generate.py
@@ -73,18 +73,18 @@ def flow_or_coord_visual_op(data):
 ######################################################################################################################################################################################################
 ######################################################################################################################################################################################################
 def I_Generate_F(model_G, _1, in_img_pre, _3, _4, use_gt_range, training=False):  ### training 這個參數是為了 一開使 用BN ，為了那些exp 還能重現所以才保留，現在用 IN 完全不會使用到他這樣子拉～
-    flow      = model_G(in_img_pre, training=training)
+    flow = model_G(in_img_pre, training=training)
     # print("flow before max, min:", flow.numpy().max(), flow.numpy().min())  ### 測試 拉range 有沒有拉對
     if(use_gt_range == Range(-1, 1)): flow = (flow + 1) / 2
     # print("flow after max, min:", flow.numpy().max(), flow.numpy().min())  ### 測試 拉range 有沒有拉對
+    flow = flow[0].numpy()
     return flow
 
 def I_Generate_F_see(model_G, see_index, in_img, in_img_pre, gt_flow, _4, rec_hope, epoch=0, exp_obj=None, training=True, see_reset_init=True):
-    flow           = I_Generate_F(model_G, None, in_img_pre, None, None, exp_obj.use_gt_range, training=training)
-    flow           = flow[0]
-    gt_flow        = gt_flow[0]
-    # print("flow.shape~~~~~~~~~~~", flow.shape)
-    # print("gt_flow.shape~~~~~~~~~~~", gt_flow.shape)
+    in_img   = in_img  [0].numpy()[:, :, ::-1]
+    flow     = I_Generate_F(model_G, None, in_img_pre, None, None, exp_obj.use_gt_range, training=training)
+    gt_flow  = gt_flow [0].numpy()
+    rec_hope = rec_hope[0].numpy()[:, :, ::-1]
 
     flow_visual    = flow_or_coord_visual_op(flow)   .astype(np.uint8)
     gt_flow_visual = flow_or_coord_visual_op(gt_flow).astype(np.uint8)
@@ -93,11 +93,11 @@ def I_Generate_F_see(model_G, see_index, in_img, in_img_pre, gt_flow, _4, rec_ho
 
     if(epoch == 0 or see_reset_init):  ### 第一次執行的時候，建立資料夾 和 寫一些 進去資料夾比較好看的東西
         Check_dir_exist_and_build(see_write_dir)   ### 建立 see資料夾
-        cv2.imwrite(see_write_dir + "/" + "0a-in_img.jpg", in_img[0][:, :, ::-1].numpy())   ### 寫一張 in圖進去，進去資料夾時比較好看，0a是為了保證自動排序會放在第一張
-        cv2.imwrite(see_write_dir + "/" + "0b-gt_a_gt_flow.jpg", gt_flow_visual)  ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
-        cv2.imwrite(see_write_dir + "/" + "0c-rec_hope.jpg", rec_hope[0][:, :, ::-1].numpy())  ### 寫一張 rec_hope圖進去，hope 我 rec可以做到這麼好ˊ口ˋ，0c是為了保證自動排序會放在第三張
-        np.save(see_write_dir + "/" + "0b-gt_a_gt_flow", gt_flow)  ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
-    np.save(    see_write_dir + "/" + "epoch_%04i_a_flow"            % epoch, flow)      ### 我覺得不可以直接存npy，因為太大了！但最後為了省麻煩還是存了，相對就減少see的數量來讓總大小變小囉～
+        cv2.imwrite(see_write_dir + "/" + "0a-in_img.jpg",       in_img)                    ### 寫一張 in圖進去，進去資料夾時比較好看，0a是為了保證自動排序會放在第一張
+        cv2.imwrite(see_write_dir + "/" + "0b-gt_a_gt_flow.jpg", gt_flow_visual)            ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
+        cv2.imwrite(see_write_dir + "/" + "0c-rec_hope.jpg",     rec_hope)                  ### 寫一張 rec_hope圖進去，hope 我 rec可以做到這麼好ˊ口ˋ，0c是為了保證自動排序會放在第三張
+        np.save(see_write_dir + "/" + "0b-gt_a_gt_flow",         gt_flow)                   ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
+    np.save(    see_write_dir + "/" + "epoch_%04i_a_flow"            % epoch, flow)         ### 我覺得不可以直接存npy，因為太大了！但最後為了省麻煩還是存了，相對就減少see的數量來讓總大小變小囉～
     cv2.imwrite(see_write_dir + "/" + "epoch_%04i_a_flow_visual.jpg" % epoch, flow_visual)  ### 把 生成的 flow_visual 存進相對應的資料夾
     # cv2.imwrite(see_write_dir + "/" + "epoch_%04i_b_in_rec_img.jpg" % epoch     , in_rec_img)  ### 把 生成影像存進相對應的資料夾，因為 tf訓練時是rgb，生成也是rgb，所以用cv2操作要轉bgr存才對！
 
