0495d7daee2dc783f19ef6e396f9f85b4b1ef4e7
KongBoy <s89334roy@yahoo.com.tw>
2021-12-03 11:05:06 +0800

    step8b_use_G 整理 I_to_R 應該可以吧沒測

--- step08_b_use_G_generate.py
+++ step08_b_use_G_generate.py
@@ -12,16 +12,19 @@ from step06_a_datas_obj import Range
 
 ### 用 網路 生成 影像
 def I_Generate_R(model_G, _1, in_img_pre, _3, _4, use_gt_range):
-    rect       = model_G(in_img_pre, training=True)  ### 把影像丟進去model生成還原影像
+    rect = model_G(in_img_pre, training=True)  ### 把影像丟進去model生成還原影像
+    rect = rect[0].numpy()[:, :, ::-1]
     # print("rect_back before max, min:", rect_back.numpy().max(), rect_back.numpy().min())  ### 測試 拉range 有沒有拉對
-    if  (use_gt_range == Range(-1, 1)): rect_back  = ((rect[0].numpy() + 1) * 125).astype(np.uint8)   ### 把值從 -1~1轉回0~255 且 dtype轉回np.uint8
-    elif(use_gt_range == Range( 0, 1)): rect_back  = ( rect[0].numpy() * 255).astype(np.uint8)   ### 把值從 -1~1轉回0~255 且 dtype轉回np.uint8
+    if  (use_gt_range == Range(-1, 1)): rect_back  = ((rect + 1) * 125).astype(np.uint8)   ### 把值從 -1~1轉回0~255 且 dtype轉回np.uint8
+    elif(use_gt_range == Range( 0, 1)): rect_back  = ( rect * 255).astype(np.uint8)   ### 把值從 -1~1轉回0~255 且 dtype轉回np.uint8
     # print("rect_back after max, min:", rect_back.numpy().max(), rect_back.numpy().min())  ### 測試 拉range 有沒有拉對
     return rect_back  ### 注意訓練model時是用tf來讀img，為rgb的方式訓練，所以生成的是rgb的圖喔！
 
 
 ### 這是一張一張進來的，沒有辦法跟 Result 裡面的 see 生成法合併，要的話就是把這裡matplot部分去除，用result裡的see生成matplot圖囉！
 def I_Generate_R_see(model_G, see_index, in_img, in_img_pre, gt_img, _4, rec_hope, epoch=0, exp_obj=None, see_reset_init=False):
+    in_img = in_img[0].numpy()
+    gt_img = gt_img[0].numpy()
     rect_back = I_Generate_R(model_G, None, in_img_pre, None, None, exp_obj.use_gt_range)
 
     see_write_dir  = exp_obj.result_obj.sees[see_index].see_write_dir  ### 每個 see 都有自己的資料夾 存 model生成的結果，先定出位置
@@ -29,10 +32,10 @@ def I_Generate_R_see(model_G, see_index, in_img, in_img_pre, gt_img, _4, rec_hop
 
     if(epoch == 0 or see_reset_init):  ### 第一次執行的時候，建立資料夾 和 寫一些 進去資料夾比較好看的東西
         Check_dir_exist_and_build(see_write_dir)   ### 建立 see資料夾
-        Check_dir_exist_and_build(plot_dir)  ### 建立 see資料夾/matplot_visual資料夾
-        cv2.imwrite(see_write_dir + "/" + "0a-in_img.jpg", in_img[0].numpy())   ### 寫一張 in圖進去，進去資料夾時比較好看，0a是為了保證自動排序會放在第一張
-        cv2.imwrite(see_write_dir + "/" + "0b-gt_img.jpg", gt_img[0].numpy())  ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
-    cv2.imwrite(see_write_dir + "/" + "epoch_%04i.jpg" % epoch, rect_back[:, :, ::-1])  ### 把 生成影像存進相對應的資料夾，因為 tf訓練時是rgb，生成也是rgb，所以用cv2操作要轉bgr存才對！
+        Check_dir_exist_and_build(plot_dir)        ### 建立 see資料夾/matplot_visual資料夾
+        cv2.imwrite(see_write_dir + "/" + "0a-in_img.jpg", in_img)          ### 寫一張 in圖進去，進去資料夾時比較好看，0a是為了保證自動排序會放在第一張
+        cv2.imwrite(see_write_dir + "/" + "0b-gt_img.jpg", gt_img)          ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
+    cv2.imwrite(see_write_dir + "/" + "epoch_%04i.jpg" % epoch, rect_back)  ### 把 生成影像存進相對應的資料夾，因為 tf訓練時是rgb，生成也是rgb，所以用cv2操作要轉bgr存才對！
 
     ### matplot_visual的部分，記得因為用 matplot 所以要 bgr轉rgb，但是因為有用matplot_visual_single_row_imgs，裡面會bgr轉rgb了，所以這裡不用轉囉！
     ### 這部分要記得做！在 train_step3 的 exp_obj.result_obj.Draw_loss_during_train(epoch, self.epochs) 才有畫布可以畫loss！
