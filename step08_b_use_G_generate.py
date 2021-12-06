0fd2298977bc8da2c0555df7273d3403e6565c2e
KongBoy <s89334roy@yahoo.com.tw>
2021-12-06 14:18:48 +0800

    step8b I_to_R 和 I_to_W 都還沒有測試 test_see 就先把 generate_see commit 進來了， 因為想有空再測試這樣子

--- step08_b_use_G_generate.py
+++ step08_b_use_G_generate.py
@@ -13,9 +13,15 @@ from matplot_fig_ax_util import Matplot_single_row_imgs
 import matplotlib.pyplot as plt
 
 ### 用 網路 生成 影像
-def I_Generate_R(model_G, _1, in_img_pre, _3, _4, use_gt_range):
+def I_Generate_R(model_G, _1, in_img_pre, _3, _4, use_gt_range, bgr2rgb=False):
+    '''
+    bgr2rgb： tf2 讀出來是 rgb， 但 cv2 存圖是bgr， 所以此狀況記得要轉一下ch 把 bgr2rgb設True！
+                                但 plt 存圖是rgb， 所以存圖不用轉ch， 把 bgr2rgb設False喔！
+    '''
     rect = model_G(in_img_pre, training=True)  ### 把影像丟進去model生成還原影像
-    rect = rect[0].numpy()[:, :, ::-1]
+    rect = rect[0].numpy()
+    if(bgr2rgb): rect = rect[:, :, ::-1]  ### tf2 讀出來是 rgb， 但cv2存圖是bgr， 所以記得要轉一下ch
+
     # print("rect_back before max, min:", rect_back.numpy().max(), rect_back.numpy().min())  ### 測試 拉range 有沒有拉對
     if  (use_gt_range == Range(-1, 1)): rect_back  = ((rect + 1) * 125).astype(np.uint8)   ### 把值從 -1~1轉回0~255 且 dtype轉回np.uint8
     elif(use_gt_range == Range( 0, 1)): rect_back  = ( rect * 255).astype(np.uint8)   ### 把值從 -1~1轉回0~255 且 dtype轉回np.uint8
@@ -24,7 +30,10 @@ def I_Generate_R(model_G, _1, in_img_pre, _3, _4, use_gt_range):
 
 
 ### 這是一張一張進來的，沒有辦法跟 Result 裡面的 see 生成法合併，要的話就是把這裡matplot部分去除，用result裡的see生成matplot圖囉！
-def I_Generate_R_see(model_G, see_index, in_img, in_img_pre, gt_img, _4, rec_hope, epoch=0, exp_obj=None, see_reset_init=False):
+def I_Generate_R_see(model_G, see_index, in_img, in_img_pre, gt_img, _4, rec_hope, epoch=0, exp_obj=None, see_reset_init=False, bgr2rgb=True):
+    '''
+    bgr2rgb： tf2 讀出來是 rgb， 但 cv2 存圖是bgr， 所以此狀況記得要轉一下ch 把 bgr2rgb設True！
+    '''
     in_img = in_img[0].numpy()
     gt_img = gt_img[0].numpy()
     rect_back = I_Generate_R(model_G, None, in_img_pre, None, None, exp_obj.use_gt_range)
@@ -329,15 +338,22 @@ def I_Generate_W(model_G, _1, in_img_pre, _3, _4, use_gt_range, training=False):
     wc = wc[0].numpy()
     return wc
 
-def I_Generate_W_see(model_G, see_index, in_img, in_img_pre, gt_wc, _4, rec_hope=None, epoch=0, exp_obj=None, training=True, see_reset_init=True):
+def I_Generate_W_see(model_G, see_index, in_img, in_img_pre, gt_wc, _4, rec_hope=None, epoch=0, exp_obj=None, training=True, see_reset_init=True, bgr2rgb=True):
     '''
     gt_mask_coord[0] 為 mask  (1, h, w, 1)
     gt_mask_coord[1] 為 coord (1, h, w, 2) 先y 在x
+
+    bgr2rgb： tf2 讀出來是 rgb， 但 cv2 存圖是bgr， 所以此狀況記得要轉一下ch 把 bgr2rgb設True！
     '''
     # plt.imshow(in_img[0])
     # plt.show()
-    wc    = I_Generate_W(model_G, None, in_img_pre, None, None, exp_obj.use_gt_range, training=training)
+    in_img   = in_img  [0].numpy()
+    rec_hope = rec_hope[0].numpy()
+    if(bgr2rgb):
+        in_img = in_img[:, :, ::-1]
+        rec_hope = rec_hope[:, :, ::-1]
 
+    wc    = I_Generate_W(model_G, None, in_img_pre, None, None, exp_obj.use_gt_range, training=training)
     gt_wc = gt_wc[0].numpy()
     gt_wc = gt_wc[..., :3]
 
@@ -351,12 +367,12 @@ def I_Generate_W_see(model_G, see_index, in_img, in_img_pre, gt_wc, _4, rec_hope
     see_write_dir   = exp_obj.result_obj.sees[see_index].see_write_dir   ### 每個 see 都有自己的資料夾 存 in/gt 之類的 輔助檔案 ，先定出位置
     if(epoch == 0 or see_reset_init):  ### 第一次執行的時候，建立資料夾 和 寫一些 進去資料夾比較好看的東西
         Check_dir_exist_and_build(see_write_dir)    ### 建立 放輔助檔案 的資料夾
-        cv2.imwrite(see_write_dir + "/" + "0a-in_img.jpg",       in_img[0][:, :, ::-1].numpy())             ### 寫一張 in圖進去，進去資料夾時比較好看，0a是為了保證自動排序會放在第一張
+        cv2.imwrite(see_write_dir + "/" + "0a-in_img.jpg",       in_img)             ### 寫一張 in圖進去，進去資料夾時比較好看，0a是為了保證自動排序會放在第一張
         # cv2.imwrite(see_write_dir + "/" + "0b-gt_a_gt_mask.jpg", (gt_mask.numpy() * 255).astype(np.uint8))  ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
         # np.save    (see_write_dir + "/" + "0b-gt_a_gt_mask",     gt_mask)                                   ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
         cv2.imwrite(see_write_dir + "/" + "0b-gt_b_gt_wc.jpg", gt_wc_visual)                            ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
         np.save    (see_write_dir + "/" + "0b-gt_b_gt_wc",     gt_wc)                                   ### 寫一張 gt圖進去，進去資料夾時比較好看，0b是為了保證自動排序會放在第二張
-        cv2.imwrite(see_write_dir + "/" + "0c-rec_hope.jpg",   rec_hope[0][:, :, ::-1].numpy())           ### 寫一張 rec_hope圖進去，hope 我 rec可以做到這麼好ˊ口ˋ，0c是為了保證自動排序會放在第三張
+        cv2.imwrite(see_write_dir + "/" + "0c-rec_hope.jpg",   rec_hope)           ### 寫一張 rec_hope圖進去，hope 我 rec可以做到這麼好ˊ口ˋ，0c是為了保證自動排序會放在第三張
     np.save(    see_write_dir + "/" + "epoch_%04i_a_wc"            % epoch, wc)                         ### 我覺得不可以直接存npy，因為太大了！但最後為了省麻煩還是存了，相對就減少see的數量來讓總大小變小囉～
     cv2.imwrite(see_write_dir + "/" + "epoch_%04i_a_wc_visual.jpg" % epoch, wc_visual)                  ### 把 生成的 flow_visual 存進相對應的資料夾
 ######################################################################################################################################################################################################
