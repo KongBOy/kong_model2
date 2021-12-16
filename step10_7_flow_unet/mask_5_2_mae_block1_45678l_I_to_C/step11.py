db4f8823291d27c95d78dcd8769ccc97502a6539
KongBoy <s89334roy@yahoo.com.tw>
2021-12-16 14:11:13 +0800

    I_to_C 和 I_to_F 整理

--- step10_7_flow_unet/mask_5_2_mae_block1_45678l_I_to_C/step11.py
+++ step10_7_flow_unet/mask_5_2_mae_block1_45678l_I_to_C/step11.py
@@ -20,70 +20,66 @@ if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py
 # print("current_path:", os.getcwd())
 ###############################################################################################################################################################################################################
 
-import step10_a as mae_block1
+import a_normal.step10_a as block1_mae_s001_L2345678
 
 #################################################################################################################################################################################################################################################################################################################################################################################################
 ### L2345678_flow
 mae_block1_flow_s001_L345678 = [
 ### L3 之前直接跳過 因為發現幾乎無法 rec #######################################################################
+    [block1_mae_s001_L2345678.L3_ch128_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch064_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch032_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch016_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch008_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch004_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch002_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L3_ch001_mae_s001.build().result_obj],
 
-    [mae_block1.L3_ch128_mae_s001.build().result_obj,
-     mae_block1.L3_ch064_mae_s001.build().result_obj,
-     mae_block1.L3_ch032_mae_s001.build().result_obj,
-     mae_block1.L3_ch016_mae_s001.build().result_obj,
-     mae_block1.L3_ch008_mae_s001.build().result_obj,
-     mae_block1.L3_ch004_mae_s001.build().result_obj,
-     mae_block1.L3_ch002_mae_s001.build().result_obj,
-     mae_block1.L3_ch001_mae_s001.build().result_obj],
+    [block1_mae_s001_L2345678.L4_ch128_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch064_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch032_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch016_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch008_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch004_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch002_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L4_ch001_mae_s001.build().result_obj],
+
+    [block1_mae_s001_L2345678.L5_ch128_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch064_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch032_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch016_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch008_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch004_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch002_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L5_ch001_mae_s001.build().result_obj],
+
+    [block1_mae_s001_L2345678.L6_ch128_mae_s001_limit.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch064_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch032_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch016_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch008_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch004_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch002_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L6_ch001_mae_s001.build().result_obj],
 
 
-    [mae_block1.L4_ch064_mae_s001.build().result_obj,
-     mae_block1.L4_ch032_mae_s001.build().result_obj,
-     mae_block1.L4_ch016_mae_s001.build().result_obj,
-     mae_block1.L4_ch008_mae_s001.build().result_obj,
-     mae_block1.L4_ch004_mae_s001.build().result_obj,
-     mae_block1.L4_ch002_mae_s001.build().result_obj,
-     mae_block1.L4_ch001_mae_s001.build().result_obj],
+    [block1_mae_s001_L2345678.L7_ch128_mae_s001_limit.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch064_mae_s001_limit.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch032_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch016_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch008_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch004_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch002_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L7_ch001_mae_s001.build().result_obj],
 
-    [mae_block1.L5_ch128_mae_s001.build().result_obj,
-     mae_block1.L5_ch064_mae_s001.build().result_obj,
-     mae_block1.L5_ch032_mae_s001.build().result_obj,
-     mae_block1.L5_ch016_mae_s001.build().result_obj,
-     mae_block1.L5_ch008_mae_s001.build().result_obj,
-     mae_block1.L5_ch004_mae_s001.build().result_obj,
-     mae_block1.L5_ch002_mae_s001.build().result_obj,
-     mae_block1.L5_ch001_mae_s001.build().result_obj],
-
-    [
-    #  mae_block1.L6_ch128_mae_s001.build().result_obj,
-     mae_block1.L6_ch064_mae_s001.build().result_obj,
-     mae_block1.L6_ch032_mae_s001.build().result_obj,
-     mae_block1.L6_ch016_mae_s001.build().result_obj,
-     mae_block1.L6_ch008_mae_s001.build().result_obj,
-     mae_block1.L6_ch004_mae_s001.build().result_obj,
-     mae_block1.L6_ch002_mae_s001.build().result_obj,
-     mae_block1.L6_ch001_mae_s001.build().result_obj],
-
-    [
-    #  mae_block1.L7_ch128_mae_s001.build().result_obj,
-    #  mae_block1.L7_ch064_mae_s001.build().result_obj,
-     mae_block1.L7_ch032_mae_s001.build().result_obj,
-     mae_block1.L7_ch016_mae_s001.build().result_obj,
-     mae_block1.L7_ch008_mae_s001.build().result_obj,
-     mae_block1.L7_ch004_mae_s001.build().result_obj,
-     mae_block1.L7_ch002_mae_s001.build().result_obj,
-     mae_block1.L7_ch001_mae_s001.build().result_obj],
-
-    [
-    #  mae_block1.L8_ch128_mae_s001.build().result_obj,
-    #  mae_block1.L8_ch064_mae_s001.build().result_obj,
-    #  mae_block1.L8_ch032_mae_s001.build().result_obj,
-     mae_block1.L8_ch016_mae_s001.build().result_obj,
-     mae_block1.L8_ch008_mae_s001.build().result_obj,
-     mae_block1.L8_ch004_mae_s001.build().result_obj,
-     mae_block1.L8_ch002_mae_s001.build().result_obj,
-     mae_block1.L8_ch001_mae_s001.build().result_obj],
-
+    [block1_mae_s001_L2345678.L8_ch128_mae_s001_limit.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch064_mae_s001_limit.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch032_mae_s001_limit.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch016_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch008_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch004_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch002_mae_s001.build().result_obj,
+     block1_mae_s001_L2345678.L8_ch001_mae_s001.build().result_obj]
 ]
 
 
@@ -158,5 +154,4 @@ mae_block1_rec_s001_L45678 = [
         mae_block1.L8_ch002_mae_s001.build().result_obj,
         # mae_block1.L8_ch001_mae_s001.build().result_obj,  ### 做不起來
     ],
-
 ]
---
+++ step10_7_flow_unet/mask_5_2_mae_block1_45678l_I_to_F/b_limit/step10_a.py
@@ -0,0 +1,74 @@
+#############################################################################################################################################################################################################
+### 把 kong_model2 加入 sys.path
+import os
+code_exe_path = os.path.realpath(__file__)                   ### 目前執行 step10_b.py 的 path
+code_exe_path_element = code_exe_path.split("\\")            ### 把 path 切分 等等 要找出 kong_model 在第幾層
+kong_layer = code_exe_path_element.index("kong_model2")      ### 找出 kong_model2 在第幾層
+kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer + 1])    ### 定位出 kong_model2 的 dir
+import sys                                                   ### 把 kong_model2 加入 sys.path
+sys.path.append(kong_model2_dir)
+# print(__file__.split("\\")[-1])
+# print("    code_exe_path:", code_exe_path)
+# print("    code_exe_path_element:", code_exe_path_element)
+# print("    kong_layer:", kong_layer)
+# print("    kong_model2_dir:", kong_model2_dir)
+#############################################################################################################################################################################################################
+kong_to_py_layer = len(code_exe_path_element) - 1 - kong_layer
+# print("    kong_to_py_layer:", kong_to_py_layer)
+if  (kong_to_py_layer == 2): template_dir = code_exe_path_element[kong_layer + 1][7:]  ### [7:] 是為了去掉 step1x_
+elif(kong_to_py_layer == 3): template_dir = code_exe_path_element[kong_layer + 1][7:] + "/" + code_exe_path_element[kong_layer + 2][5:]  ### [5:] 是為了去掉 mask_ ，前面的 mask_ 是為了python 的 module 不能 數字開頭， 隨便加的這樣子
+elif(kong_to_py_layer >  3): template_dir = code_exe_path_element[kong_layer + 1][7:] + "/" + code_exe_path_element[kong_layer + 2][5:] + "/" + "/".join(code_exe_path_element[kong_layer + 3: -1])  ### 前面的 mask_ 是為了python 的 module 不能 數字開頭， 隨便加的這樣子
+# print("    template_dir:", template_dir)  ### 舉例： template_dir: 7_mask_unet/5_os_book_and_paper_have_dtd_hdr_mix_bg_tv_s04_mae
+#############################################################################################################################################################################################################
+exp_dir = template_dir
+#############################################################################################################################################################################################################
+from step06_a_datas_obj import *
+from step09_e3_flow_unet2_obj_I_to_C import *
+from step09_b_loss_info_obj import *
+from step10_b_exp_builder import Exp_builder
+#############################################################################################################################################################################################################
+'''
+exp_dir 是 決定 result_dir 的 "上一層"資料夾 名字喔！ exp_dir要巢狀也沒問題～
+比如：exp_dir = "7_flow_unet2_block1/自己命的名字"，那 result_dir 就都在：
+    7_flow_unet2_block1/自己命的名字/result_a
+    7_flow_unet2_block1/自己命的名字/result_b
+    7_flow_unet2_block1/自己命的名字/...
+'''
+
+use_db_obj = type9_mask_flow_have_bg_dtd_hdr_mix_and_paper
+#################################################################################################################################################################################################################################################################################################################################################################################################
+#################################################################################################################################################################################################################################################################################################################################################################################################
+L4_ch128_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch128_sig_L4_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L4_ch128_limit", describe_end="block1_L4_ch128_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L4_ch128_limit-flow_unet2-block1_L4_ch128_mae_s001_limit-20211206_185506")
+#################################################################################################################################################################################################################################################################################################################################################################################################
+L5_ch128_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch128_sig_L5_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L5_ch128_limit", describe_end="block1_L5_ch128_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L5_ch128_limit-flow_unet2-block1_L5_ch128_mae_s001_limit-20211206_202813")
+L5_ch064_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch064_sig_L5_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L5_ch064_limit", describe_end="block1_L5_ch064_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L5_ch064_limit-flow_unet2-block1_L5_ch064_mae_s001_limit-20211206_162744")
+#################################################################################################################################################################################################################################################################################################################################################################################################
+L6_ch128_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch128_sig_L6_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L6_ch128_limit", describe_end="block1_L6_ch128_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L6_ch128_limit-flow_unet2-block1_L6_ch128_limit_mae_s001-20211206_171010")
+L6_ch064_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch064_sig_L6_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L6_ch064_limit", describe_end="block1_L6_ch064_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L6_ch064_limit-flow_unet2-block1_L6_ch064_limit_mae_s001-20211207_000943")
+L6_ch032_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch032_sig_L6_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L6_ch032_limit", describe_end="block1_L6_ch032_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L6_ch032_limit-flow_unet2-block1_L6_ch032_limit_mae_s001-20211207_013740")
+#################################################################################################################################################################################################################################################################################################################################################################################################
+L7_ch128_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch128_sig_L7_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L7_ch128_limit", describe_end="block1_L7_ch128_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L7_ch128_limit-flow_unet2-block1_L7_ch128_mae_s001_limit-20211206_232407")
+L7_ch064_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch064_sig_L7_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L7_ch064_limit", describe_end="block1_L7_ch064_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L7_ch064_limit-flow_unet2-block1_L7_ch064_mae_s001_limit-20211207_024308")
+L7_ch032_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch032_sig_L7_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L7_ch032_limit", describe_end="block1_L7_ch032_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L7_ch032_limit-flow_unet2-block1_L7_ch032_mae_s001_limit-20211207_021706")
+L7_ch016_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch016_sig_L7_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L7_ch016_limit", describe_end="block1_L7_ch016_mae_s001_limit") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L7_ch016_limit-flow_unet2-block1_L7_ch016_mae_s001_limit-20211207_043639")
+#################################################################################################################################################################################################################################################################################################################################################################################################
+L8_ch128_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch128_sig_L8_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L8_ch128_limit", describe_end="block1_L8_ch128_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L8_ch128_limit-flow_unet2-block1_L8_ch128_limit_mae_s001-20211206_174943")
+L8_ch064_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch064_sig_L8_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L8_ch064_limit", describe_end="block1_L8_ch064_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L8_ch064_limit-flow_unet2-block1_L8_ch064_limit_mae_s001-20211206_215704")
+L8_ch032_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch032_sig_L8_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L8_ch032_limit", describe_end="block1_L8_ch032_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L8_ch032_limit-flow_unet2-block1_L8_ch032_limit_mae_s001-20211206_235237")
+L8_ch016_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch016_sig_L8_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L8_ch016_limit", describe_end="block1_L8_ch016_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L8_ch016_limit-flow_unet2-block1_L8_ch016_limit_mae_s001-20211207_035517")
+L8_ch008_mae_s001_limit = Exp_builder().set_basic("train", use_db_obj, flow_unet2_block1_ch008_sig_L8_limit, G_mae_s001_loss_info_builder, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_mid="2_L8_ch008_limit", describe_end="block1_L8_ch008_limit_mae_s001") .set_train_args(epochs= 60, exp_bn_see_arg=None).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="type8_blender-2_L8_ch008_limit-flow_unet2-block1_L8_ch008_limit_mae_s001-20211207_052703")
+#################################################################################################################################################################################################################################################################################################################################################################################################
+#################################################################################################################################################################################################################################################################################################################################################################################################
+if(__name__ == "__main__"):
+    print("build exps cost time:", time.time() - start_time)
+    if len(sys.argv) < 2:
+        ############################################################################################################
+        ### 直接按 F5 或打 python step10_a_load_and_train_and_test.py，後面沒有接東西喔！才不會跑到下面給 step10_b_subprocss.py 用的程式碼~~~
+        L5_ch064_mae_s001_copy.build().run()
+        # L3_ch128_mae_s001_copy.build().run()
+
+        # print('no argument')
+        sys.exit()
+
+    ### 以下是給 step10_b_subprocess.py 用的，相當於cmd打 python step10_a_load_and_train_and_test.py 某個exp.build().run()
+    eval(sys.argv[1])
