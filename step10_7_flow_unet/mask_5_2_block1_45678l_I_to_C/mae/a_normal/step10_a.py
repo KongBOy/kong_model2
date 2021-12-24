e2d77877247150132f09cb377d8a1c8ec4721f33
KongBoy <s89334roy@yahoo.com.tw>
2021-12-24 14:42:08 +0800

    I_to_C 小修

--- step10_7_flow_unet/mask_5_2_block1_45678l_I_to_C/mae/a_normal/step10_a.py
+++ step10_7_flow_unet/mask_5_2_block1_45678l_I_to_C/mae/a_normal/step10_a.py
@@ -104,7 +104,7 @@ if(__name__ == "__main__"):
     if len(sys.argv) < 2:
         ############################################################################################################
         ### 直接按 F5 或打 python step10_a_load_and_train_and_test.py，後面沒有接東西喔！才不會跑到下面給 step10_b_subprocss.py 用的程式碼~~~
-        # L2_ch001_s001.build().run()
+        # L2_ch004.build().run()
 
         # print('no argument')
         sys.exit()
