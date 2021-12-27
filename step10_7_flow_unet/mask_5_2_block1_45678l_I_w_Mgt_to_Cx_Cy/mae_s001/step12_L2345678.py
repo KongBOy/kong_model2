b20ca5d085dd9fdd9c8ebf5a7620f0e9ced3577b
KongBoy <s89334roy@yahoo.com.tw>
2021-12-27 16:29:00 +0800

    做一些 analyze

--- step10_7_flow_unet/mask_5_2_block1_45678l_I_w_Mgt_to_Cx_Cy/mae_s001/step12_L2345678.py
+++ step10_7_flow_unet/mask_5_2_block1_45678l_I_w_Mgt_to_Cx_Cy/mae_s001/step12_L2345678.py
@@ -18,7 +18,7 @@ if(__name__ == "__main__"):
     # print("    kong_model2_dir:", kong_model2_dir)
     #############################################################################################################################################################################################################
     from step12_result_analyzer import Row_col_results_analyzer
-    from step11 import  *
+    from step11_L2345678 import  *
     #############################################################################################################################################################################################################
     kong_to_py_layer = len(code_exe_path_element) - 1 - kong_layer
     if  (kong_to_py_layer == 2): template_dir = code_exe_path_element[kong_layer + 1][7:]  ### [7:] 是為了去掉 step1x_
@@ -40,16 +40,28 @@ if(__name__ == "__main__"):
     """
     #################################################################################################################################################################################################################
     #################################################################################################################################################################################################################
-    ana_name = "Layer_Ch"
+    ana_name = "L2345678_Ch"
     analyzer = Row_col_results_analyzer(ana_describe=f"{ana_dir}/0_ana_{ana_name}",
-                                        ana_what="mask",
+                                        ana_what="flow",
+                                        row_col_results=block1_mask_L2345678, show_in_img=False, show_gt_img=False, bgr2rgb=True, add_loss=False, img_h=512, img_w=512)\
+                                            # .analyze_row_col_results_all_single_see(single_see_multiprocess=True, single_see_core_amount=6)\
+                                            # .Gather_all_see_final_img()
+
+    analyzer = Row_col_results_analyzer(ana_describe=f"{ana_dir}/0_ana_{ana_name}",
+                                        ana_what="rec",
                                         row_col_results=block1_mask_L2345678, show_in_img=False, show_gt_img=False, bgr2rgb=True, add_loss=False, img_h=512, img_w=512)\
                                             # .analyze_row_col_results_all_single_see(single_see_multiprocess=True, single_see_core_amount=6)\
                                             # .Gather_all_see_final_img()
     ############################################################################
     ana_name = "Normal_vs_Limit"
     analyzer = Row_col_results_analyzer(ana_describe=f"{ana_dir}/0_ana_{ana_name}",
-                                        ana_what="mask",
+                                        ana_what="flow",
                                         row_col_results=block1_mask_L45678_normal_vs_limit, show_in_img=False, show_gt_img=False, bgr2rgb=True, add_loss=False, img_h=512, img_w=512)\
                                             # .analyze_row_col_results_all_single_see(single_see_multiprocess=True, single_see_core_amount=6)\
                                             # .Gather_all_see_final_img()
+
+    analyzer = Row_col_results_analyzer(ana_describe=f"{ana_dir}/0_ana_{ana_name}",
+                                        ana_what="rec",
+                                        row_col_results=block1_mask_L45678_normal_vs_limit, show_in_img=False, show_gt_img=False, bgr2rgb=True, add_loss=False, img_h=512, img_w=512)\
+                                            .analyze_row_col_results_all_single_see(single_see_multiprocess=True, single_see_core_amount=6)\
+                                            .Gather_all_see_final_img()
