6bd64584ccdfdf8e8767cedc8cfb19714291bc2f
KongBoy <s89334roy@yahoo.com.tw>
2022-01-13 10:40:37 +0800

    加入 z_RunRunRun

---
+++ z_RunRunRun.py
@@ -0,0 +1,5 @@
+import subprocess as sb
+
+sb.run( ["python", "F:/kong_model2/step10_7_flow_unet/mask_5_2_block1_45678l_I_w_Mgt_to_Cx_Cy/mae_s001/step12_L2345678.py"] )
+sb.run( ["python", "F:/kong_model2/step10_7_flow_unet/mask_5_2_block1_45678l_I_w_Mgt_to_Cx_Cy_bottle_div/mae_s001/step12_L2345678.py"] )
+sb.run( ["python", "F:/kong_model2/step10_8_multi_unet/comb_multi_try/I_w_Mgt_to_Cx_Cy/step12.py"] )
