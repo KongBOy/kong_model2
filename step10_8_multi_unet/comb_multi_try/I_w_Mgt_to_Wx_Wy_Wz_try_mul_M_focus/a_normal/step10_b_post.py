81f5847eb315de380a723bd1e6db9b2819607e2d
KongBoy <s89334roy@yahoo.com.tw>
2022-01-15 20:15:26 +0800

    I_w_Mgt_to_Wx_Wy_Wz (3UNet) 127.29 L6 compress_all

--- step10_8_multi_unet/comb_multi_try/I_w_Mgt_to_Wx_Wy_Wz_try_mul_M_focus/a_normal/step10_b_post.py
+++ step10_8_multi_unet/comb_multi_try/I_w_Mgt_to_Wx_Wy_Wz_try_mul_M_focus/a_normal/step10_b_post.py
@@ -63,12 +63,12 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"I_to_Wx_L5_ch001_and_I_to_Wy_L5_ch001_ep060_and_I_to_Wz_L5_ch001_ep060.{run}"])
 
 # sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch032_and_I_to_Wy_L6_ch032_ep060_and_I_to_Wz_L6_ch032_ep060.{run}"])  ### 超出GPU記憶體，再試一次，確定超出記憶體
-# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch064_and_I_to_Wy_L6_ch064_ep060_and_I_to_Wz_L6_ch064_ep060.{run}"])
-# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch016_and_I_to_Wy_L6_ch016_ep060_and_I_to_Wz_L6_ch016_ep060.{run}"])
-# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch008_and_I_to_Wy_L6_ch008_ep060_and_I_to_Wz_L6_ch008_ep060.{run}"])
-# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch004_and_I_to_Wy_L6_ch004_ep060_and_I_to_Wz_L6_ch004_ep060.{run}"])
-# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch002_and_I_to_Wy_L6_ch002_ep060_and_I_to_Wz_L6_ch002_ep060.{run}"])
-# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch001_and_I_to_Wy_L6_ch001_ep060_and_I_to_Wz_L6_ch001_ep060.{run}"])
+# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch064_and_I_to_Wy_L6_ch064_ep060_and_I_to_Wz_L6_ch064_ep060.{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch016_and_I_to_Wy_L6_ch016_ep060_and_I_to_Wz_L6_ch016_ep060.{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch008_and_I_to_Wy_L6_ch008_ep060_and_I_to_Wz_L6_ch008_ep060.{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch004_and_I_to_Wy_L6_ch004_ep060_and_I_to_Wz_L6_ch004_ep060.{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch002_and_I_to_Wy_L6_ch002_ep060_and_I_to_Wz_L6_ch002_ep060.{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_to_Wx_L6_ch001_and_I_to_Wy_L6_ch001_ep060_and_I_to_Wz_L6_ch001_ep060.{compress_all}"])
 
 # sb.run(cmd_python_step10_a + [f"I_to_Wx_L7_ch032_and_I_to_Wy_L7_ch032_ep060_and_I_to_Wz_L7_ch032_ep060.{run}"])
 # sb.run(cmd_python_step10_a + [f"I_to_Wx_L7_ch016_and_I_to_Wy_L7_ch016_ep060_and_I_to_Wz_L7_ch016_ep060.{run}"])
