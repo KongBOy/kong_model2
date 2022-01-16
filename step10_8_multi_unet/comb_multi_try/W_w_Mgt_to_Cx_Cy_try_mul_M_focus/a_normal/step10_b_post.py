437666078218df5746a79d5171bce871d7010460
KongBoy <s89334roy@yahoo.com.tw>
2022-01-16 12:19:56 +0800

    W_w_Mgt_to_Cx_Cy_focus 127.29 L5 128 compress_and_bm_rec_all

--- step10_8_multi_unet/comb_multi_try/W_w_Mgt_to_Cx_Cy_try_mul_M_focus/a_normal/step10_b_post.py
+++ step10_8_multi_unet/comb_multi_try/W_w_Mgt_to_Cx_Cy_try_mul_M_focus/a_normal/step10_b_post.py
@@ -59,7 +59,7 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L5_ch008.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch004.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch002.{compress_and_bm_rec_all}"])
-# sb.run(cmd_python_step10_a + [f"L5_ch128.{run}"])  ### 超出GPU記憶體，再試一次，確定超出記憶體
+# sb.run(cmd_python_step10_a + [f"L5_ch128.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch001.{compress_and_bm_rec_all}"])
 
 # sb.run(cmd_python_step10_a + [f"L6_ch032.{compress_and_bm_rec_all}"])
