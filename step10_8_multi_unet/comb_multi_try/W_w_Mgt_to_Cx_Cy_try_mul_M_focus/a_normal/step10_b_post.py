689997a3f5bd68c149bbdd7b9331b927cfb3460d
KongBoy <s89334roy@yahoo.com.tw>
2022-01-16 10:30:21 +0800

    W_w_Mgt_to_Cx_Cy_focus (2UNet) 127.28 L7 16~2 compress_and_bm_rec_all

--- step10_8_multi_unet/comb_multi_try/W_w_Mgt_to_Cx_Cy_try_mul_M_focus/a_normal/step10_b_post.py
+++ step10_8_multi_unet/comb_multi_try/W_w_Mgt_to_Cx_Cy_try_mul_M_focus/a_normal/step10_b_post.py
@@ -70,10 +70,10 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L6_ch064.{run}"])  ### 超出GPU記憶體，再試一次，確定超出記憶體
 # sb.run(cmd_python_step10_a + [f"L6_ch001.{run}"])
 
-# sb.run(cmd_python_step10_a + [f"L7_ch016.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch008.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch004.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch002.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch016.{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch008.{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch004.{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch002.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch032.{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch001.{run}"])
 
