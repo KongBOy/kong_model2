36108d778129070b069aeb4579d62a82454bc689
KongBoy <s89334roy@yahoo.com.tw>
2022-01-20 11:23:15 +0800

    I_w_Mgt_to_Wxyz_focus_to_Cxy_focus 一對一 127.28 L5 compress_and_bm_rec_all_from_begin, 128的I_to_Wxyz超出記憶體，所以本實驗根本不存在應該刪掉才對

--- Exps_9_combine/I_w_Mgt_to_Wxyz_focus_to_Cxy_focus/a_normal/step10_b.py
+++ Exps_9_combine/I_w_Mgt_to_Wxyz_focus_to_Cxy_focus/a_normal/step10_b.py
@@ -59,7 +59,6 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L5_ch008.{run}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch004.{run}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch002.{run}"])
-# sb.run(cmd_python_step10_a + [f"L5_ch128.{run}"])
 # sb.run(cmd_python_step10_a + [f"L5_ch001.{run}"])
 
 # sb.run(cmd_python_step10_a + [f"L6_ch032.{run}"])
