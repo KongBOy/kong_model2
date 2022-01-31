1051f23da778a5ad57800eeee472b97d6f2bbc9c
KongBoy <s89334roy@yahoo.com.tw>
2022-01-31 18:47:58 +0800

    W_w_Mgt_to_Cxy (2UNet) GAN same_G用 L5_ch032 配 各種diff_D_no_pad 127.27 L7  16~ 2 compress_and_bm_rec_all

--- Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D_no_pad/step10_b2_post.py
+++ Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D_no_pad/step10_b2_post.py
@@ -79,9 +79,9 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L6_ch001.{run}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch128.{run}"])
 
-# sb.run(cmd_python_step10_a + [f"L7_ch002.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch004.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch008.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch002.{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch004.{compress_and_bm_rec_all}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch008.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch016.{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch032.{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch064.{run}"])
