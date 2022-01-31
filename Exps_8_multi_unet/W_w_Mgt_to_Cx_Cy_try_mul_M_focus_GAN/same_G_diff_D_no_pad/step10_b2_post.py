431235bdb92efe3a53f9852dc3a77ba4c0fe527c
str_cvml <strong.85.wu@gmail.com>
2022-01-31 22:46:06 +0800

    W_w_Mgt_to_Cxy (2UNet) GAN same_G用 L5_ch032 配 各種diff_D_no_pad 127.55 L6 001 compress_and_bm_rec_all

--- Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D_no_pad/step10_b2_post.py
+++ Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D_no_pad/step10_b2_post.py
@@ -76,7 +76,7 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L6_ch016.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch032.{compress_all}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch064.{compress_and_bm_rec_all}"])
-# sb.run(cmd_python_step10_a + [f"L6_ch001.{run}"])
+# sb.run(cmd_python_step10_a + [f"L6_ch001.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch128.{run}"])
 
 # sb.run(cmd_python_step10_a + [f"L7_ch002.{compress_and_bm_rec_all}"])
