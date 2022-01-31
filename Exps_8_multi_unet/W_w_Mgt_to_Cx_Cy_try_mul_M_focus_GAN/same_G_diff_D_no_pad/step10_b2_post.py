abb508041c127a75ac4e58491a95d1e5c15035c2
KongBoy <s89334roy@yahoo.com.tw>
2022-01-31 18:45:28 +0800

    W_w_Mgt_to_Cxy (2UNet) GAN same_G用 L5_ch032 配 各種diff_D_no_pad 127.38 L6 64, 32  compress_and_bm_rec_all

--- Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D_no_pad/step10_b2_post.py
+++ Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D_no_pad/step10_b2_post.py
@@ -74,8 +74,8 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L6_ch004.{run}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch008.{run}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch016.{run}"])
-# sb.run(cmd_python_step10_a + [f"L6_ch032.{run}"])
-# sb.run(cmd_python_step10_a + [f"L6_ch064.{run}"])
+# sb.run(cmd_python_step10_a + [f"L6_ch032.{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"L6_ch064.{compress_and_bm_rec_all}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch001.{run}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch128.{run}"])
 
