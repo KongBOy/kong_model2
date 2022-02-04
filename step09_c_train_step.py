ec8ac585f529d3a284e96c649e8b34595279e254
KongBoy <s89334roy@yahoo.com.tw>
2022-02-04 19:59:54 +0800

    Train_step_w_GAN 把 訓練GAN 相同的code抽出來了

--- step09_c_train_step.py
+++ step09_c_train_step.py
@@ -8,42 +8,106 @@ from step10_a1_loss import *
     因為 有些架構 用 的 train_step 是一樣的， 所以 先只寫成 function， 給各個架構掛上去
 '''
 
-class Ttrain_step_Cxy_GAN():
-    def __init__(self, BCE_use_mask=False):
+class Ttrain_step_w_GAN:
+    def __init__(self, op_type, BCE_use_mask=False):
+        self.op_type = op_type
         self.BCE_use_mask = BCE_use_mask
 
     # def train_step_Cxy_GAN(model_obj, in_data, gt_data, loss_info_objs=None):
-    @tf.function
-    def __call__(self, model_obj, in_data, gt_data, loss_info_objs=None):
-        in_Mask  = in_data[..., 3:4]
-        in_W     = in_data[..., 0:3]
-        W_w_M = in_W * in_Mask
-
-        Cxgt_pre = gt_data[..., 2:3]
-        Cygt_pre = gt_data[..., 1:2]
-
+    def multi_output_w_GAN_train(self, model_obj, in_data, gt_datas, loss_info_objs=None, Mask=None):
         BCE_mask = None
-        if(self.BCE_use_mask): BCE_mask = in_Mask
-        ### 訓練 Discriminator
+        if(self.BCE_use_mask): BCE_mask = Mask
+        ### 訓練 Discriminato
         with tf.GradientTape(persistent=True) as D_tape:
             ### 生成 fake_data， 並丟入 D 取得 fake_score
-            Cx_pre_raw, Cy_pre_raw = model_obj.generator(W_w_M)
-            C_pre_raw  = tf.concat([Cy_pre_raw, Cx_pre_raw], axis=-1)
-            C_pre_w_M  = C_pre_raw * in_Mask
-            fake_score = model_obj.discriminator(C_pre_w_M)
+            model_outputs_raw = model_obj.generator(in_data)           ### 舉例：model_outputs_raw == [Cx_pre_raw, Cy_pre_raw], in_data == W_w_M
+            model_output_raw  = tf.concat(model_outputs_raw, axis=-1)  ### 舉例：model_output_raw  ==  C_pre_raw
+            model_output_w_M  = model_output_raw * Mask                ### 舉例：model_output_w_M  ==  C_pre_w_M
+            fake_score = model_obj.discriminator(model_output_w_M)
             ### 取用 real_data， 並丟入 D 取得 real_score
-            Cgt_pre    = tf.concat([Cygt_pre, Cxgt_pre], axis=-1)
-            real_score = model_obj.discriminator(Cgt_pre)
+            gt_data    = tf.concat(gt_datas, axis=-1)                  ### 舉例：gt_data == Cgt_pre, gt_datas == [Cygt_pre, Cxgt_pre]
+            real_score = model_obj.discriminator(gt_data)
 
             ### 訓練D： fake 越低分越好， real 越高分越好
             fake_score_gt0 = tf.zeros_like(fake_score, dtype=tf.float32)
             real_score_gt1 = tf.ones_like (real_score, dtype=tf.float32)
 
-            BCE_D_fake = loss_info_objs[2].loss_funs_dict["BCE_D_fake"](fake_score_gt0, fake_score, BCE_mask)
-            BCE_D_real = loss_info_objs[2].loss_funs_dict["BCE_D_real"](real_score_gt1, real_score, BCE_mask)
+            BCE_posi = len(model_outputs_raw)  ### 舉例： 第一個放 Cx 的 loss_info_obj, 第二個放 Cy  的 loss_info_obj, 第三個 才放 GAN 的 loss
+            BCE_D_fake = loss_info_objs[BCE_posi].loss_funs_dict["BCE_D_fake"](fake_score_gt0, fake_score, BCE_mask)
+            BCE_D_real = loss_info_objs[BCE_posi].loss_funs_dict["BCE_D_real"](real_score_gt1, real_score, BCE_mask)
             D_total_loss = (BCE_D_fake + BCE_D_real) / 2
         grad_D = D_tape.gradient(D_total_loss, model_obj.discriminator.trainable_weights)
         model_obj.optimizer_D.apply_gradients(zip(grad_D, model_obj.discriminator.trainable_weights))
+
+        ### 更新完D 後 訓練 Generator， 所以應該要重新丟一次資料進去G
+        with tf.GradientTape(persistent=True) as G_tape:
+            model_outputs_raw = model_obj.generator(in_data)  ### 舉例：model_outputs_raw == [Cx_pre_raw, Cy_pre_raw] (同上)
+            multi_losses = []
+            multi_total_loss = 0
+            for go_m, model_output in enumerate(model_outputs_raw):  ### 舉例：model_outputs_raw == [Cx_pre_raw, Cy_pre_raw]， 第一次跑 Cx， 第二次跑 Cy
+                total_loss, losses = one_loss_info_obj_total_loss(loss_info_objs[go_m], model_output, gt_datas[go_m], Mask=Mask)
+                multi_losses.append(losses)
+                multi_total_loss += total_loss
+
+            model_output_raw  = tf.concat(model_outputs_raw, axis=-1)  ### 舉例：model_output_raw  ==  C_pre_raw (同上)
+            model_output_w_M  = model_output_raw * Mask                ### 舉例：model_output_w_M  ==  C_pre_w_M (同上)
+            fake_score = model_obj.discriminator(model_output_w_M)
+            fake_score_gt1 = tf.ones_like(fake_score, dtype=tf.float32)   ### 訓練G時， 希望騙過D， 所以希望越高分越好
+
+            BCE_posi = len(model_outputs_raw)  ### 舉例： 第一個放 Cx 的 loss_info_obj, 第二個放 Cy  的 loss_info_obj, 第三個 才放 GAN 的 loss (同上)
+            BCE_G_to_D = loss_info_objs[BCE_posi].loss_funs_dict["BCE_G_to_D"](fake_score_gt1, fake_score, BCE_mask)
+            G_total_loss = BCE_G_to_D + multi_total_loss
+            # G_total_loss = multi_total_loss  ### debug 用， 看看 不加 GAN loss 效果如何
+        grad_G = G_tape.gradient(G_total_loss, model_obj.generator.trainable_weights)
+        model_obj.optimizer_G.apply_gradients(zip(grad_G, model_obj.generator.trainable_weights))
+
+        ### 把值放進 loss containor裡面，在外面才會去算 平均後 才畫出來喔！
+        for go_m, _ in enumerate(model_outputs_raw):
+            loss_info_obj = loss_info_objs[go_m]
+            for go_containor, loss_containor in enumerate(loss_info_obj.loss_containors.values()):
+                loss_containor( multi_losses[go_m][go_containor] )
+        loss_info_objs[2].loss_containors["BCE_D_fake"](BCE_D_fake)
+        loss_info_objs[2].loss_containors["BCE_D_real"](BCE_D_real)
+        loss_info_objs[2].loss_containors["BCE_G_to_D"](BCE_G_to_D)
+
+
+    @tf.function
+    def __call__(self, model_obj, in_data, gt_data, loss_info_objs=None):
+        if(self.op_type == "W_w_Mgt_to_Cx_Cy_focus"):
+            in_Mask  = in_data[..., 3:4]
+            in_W     = in_data[..., 0:3]
+            W_w_M = in_W * in_Mask
+
+            Cxgt_pre = gt_data[..., 2:3]
+            Cygt_pre = gt_data[..., 1:2]
+            self.multi_output_w_GAN_train(model_obj,
+                                         in_data=W_w_M,
+                                         gt_datas=[Cxgt_pre, Cygt_pre],  ### 沒辦法當初設定成這樣子train， 就只能繼續保持這樣子了，要不然以前train好的東西 不能繼續用下去 QQ
+                                         loss_info_objs=loss_info_objs,  ### 第一個放 Cx 的 loss_info_obj, 第二個放 Cy  的 loss_info_obj, 第三個 才放 GAN 的 loss
+                                         Mask=in_Mask)
+
+        # BCE_mask = None
+        # if(self.BCE_use_mask): BCE_mask = in_Mask
+        # ### 訓練 Discriminator
+        # with tf.GradientTape(persistent=True) as D_tape:
+        #     ### 生成 fake_data， 並丟入 D 取得 fake_score
+        #     Cx_pre_raw, Cy_pre_raw = model_obj.generator(W_w_M)
+        #     C_pre_raw  = tf.concat([Cy_pre_raw, Cx_pre_raw], axis=-1)
+        #     C_pre_w_M  = C_pre_raw * in_Mask
+        #     fake_score = model_obj.discriminator(C_pre_w_M)
+        #     ### 取用 real_data， 並丟入 D 取得 real_score
+        #     Cgt_pre    = tf.concat([Cygt_pre, Cxgt_pre], axis=-1)
+        #     real_score = model_obj.discriminator(Cgt_pre)
+
+        #     ### 訓練D： fake 越低分越好， real 越高分越好
+        #     fake_score_gt0 = tf.zeros_like(fake_score, dtype=tf.float32)
+        #     real_score_gt1 = tf.ones_like (real_score, dtype=tf.float32)
+
+        #     BCE_D_fake = loss_info_objs[2].loss_funs_dict["BCE_D_fake"](fake_score_gt0, fake_score, BCE_mask)
+        #     BCE_D_real = loss_info_objs[2].loss_funs_dict["BCE_D_real"](real_score_gt1, real_score, BCE_mask)
+        #     D_total_loss = (BCE_D_fake + BCE_D_real) / 2
+        # grad_D = D_tape.gradient(D_total_loss, model_obj.discriminator.trainable_weights)
+        # model_obj.optimizer_D.apply_gradients(zip(grad_D, model_obj.discriminator.trainable_weights))
 
         # import numpy as np
         # print(fake_score)
@@ -58,36 +122,36 @@ class Ttrain_step_Cxy_GAN():
         # plt.show()
 
         ### 更新完D 後 訓練 Generator， 所以應該要重新丟一次資料進去G
-        with tf.GradientTape(persistent=True) as G_tape:
-            Cx_pre_raw, Cy_pre_raw = model_obj.generator(W_w_M)
-            model_outputs = [Cx_pre_raw, Cy_pre_raw]
-            gt_datas      = [Cxgt_pre  , Cygt_pre  ]  ### 沒辦法當初設定成這樣子train， 就只能繼續保持這樣子了，要不然以前train好的東西 不能繼續用下去 QQ
-            multi_losses = []
-            multi_total_loss = 0
-            for go_m, model_output in enumerate(model_outputs):
-                total_loss, losses = one_loss_info_obj_total_loss(loss_info_objs[go_m], model_output, gt_datas[go_m], Mask=in_Mask)
-                multi_losses.append(losses)
-                multi_total_loss += total_loss
+        # with tf.GradientTape(persistent=True) as G_tape:
+        #     Cx_pre_raw, Cy_pre_raw = model_obj.generator(W_w_M)
+        #     model_outputs = [Cx_pre_raw, Cy_pre_raw]
+        #     gt_datas      = [Cxgt_pre  , Cygt_pre  ]  ### 沒辦法當初設定成這樣子train， 就只能繼續保持這樣子了，要不然以前train好的東西 不能繼續用下去 QQ
+        #     multi_losses = []
+        #     multi_total_loss = 0
+        #     for go_m, model_output in enumerate(model_outputs):
+        #         total_loss, losses = one_loss_info_obj_total_loss(loss_info_objs[go_m], model_output, gt_datas[go_m], Mask=in_Mask)
+        #         multi_losses.append(losses)
+        #         multi_total_loss += total_loss
 
-            C_pre_raw = tf.concat([Cy_pre_raw, Cx_pre_raw], axis=-1)
-            C_pre_w_M = C_pre_raw * in_Mask
-            fake_score = model_obj.discriminator(C_pre_w_M)
-            fake_score_gt1 = tf.ones_like(fake_score, dtype=tf.float32)
-            BCE_G_to_D = loss_info_objs[2].loss_funs_dict["BCE_G_to_D"](fake_score_gt1, fake_score, BCE_mask)
-            G_total_loss = BCE_G_to_D + multi_total_loss
-            # G_total_loss = multi_total_loss  ### debug 用， 看看 不加 GAN loss 效果如何
-        grad_G = G_tape.gradient(G_total_loss, model_obj.generator.trainable_weights)
-        model_obj.optimizer_G.apply_gradients(zip(grad_G, model_obj.generator.trainable_weights))
+        #     C_pre_raw = tf.concat([Cy_pre_raw, Cx_pre_raw], axis=-1)
+        #     C_pre_w_M = C_pre_raw * in_Mask
+        #     fake_score = model_obj.discriminator(C_pre_w_M)
+        #     fake_score_gt1 = tf.ones_like(fake_score, dtype=tf.float32)
+        #     BCE_G_to_D = loss_info_objs[2].loss_funs_dict["BCE_G_to_D"](fake_score_gt1, fake_score, BCE_mask)
+        #     G_total_loss = BCE_G_to_D + multi_total_loss
+        #     # G_total_loss = multi_total_loss  ### debug 用， 看看 不加 GAN loss 效果如何
+        # grad_G = G_tape.gradient(G_total_loss, model_obj.generator.trainable_weights)
+        # model_obj.optimizer_G.apply_gradients(zip(grad_G, model_obj.generator.trainable_weights))
 
 
         ### 把值放進 loss containor裡面，在外面才會去算 平均後 才畫出來喔！
-        for go_m, _ in enumerate(model_outputs):
-            loss_info_obj = loss_info_objs[go_m]
-            for go_containor, loss_containor in enumerate(loss_info_obj.loss_containors.values()):
-                loss_containor( multi_losses[go_m][go_containor] )
-        loss_info_objs[2].loss_containors["BCE_D_fake"](BCE_D_fake)
-        loss_info_objs[2].loss_containors["BCE_D_real"](BCE_D_real)
-        loss_info_objs[2].loss_containors["BCE_G_to_D"](BCE_G_to_D)
+        # for go_m, _ in enumerate(model_outputs):
+        #     loss_info_obj = loss_info_objs[go_m]
+        #     for go_containor, loss_containor in enumerate(loss_info_obj.loss_containors.values()):
+        #         loss_containor( multi_losses[go_m][go_containor] )
+        # loss_info_objs[2].loss_containors["BCE_D_fake"](BCE_D_fake)
+        # loss_info_objs[2].loss_containors["BCE_D_real"](BCE_D_real)
+        # loss_info_objs[2].loss_containors["BCE_G_to_D"](BCE_G_to_D)
 ###################################################################################################################################################
 ###################################################################################################################################################
 ###################################################################################################################################################
