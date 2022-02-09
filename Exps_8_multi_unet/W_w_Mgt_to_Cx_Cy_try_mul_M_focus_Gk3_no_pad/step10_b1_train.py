0e4964943dbfe42bf4e7e9fefc267e7d6aefcd9b
str_cvml <strong.85.wu@gmail.com>
2022-02-09 11:07:23 +0800

    W_w_Mgt_to_Cxy (2UNet) Gk3_no_pad 127.55 L7 L8 64 compress_and_bm_rec_all

--- Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_Gk3_no_pad/step10_b1_train.py
+++ Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_Gk3_no_pad/step10_b1_train.py
@@ -71,12 +71,12 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"L6_ch064.{run}"])
 # sb.run(cmd_python_step10_a + [f"L6_ch001.{run}"])
 
-# sb.run(cmd_python_step10_a + [f"L7_ch016.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch008.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch004.{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch002.{run}"])
-# sb.run(cmd_python_step10_a + [f"L7_ch032.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch004.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch008.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch016.{run}"])
 # sb.run(cmd_python_step10_a + [f"L7_ch001.{run}"])
+# sb.run(cmd_python_step10_a + [f"L7_ch032.{run}"])
 
 # sb.run(cmd_python_step10_a + [f"L8_ch008.{run}"])
 # sb.run(cmd_python_step10_a + [f"L8_ch004.{run}"])
