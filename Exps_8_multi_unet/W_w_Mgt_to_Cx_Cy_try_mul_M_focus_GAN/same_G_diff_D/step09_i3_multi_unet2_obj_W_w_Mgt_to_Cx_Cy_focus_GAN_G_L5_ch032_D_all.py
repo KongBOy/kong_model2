b2bedc94a0195d07e7c8384794b4609cebd5f8bf
KongBoy <s89334roy@yahoo.com.tw>
2022-02-09 19:28:21 +0800

    加點debug資訊

--- Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D/step09_i3_multi_unet2_obj_W_w_Mgt_to_Cx_Cy_focus_GAN_G_L5_ch032_D_all.py
+++ Exps_8_multi_unet/W_w_Mgt_to_Cx_Cy_try_mul_M_focus_GAN/same_G_diff_D/step09_i3_multi_unet2_obj_W_w_Mgt_to_Cx_Cy_focus_GAN_G_L5_ch032_D_all.py
@@ -1,3 +1,18 @@
+#############################################################################################################################################################################################################
+### 把 kong_model2 加入 sys.path
+import os
+code_exe_path = os.path.realpath(__file__)                   ### 目前執行 step10_b.py 的 path
+code_exe_path_element = code_exe_path.split("\\")            ### 把 path 切分 等等 要找出 kong_model 在第幾層
+kong_layer = code_exe_path_element.index("kong_model2")      ### 找出 kong_model2 在第幾層
+kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer + 1])  ### 定位出 kong_model2 的 dir
+import sys                                                   ### 把 kong_model2 加入 sys.path
+sys.path.append(kong_model2_dir)
+# print(__file__.split("\\")[-1])
+# print("    code_exe_path:", code_exe_path)
+# print("    code_exe_path_element:", code_exe_path_element)
+# print("    kong_layer:", kong_layer)
+# print("    kong_model2_dir:", kong_model2_dir)
+#############################################################################################################################################################################################################
 from step08_b_use_G_generate_W_w_M_to_Cx_Cy_focus import W_w_M_Gen_Cx_Cy_focus_see
 
 from step09_c_train_step import Ttrain_step_w_GAN
@@ -88,3 +103,11 @@ disc_L9_ch008_sig = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).
 disc_L9_ch004_sig = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_or_W_to_Cx_Cy", I_to_Cx=step09_e5_flow_unet2_obj_I_to_M.block1_L5_ch032_sig, I_to_Cy=step09_e5_flow_unet2_obj_I_to_M.block1_L5_ch032_sig).set_gen_op(W_w_M_Gen_Cx_Cy_focus_see).set_discriminator_by_exist_builder(step09_i3_discriminator.disc_L9_ch004_sig).set_train_step(Ttrain_step_w_GAN(op_type="W_w_Mgt_to_Cx_Cy_focus"))
 disc_L9_ch002_sig = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_or_W_to_Cx_Cy", I_to_Cx=step09_e5_flow_unet2_obj_I_to_M.block1_L5_ch032_sig, I_to_Cy=step09_e5_flow_unet2_obj_I_to_M.block1_L5_ch032_sig).set_gen_op(W_w_M_Gen_Cx_Cy_focus_see).set_discriminator_by_exist_builder(step09_i3_discriminator.disc_L9_ch002_sig).set_train_step(Ttrain_step_w_GAN(op_type="W_w_Mgt_to_Cx_Cy_focus"))
 disc_L9_ch001_sig = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_or_W_to_Cx_Cy", I_to_Cx=step09_e5_flow_unet2_obj_I_to_M.block1_L5_ch032_sig, I_to_Cy=step09_e5_flow_unet2_obj_I_to_M.block1_L5_ch032_sig).set_gen_op(W_w_M_Gen_Cx_Cy_focus_see).set_discriminator_by_exist_builder(step09_i3_discriminator.disc_L9_ch001_sig).set_train_step(Ttrain_step_w_GAN(op_type="W_w_Mgt_to_Cx_Cy_focus"))
+
+if(__name__ == "__main__"):
+    import numpy as np
+    data = np.zeros(shape=(1, 512, 512, 1), dtype=np.float32)
+
+    use_model = disc_L2_ch016_sig
+    use_model = use_model.build()
+    print(use_model.generator(data)[0].shape)
