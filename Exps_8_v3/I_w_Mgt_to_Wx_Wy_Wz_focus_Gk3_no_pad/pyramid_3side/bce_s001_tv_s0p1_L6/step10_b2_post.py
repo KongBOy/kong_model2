4b65b214c4fa6660f0d6a9472d64b988e2bdb632
KongBoy <s89334roy@yahoo.com.tw>
2022-03-07 15:50:27 +0800

    I_to_Wxyz( Exps8 3UNet) 的 pyramid 127.38 3side_L6 1s7 2s1 3s1 compress_and_WM_visual

--- Exps_8_v3/I_w_Mgt_to_Wx_Wy_Wz_focus_Gk3_no_pad/pyramid_3side/bce_s001_tv_s0p1_L6/step10_b2_post.py
+++ Exps_8_v3/I_w_Mgt_to_Wx_Wy_Wz_focus_Gk3_no_pad/pyramid_3side/bce_s001_tv_s0p1_L6/step10_b2_post.py
@@ -89,7 +89,7 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"ch032_1side_6__2side_6__3side_5.{run}"])
 # sb.run(cmd_python_step10_a + [f"ch032_1side_6__2side_6__3side_6.{run}"])
 
-# sb.run(cmd_python_step10_a + [f"ch032_1side_7__2side_1__3side_1.{run}"])
+# sb.run(cmd_python_step10_a + [f"ch032_1side_7__2side_1__3side_1.{compress_and_WM_3D_all_from_begin}"])
 # sb.run(cmd_python_step10_a + [f"ch032_1side_7__2side_2__3side_1.{run}"])
 # sb.run(cmd_python_step10_a + [f"ch032_1side_7__2side_2__3side_2.{run}"])
 # sb.run(cmd_python_step10_a + [f"ch032_1side_7__2side_3__3side_1.{run}"])
