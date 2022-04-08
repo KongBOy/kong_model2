e580056c597f6d1b20af3e36579707cafb757dfd
KongBoy <s89334roy@yahoo.com.tw>
2022-04-08 11:13:03 +0800

    step12 size256 的 更新 和 jumpt_to加入

--- step12_result_analyzer.py
+++ step12_result_analyzer.py
@@ -7,7 +7,7 @@ import numpy as np
 import sys
 sys.path.append("kong_util")
 from step0_access_path import Analyze_Write_Dir, Analyze_Read_Dir, JPG_QUALITY, CORE_AMOUNT_FIND_LTRD_AND_CROP, CORE_AMOUNT_SAVE_AS_JPG, Syn_write_to_read_dir
-from matplot_fig_ax_util import Matplot_single_row_imgs, Matplot_multi_row_imgs
+from kong_util.matplot_fig_ax_util import Matplot_single_row_imgs, Matplot_multi_row_imgs
 from build_dataset_combine import Check_dir_exist_and_build, Check_dir_exist_and_build_new_dir, Find_ltrd_and_crop, Save_as_jpg
 from video_from_img import Video_combine_from_dir
 from multiprocessing import Process
@@ -18,7 +18,7 @@ import os
 import shutil
 
 class Result_analyzer:
-    def __init__(self, ana_describe, ana_what_sees, ana_what, show_in_img, show_gt_img, bgr2rgb=False, add_loss=False, img_h=768, img_w=768):
+    def __init__(self, ana_describe, ana_what_sees, ana_what, show_in_img, show_gt_img, bgr2rgb=False, add_loss=False, img_h=768, img_w=768, fontsize=16, title_fontsize=20):
         '''
         ana_what_sees: test / see
         '''
@@ -34,6 +34,9 @@ class Result_analyzer:
 
         self.img_h = img_h  ### 給 空影像用的
         self.img_w = img_w  ### 給 空影像用的
+
+        self.fontsize = fontsize
+        self.title_fontsize = title_fontsize
 
         '''
         大小寫都可以
@@ -133,14 +136,16 @@ class Result_analyzer:
 ########################################################################################################################################
 ########################################################################################################################################
 class Col_results_analyzer(Result_analyzer):
-    def __init__(self, ana_describe, ana_what_sees, ana_what, col_results, show_in_img=True, show_gt_img=True, bgr2rgb=False, add_loss=False, img_h=768, img_w=768):
-        super().__init__(ana_describe, ana_what_sees, ana_what, show_in_img, show_gt_img, img_h=img_h, img_w=img_w)
+    def __init__(self, ana_describe, ana_what_sees, ana_what, col_results, show_in_img=True, show_gt_img=True, bgr2rgb=False, add_loss=False, img_h=768, img_w=768, fontsize=16, title_fontsize=20, jump_to=0):
+        super().__init__(ana_describe, ana_what_sees, ana_what, show_in_img, show_gt_img, img_h=img_h, img_w=img_w, fontsize=fontsize, title_fontsize=title_fontsize)
 
         self.c_results = col_results
         self.c_min_trained_epoch = None  ### 要使用的時候再去用 self.step0_get_c_min_trained_epoch()去抓
         self.c_max_trained_epoch = None  ### 要使用的時候再去用 self.step0_get_c_max_trained_epoch()去抓
         self.check_exp_builder_change_to_result()
         print("Col_results_analyzer build finish")
+        
+        self.jump_to = jump_to
 
     def check_exp_builder_change_to_result(self):
         from step10_b2_exp_builder import Exp_builder
@@ -238,15 +243,18 @@ class Col_results_analyzer(Result_analyzer):
         """
 
         for go_epoch in tqdm(range(start_epoch, start_epoch + epoch_amount + 1)):  ### +1 是因為 0~epoch 都要做，  range 只到 end-1， 所以 +1 補回來～
+            if(go_epoch < self.jump_to): continue
             ### step2 去抓 一行 要 show 的影像
             c_imgs = self.step2b_get_c_results_imgs_and_attach_in_gt(see_num, go_epoch, in_img, gt_img)
 
             single_row_imgs = Matplot_single_row_imgs(
-                            imgs       = c_imgs,      ### 把要顯示的每張圖包成list
-                            img_titles = c_titles,    ### 把每張圖要顯示的字包成list
-                            fig_title  = "epoch=%04i" % go_epoch,   ### 圖上的大標題
-                            bgr2rgb    = self.bgr2rgb,
-                            add_loss   = self.add_loss)
+                            imgs           = c_imgs,      ### 把要顯示的每張圖包成list
+                            img_titles     = c_titles,    ### 把每張圖要顯示的字包成list
+                            fig_title      = "epoch=%04i" % go_epoch,   ### 圖上的大標題
+                            fontsize       = self.fontsize,
+                            title_fontsize = self.title_fontsize,
+                            bgr2rgb        = self.bgr2rgb,
+                            add_loss       = self.add_loss)
             single_row_imgs.Draw_img()
             if(self.add_loss):
                 for go_result, result in enumerate(self.c_results):
@@ -428,8 +436,8 @@ class Col_results_analyzer(Result_analyzer):
 
 ### 目前小任務還沒有切multiprocess喔！
 class Row_col_results_analyzer(Result_analyzer):
-    def __init__(self, ana_describe, ana_what_sees, ana_what, row_col_results, show_in_img=True, show_gt_img=True, bgr2rgb=False, add_loss=False, img_h=768, img_w=768):
-        super().__init__(ana_describe, ana_what_sees, ana_what, show_in_img, show_gt_img, bgr2rgb, add_loss, img_h=img_h, img_w=img_w)
+    def __init__(self, ana_describe, ana_what_sees, ana_what, row_col_results, show_in_img=True, show_gt_img=True, bgr2rgb=False, add_loss=False, img_h=768, img_w=768, fontsize=16, title_fontsize=20, jump_to=0):
+        super().__init__(ana_describe, ana_what_sees, ana_what, show_in_img, show_gt_img, bgr2rgb, add_loss, img_h=img_h, img_w=img_w, fontsize=fontsize, title_fontsize=title_fontsize)
         self.r_c_results = row_col_results
         self.r_c_min_trained_epoch = None  ### 要使用的時候再去用 self.step0_get_r_c_min_trained_epoch()去抓
         self.r_c_max_trained_epoch = None  ### 要使用的時候再去用 self.step0_get_r_c_max_trained_epoch()去抓
@@ -438,6 +446,8 @@ class Row_col_results_analyzer(Result_analyzer):
         for c_results in row_col_results:
             self.c_results_list.append(Col_results_analyzer(ana_describe=ana_describe, ana_what_sees=ana_what_sees, ana_what=ana_what, col_results=c_results, show_in_img=self.show_in_img, show_gt_img=self.show_gt_img, bgr2rgb=self.bgr2rgb, add_loss=self.add_loss, img_h=img_h, img_w=img_w))
         print("Row_col_results_analyzer build finish")
+
+        self.jump_to = jump_to
 
     def _step0_get_r_c_trained_epochs(self):
         ### 在使用 所有 result 前， 要記得先去 update 一下 他們的 sees 喔！
@@ -508,12 +518,15 @@ class Row_col_results_analyzer(Result_analyzer):
 
         # for go_img in tqdm(range(self.r_c_min_trained_epoch)):
         for go_epoch in tqdm(range(start_epoch, start_epoch + epoch_amount + 1)):  ### +1 是因為 0~epoch 都要做，  range 只到 end-1， 所以 +1 補回來～
+            if(go_epoch < self.jump_to): continue
             # print("see_num=", see_num, "go_epoch=", go_epoch)
             r_c_imgs  = self.step2b_get_r_c_imgs(see_num, go_epoch, in_img, gt_img)
             row_col_imgs = Matplot_multi_row_imgs(
                                             rows_cols_imgs   = r_c_imgs,
                                             rows_cols_titles = r_c_titles,
                                             fig_title        = "epoch=%04i" % go_epoch,   ### 圖上的大標題
+                                            fontsize         = self.fontsize,
+                                            title_fontsize   = self.title_fontsize,
                                             bgr2rgb          = self.bgr2rgb,
                                             add_loss         = self.add_loss)
             row_col_imgs.Draw_img()
