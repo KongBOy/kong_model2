520bce621daed0062a8962170966b34a6a2070a8
KongBoy <s89334roy@yahoo.com.tw>
2022-04-08 10:38:24 +0800

    修正 import來源

--- Exps_7_v3/I_to_M_Gk3_no_pad/pyramid_size256/pyr_6s/6side_step11_seed.py
+++ Exps_7_v3/I_to_M_Gk3_no_pad/pyramid_size256/pyr_6s/6side_step11_seed.py
@@ -20,12 +20,12 @@ if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py
     os.chdir(code_exe_dir)
 # print("current_path:", os.getcwd())
 ###############################################################################################################################################################################################################
-import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_0side.bce_s001_tv_s0p1_L3.step10_a as L3_0side
-import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_1side.bce_s001_tv_s0p1_L3.step10_a as L3_1side
-import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_2side.bce_s001_tv_s0p1_L3.step10_a as L3_2side
-import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_3side.bce_s001_tv_s0p1_L3.step10_a as L3_3side
-import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_4side.bce_s001_tv_s0p1_L3.step10_a as L3_4side
-import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_5side.bce_s001_tv_s0p1_L3.step10_a as L3_5side
+import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_size256.pyr_0s.bce_s001_tv_s0p1_L3.step10_a as L3_0side
+import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_size256.pyr_1s.bce_s001_tv_s0p1_L3.step10_a as L3_1side
+import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_size256.pyr_2s.bce_s001_tv_s0p1_L3.step10_a as L3_2side
+import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_size256.pyr_3s.bce_s001_tv_s0p1_L3.step10_a as L3_3side
+import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_size256.pyr_4s.bce_s001_tv_s0p1_L3.step10_a as L3_4side
+import Exps_7_v3.I_to_M_Gk3_no_pad.pyramid_size256.pyr_5s.bce_s001_tv_s0p1_L3.step10_a as L3_5side
 import step10_a as L3_6side
 #################################################################################################################################################################################################################################################################################################################################################################################################
 ###################
