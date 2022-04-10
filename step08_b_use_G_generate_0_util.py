b308859423cc5948f53127d89f889580e752ead4
KongBoy <s89334roy@yahoo.com.tw>
2022-04-10 20:23:05 +0800

    為了 tight_crop 的 random_jit 把它物件化 並且 修正 一組組data 用相同random_jit 的問題囉

--- step08_b_use_G_generate_0_util.py
+++ step08_b_use_G_generate_0_util.py
@@ -90,94 +90,112 @@ def W_01_and_W_01_w_M_to_WM_and_visualize(W_raw, M, out_ch3=False):
 
 ######################################################################################################################################################################################################
 ######################################################################################################################################################################################################
-def tight_crop(data, Mask, pad_size=20, resize=None, jit_scale=0):
-    import tensorflow as tf
-    '''
-    目前的寫法連 batch 都考慮進去囉
-    resize: [h, w]
-    '''
-    ### np.where( T/F_map 或 0/非0_array ) 可參考：https://numpy.org/doc/stable/reference/generated/numpy.where.html，
-    ### np.where( T/F_map 或 0/非0_array ) 只有放一個參數的時候， 相當於 np.nonzero()， 如果放三個參數時， 為true的地方 填入 第二個參數值， 為False的地方 填入 第三個參數值,
-    ### 不直接用 nonzero的原因是 tf 沒有 nonzero 但有 where， 為了 tf, numpy 都通用 這邊 numpy 就配合tf 用 where 囉～
-    ### 但也要注意 tf.where( T/F_map 或 0/非0_array ) return 回來的東西 跟 np.where( T/F_map 或 0/非0_array ) 回傳的東西 shape 不大一樣喔
-    ### np.where( T/F_map 或 0/非0_array ) -> tuple ( x非零indexs, y非零indexs, z非零indexs )
-    ### tf.where( T/F_map 或 0/非0_array ) -> tensor.shape( 所有非零點的個數, 3 ( 非零xyz_index) )
-    nonzero_map = Mask > 0
-    ### numpy 寫法：
-    # if  (len(Mask.shape) == 4): b_ind, y_ind, x_ind, c_ind = np.where(nonzero_map)
-    # elif(len(Mask.shape) == 3): y_ind, x_ind, c_ind = np.where(nonzero_map)
-    # elif(len(Mask.shape) == 2): y_ind, x_ind = np.where(nonzero_map)
+import tensorflow as tf
+class Tight_crop():
+    def __init__(self, pad_size=20, resize=None, jit_scale=0):
+        self.pad_size  = pad_size
+        self.resize    = resize
+        self.jit_scale = jit_scale
 
-    ### tf 寫法
-    nonzero_index = tf.where(nonzero_map)
-    if  (len(Mask.shape) == 4): col_id = 1  ### BHWC， H在第1個col
-    elif(len(Mask.shape) == 3): col_id = 0  ### HWC ， H在第0個col
-    elif(len(Mask.shape) == 2): col_id = 0  ### HWC ， H在第0個col
-    y_ind = nonzero_index[:, col_id     : col_id + 1]
-    x_ind = nonzero_index[:, col_id + 1 : col_id + 2]
+        ### tf.random.uniform 不能夠接受 jit_scale = 0， 所以才必須要有這個if
+        if(self.jit_scale > 0):  self.reset_jit()
 
-    # l = x_ind.min()
-    # r = x_ind.max()
-    # t = y_ind.min()
-    # d = y_ind.max()
-    l = tf.reduce_min(x_ind)
-    r = tf.reduce_max(x_ind)
-    t = tf.reduce_min(y_ind)
-    d = tf.reduce_max(y_ind)
+    def reset_jit(self):
+        if(self.jit_scale > 0):
+            self.l_jit = tf.random.uniform(shape=[], minval=-self.jit_scale, maxval=self.jit_scale, dtype=tf.int64)
+            self.r_jit = tf.random.uniform(shape=[], minval=-self.jit_scale, maxval=self.jit_scale, dtype=tf.int64)
+            self.t_jit = tf.random.uniform(shape=[], minval=-self.jit_scale, maxval=self.jit_scale, dtype=tf.int64)
+            self.d_jit = tf.random.uniform(shape=[], minval=-self.jit_scale, maxval=self.jit_scale, dtype=tf.int64)
+        else: print("Tight_crop=0 無法 reset_jit， 請注意 建立 Tight_crop物件 的時候 jit_scale 有沒有設定數值， 目前不做事直接跳過 reset_jit動作")
 
-    l_pad = l - pad_size
-    r_pad = r + pad_size
-    t_pad = t - pad_size
-    d_pad = d + pad_size
+    def __call__(self, data, Mask):
+        '''
+        目前的寫法連 batch 都考慮進去囉
+        resize: [h, w]
+        '''
+        ### np.where( T/F_map 或 0/非0_array ) 可參考：https://numpy.org/doc/stable/reference/generated/numpy.where.html，
+        ### np.where( T/F_map 或 0/非0_array ) 只有放一個參數的時候， 相當於 np.nonzero()， 如果放三個參數時， 為true的地方 填入 第二個參數值， 為False的地方 填入 第三個參數值,
+        ### 不直接用 nonzero的原因是 tf 沒有 nonzero 但有 where， 為了 tf, numpy 都通用 這邊 numpy 就配合tf 用 where 囉～
+        ### 但也要注意 tf.where( T/F_map 或 0/非0_array ) return 回來的東西 跟 np.where( T/F_map 或 0/非0_array ) 回傳的東西 shape 不大一樣喔
+        ### np.where( T/F_map 或 0/非0_array ) -> tuple ( x非零indexs, y非零indexs, z非零indexs )
+        ### tf.where( T/F_map 或 0/非0_array ) -> tensor.shape( 所有非零點的個數, 3 ( 非零xyz_index) )
+        nonzero_map = Mask > 0
+        ### numpy 寫法：
+        # if  (len(Mask.shape) == 4): b_ind, y_ind, x_ind, c_ind = np.where(nonzero_map)
+        # elif(len(Mask.shape) == 3): y_ind, x_ind, c_ind = np.where(nonzero_map)
+        # elif(len(Mask.shape) == 2): y_ind, x_ind = np.where(nonzero_map)
 
-    ### 隨機抖動 random jit
-    if(jit_scale > 0):  ### tf.random.uniform 不能夠接受 jit_scale = 0， 所以才必須要有這個if
-        l_pad += tf.random.uniform(shape=[], minval=-jit_scale, maxval=jit_scale, dtype=tf.int64)
-        r_pad += tf.random.uniform(shape=[], minval=-jit_scale, maxval=jit_scale, dtype=tf.int64)
-        t_pad += tf.random.uniform(shape=[], minval=-jit_scale, maxval=jit_scale, dtype=tf.int64)
-        d_pad += tf.random.uniform(shape=[], minval=-jit_scale, maxval=jit_scale, dtype=tf.int64)
+        ### tf 寫法
+        nonzero_index = tf.where(nonzero_map)
+        if  (len(Mask.shape) == 4): col_id = 1  ### BHWC， H在第1個col
+        elif(len(Mask.shape) == 3): col_id = 0  ### HWC ， H在第0個col
+        elif(len(Mask.shape) == 2): col_id = 0  ### HWC ， H在第0個col
+        y_ind = nonzero_index[:, col_id     : col_id + 1]
+        x_ind = nonzero_index[:, col_id + 1 : col_id + 2]
 
-    ########### 先 pad 再 crop
-    ###### pad part
-    ### 看 超過影像範圍多少
-    l_out = tf.constant(0, tf.int64)
-    r_out = tf.constant(0, tf.int64)
-    t_out = tf.constant(0, tf.int64)
-    d_out = tf.constant(0, tf.int64)
-    if  (len(data.shape) == 4): b, h, w, c = data.shape
-    elif(len(data.shape) == 3): h, w, c = data.shape
-    elif(len(data.shape) == 2): h, w = data.shape
+        # l = x_ind.min()
+        # r = x_ind.max()
+        # t = y_ind.min()
+        # d = y_ind.max()
+        l = tf.reduce_min(x_ind)
+        r = tf.reduce_max(x_ind)
+        t = tf.reduce_min(y_ind)
+        d = tf.reduce_max(y_ind)
 
-    if(l_pad < 0): l_out = - l_pad
-    if(t_pad < 0): t_out = - t_pad
-    if(r_pad > w - 1): r_out = r_pad - (w - 1)
-    if(d_pad > h - 1): d_out = d_pad - (h - 1)
+        l_pad = l - self.pad_size
+        r_pad = r + self.pad_size
+        t_pad = t - self.pad_size
+        d_pad = d + self.pad_size
 
-    ### 看 pad 的範圍有沒有超過影像， 有的話就 pad
-    if(l_out > 0 or r_out > 0  or t_out > 0 or d_out > 0):
-        # if  (len(data.shape) == 4): data = np.pad(data, ( (0    ,     0), (t_out, d_out), (l_out, r_out), (    0,     0) ) , 'reflect')
-        # elif(len(data.shape) == 3): data = np.pad(data, ( (t_out, d_out), (l_out, r_out), (    0,     0) )                 , 'reflect')
-        # elif(len(data.shape) == 2): data = np.pad(data, ( (t_out, d_out), (l_out, r_out) )                                 , 'reflect')
-        if  (len(data.shape) == 4): data = tf.pad(data, ( (0    ,     0), (t_out, d_out), (l_out, r_out), (    0,     0) ) , 'REFLECT')
-        elif(len(data.shape) == 3): data = tf.pad(data, ( (t_out, d_out), (l_out, r_out), (    0,     0) )                 , 'REFLECT')
-        elif(len(data.shape) == 2): data = tf.pad(data, ( (t_out, d_out), (l_out, r_out) )                                 , 'REFLECT')
-    # breakpoint()
+        ### 隨機抖動 random jit
+        if(self.jit_scale > 0):  ### tf.random.uniform 不能夠接受 jit_scale = 0， 所以才必須要有這個if
+            l_pad += self.l_jit
+            r_pad += self.r_jit
+            t_pad += self.t_jit
+            d_pad += self.d_jit
 
-    ###### pad 完成了， 以下開始 crop
-    ### 對 pad完成 的 data 重新定位
-    if(l_pad < 0): l_pad = tf.constant(0, tf.int64)
-    if(t_pad < 0): t_pad = tf.constant(0, tf.int64)
-    # l_pad = max(l_pad, 0)          ### l_pad, t_pad 可能會被剪到 負的， 但index最小是0喔 ， 所以最小取0
-    # t_pad = max(t_pad, 0)          ### l_pad, t_pad 可能會被剪到 負的， 但index最小是0喔 ， 所以最小取0
-    r_pad = r_pad + l_out + r_out  ### r_pad, d_pad 自己如果超過的話， 因為會pad出去， 所以要加上 超過的部分， 在來還要考慮如果 l_pad, t_pad 超出去的話， 因為index最小為0， 代表 左、上 超出去的部分 要補到 右、下 的部分， 所以要多加 l_out, t_out 喔！
-    d_pad = d_pad + t_out + d_out  ### r_pad, d_pad 自己如果超過的話， 因為會pad出去， 所以要加上 超過的部分， 在來還要考慮如果 l_pad, t_pad 超出去的話， 因為index最小為0， 代表 左、上 超出去的部分 要補到 右、下 的部分， 所以要多加 l_out, t_out 喔！
+        ########### 先 pad 再 crop
+        ###### pad part
+        ### 看 超過影像範圍多少
+        l_out = tf.constant(0, tf.int64)
+        r_out = tf.constant(0, tf.int64)
+        t_out = tf.constant(0, tf.int64)
+        d_out = tf.constant(0, tf.int64)
+        if  (len(data.shape) == 4): b, h, w, c = data.shape
+        elif(len(data.shape) == 3): h, w, c = data.shape
+        elif(len(data.shape) == 2): h, w = data.shape
 
-    ### 重新定位 完成了， 以下開始 crop
-    if  (len(data.shape) == 4): data = data[:, t_pad : d_pad + 1, l_pad : r_pad + 1, :]  ### BHWC
-    elif(len(data.shape) == 3): data = data[t_pad : d_pad + 1, l_pad : r_pad + 1, :]     ### HWC
-    elif(len(data.shape) == 2): data = data[t_pad : d_pad + 1, l_pad : r_pad + 1]        ### HW
+        if(l_pad < 0): l_out = - l_pad
+        if(t_pad < 0): t_out = - t_pad
+        if(r_pad > w - 1): r_out = r_pad - (w - 1)
+        if(d_pad > h - 1): d_out = d_pad - (h - 1)
 
-    if(resize is not None): data = tf.image.resize(data, resize, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        ### 看 pad 的範圍有沒有超過影像， 有的話就 pad
+        if(l_out > 0 or r_out > 0  or t_out > 0 or d_out > 0):
+            # if  (len(data.shape) == 4): data = np.pad(data, ( (0    ,     0), (t_out, d_out), (l_out, r_out), (    0,     0) ) , 'reflect')
+            # elif(len(data.shape) == 3): data = np.pad(data, ( (t_out, d_out), (l_out, r_out), (    0,     0) )                 , 'reflect')
+            # elif(len(data.shape) == 2): data = np.pad(data, ( (t_out, d_out), (l_out, r_out) )                                 , 'reflect')
+            if  (len(data.shape) == 4): data = tf.pad(data, ( (0    ,     0), (t_out, d_out), (l_out, r_out), (    0,     0) ) , 'REFLECT')
+            elif(len(data.shape) == 3): data = tf.pad(data, ( (t_out, d_out), (l_out, r_out), (    0,     0) )                 , 'REFLECT')
+            elif(len(data.shape) == 2): data = tf.pad(data, ( (t_out, d_out), (l_out, r_out) )                                 , 'REFLECT')
+        # breakpoint()
 
-    # breakpoint()
-    return data
+        ###### pad 完成了， 以下開始 crop
+        ### 對 pad完成 的 data 重新定位
+        if(l_pad < 0): l_pad = tf.constant(0, tf.int64)
+        if(t_pad < 0): t_pad = tf.constant(0, tf.int64)
+        # l_pad = max(l_pad, 0)          ### l_pad, t_pad 可能會被剪到 負的， 但index最小是0喔 ， 所以最小取0
+        # t_pad = max(t_pad, 0)          ### l_pad, t_pad 可能會被剪到 負的， 但index最小是0喔 ， 所以最小取0
+        r_pad = r_pad + l_out + r_out  ### r_pad, d_pad 自己如果超過的話， 因為會pad出去， 所以要加上 超過的部分， 在來還要考慮如果 l_pad, t_pad 超出去的話， 因為index最小為0， 代表 左、上 超出去的部分 要補到 右、下 的部分， 所以要多加 l_out, t_out 喔！
+        d_pad = d_pad + t_out + d_out  ### r_pad, d_pad 自己如果超過的話， 因為會pad出去， 所以要加上 超過的部分， 在來還要考慮如果 l_pad, t_pad 超出去的話， 因為index最小為0， 代表 左、上 超出去的部分 要補到 右、下 的部分， 所以要多加 l_out, t_out 喔！
+
+        ### 重新定位 完成了， 以下開始 crop
+        if  (len(data.shape) == 4): data = data[:, t_pad : d_pad + 1, l_pad : r_pad + 1, :]  ### BHWC
+        elif(len(data.shape) == 3): data = data[t_pad : d_pad + 1, l_pad : r_pad + 1, :]     ### HWC
+        elif(len(data.shape) == 2): data = data[t_pad : d_pad + 1, l_pad : r_pad + 1]        ### HW
+
+        ########### 全都處理完以後， resize 到指定的大小
+        if(self.resize is not None): data = tf.image.resize(data, self.resize, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+        # breakpoint()
+        return data
