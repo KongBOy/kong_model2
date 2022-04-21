781a941d05beeab812710d9d1320ac329f3c39fa
KongBoy <s89334roy@yahoo.com.tw>
2022-04-21 19:00:25 +0800

    template更新

--- Exps_7_v3/template_dir/pyramid_template_1_6/pyr_4s/L5/step11_L2345678.py
+++ Exps_7_v3/template_dir/pyramid_template_1_6/pyr_4s/L5/step11_L2345678.py
@@ -20,10 +20,10 @@ if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py
     os.chdir(code_exe_dir)
 # print("current_path:", os.getcwd())
 ###############################################################################################################################################################################################################
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_0s.bce_s001_tv_s0p1_L5.step10_a as L5_0side
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_1s.bce_s001_tv_s0p1_L5.step10_a as L5_1side
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_2s.bce_s001_tv_s0p1_L5.step10_a as L5_2side
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_3s.bce_s001_tv_s0p1_L5.step10_a as L5_3side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_0s.bce_s001_tv_s0p1_L5.step10_a as L5_0side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_1s.bce_s001_tv_s0p1_L5.step10_a as L5_1side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_2s.bce_s001_tv_s0p1_L5.step10_a as L5_2side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_3s.bce_s001_tv_s0p1_L5.step10_a as L5_3side
 import step10_a as L5_4side
 #################################################################################################################################################################################################################################################################################################################################################################################################
 ########
--- Exps_7_v3/template_dir/pyramid_template_1_6/pyr_4s/L7/step11_L2345678.py
+++ Exps_7_v3/template_dir/pyramid_template_1_6/pyr_4s/L7/step11_L2345678.py
@@ -20,10 +20,10 @@ if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py
     os.chdir(code_exe_dir)
 # print("current_path:", os.getcwd())
 ###############################################################################################################################################################################################################
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_0s.bce_s001_tv_s0p1_L7.step10_a as L7_0side
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_1s.bce_s001_tv_s0p1_L7.step10_a as L7_1side
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_2s.bce_s001_tv_s0p1_L7.step10_a as L7_2side
-import  Exps_7_v3.template_dir.pyramid_template_1_6.pyr_3s.bce_s001_tv_s0p1_L7.step10_a as L7_3side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_0s.bce_s001_tv_s0p1_L7.step10_a as L7_0side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_1s.bce_s001_tv_s0p1_L7.step10_a as L7_1side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_2s.bce_s001_tv_s0p1_L7.step10_a as L7_2side
+import Exps_7_v3.template_dir.pyramid_template_1_6.pyr_3s.bce_s001_tv_s0p1_L7.step10_a as L7_3side
 import step10_a as L7_4side
 #################################################################################################################################################################################################################################################################################################################################################################################################
 ########
