ea5d9bba878baa2cffe2cb814a53bd806999d4f6
KongBoy <s89334roy@yahoo.com.tw>
2022-05-03 10:13:38 +0800

    一些 train_reload 變回 train

--- Exps_7_v3/doc3d/I_to_M_Gk3_no_pad/pyr_Tcrop256_pad60_jit15/pyr_3s/L3/step10_a.py
+++ Exps_7_v3/doc3d/I_to_M_Gk3_no_pad/pyr_Tcrop256_pad60_jit15/pyr_3s/L3/step10_a.py
@@ -50,7 +50,7 @@ empty = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_1side_1__2sid
 #############################################################
 ch032_1side_1__2side_1__3side_1 = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_1side_1__2side_1__3side_1, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_1side_1__2side_1__3side_1.kong_model.model_describe) .set_train_args(epochs=  1, exp_bn_see_arg=None) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="L3_ch032_bl_pyr__1s1__2s1__3s1-20220501_092006")
 
-ch032_1side_2__2side_1__3side_1 = Exp_builder().set_basic("train_reload", use_db_obj, ch032_pyramid_1side_2__2side_1__3side_1, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_1side_2__2side_1__3side_1.kong_model.model_describe) .set_train_args(epochs=  1, exp_bn_see_arg=None) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="L3_ch032_bl_pyr__1s2__2s1__3s1-20220501_100309")
+ch032_1side_2__2side_1__3side_1 = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_1side_2__2side_1__3side_1, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_1side_2__2side_1__3side_1.kong_model.model_describe) .set_train_args(epochs=  1, exp_bn_see_arg=None) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="L3_ch032_bl_pyr__1s2__2s1__3s1-20220501_100309")
 ch032_1side_2__2side_2__3side_1 = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_1side_2__2side_2__3side_1, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_1side_2__2side_2__3side_1.kong_model.model_describe) .set_train_args(epochs=  1, exp_bn_see_arg=None) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="L3_ch032_bl_pyr__1s2__2s2__3s1-20220501_115347")
 ch032_1side_2__2side_2__3side_2 = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_1side_2__2side_2__3side_2, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_1side_2__2side_2__3side_2.kong_model.model_describe) .set_train_args(epochs=  1, exp_bn_see_arg=None) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="L3_ch032_bl_pyr__1s2__2s2__3s2-20220501_124221")
 
@@ -77,7 +77,7 @@ if(__name__ == "__main__"):
     if len(sys.argv) < 2:
         ############################################################################################################
         ### 直接按 F5 或打 python step10_b1_exp_obj_load_and_train_and_test.py，後面沒有接東西喔！才不會跑到下面給 step10_b_subprocss.py 用的程式碼~~~
-        ch032_1side_4__2side_3__3side_2.build().run()
+        ch032_1side_1__2side_1__3side_1.build().run()
         # print('no argument')
         sys.exit()
 
