0f7bd1531a9348360c40bf5785a67fce0854fdfc
KongBoy <s89334roy@yahoo.com.tw>
2022-05-10 02:26:23 +0800

    doc3d I_w_M_to_W_tight_crop256_pad60_jit15 step11 的 import 修正

--- Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok/pyr_Tcrop256_pad60_jit15/pyr_4s/L8/step11_L2345678.py
+++ Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok/pyr_Tcrop256_pad60_jit15/pyr_4s/L8/step11_L2345678.py
@@ -20,10 +20,10 @@ if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py
     os.chdir(code_exe_dir)
 # print("current_path:", os.getcwd())
 ###############################################################################################################################################################################################################
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_0s.bce_s001_tv_s0p1_L8.step10_a as L8_0side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_1s.bce_s001_tv_s0p1_L8.step10_a as L8_1side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_2s.bce_s001_tv_s0p1_L8.step10_a as L8_2side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_3s.bce_s001_tv_s0p1_L8.step10_a as L8_3side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_0s.L8.step10_a as L8_0side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_1s.L8.step10_a as L8_1side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_2s.L8.step10_a as L8_2side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_3s.L8.step10_a as L8_3side
 import step10_a as L8_4side
 #################################################################################################################################################################################################################################################################################################################################################################################################
 ########
--- Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok/pyr_Tcrop256_pad60_jit15/pyr_6s/L6/step11_L2345678.py
+++ Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok/pyr_Tcrop256_pad60_jit15/pyr_6s/L6/step11_L2345678.py
@@ -20,12 +20,12 @@ if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py
     os.chdir(code_exe_dir)
 # print("current_path:", os.getcwd())
 ###############################################################################################################################################################################################################
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_0s.bce_s001_tv_s0p1_L6.step10_a as L6_0side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_1s.bce_s001_tv_s0p1_L6.step10_a as L6_1side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_2s.bce_s001_tv_s0p1_L6.step10_a as L6_2side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_3s.bce_s001_tv_s0p1_L6.step10_a as L6_3side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_4s.bce_s001_tv_s0p1_L6.step10_a as L6_4side
-import Exps_7_v3.W_w_Mgt_to_Cx_Cy_focus.pyramid_tight_crop_size256_pad60_jit15.pyr_5s.bce_s001_tv_s0p1_L6.step10_a as L6_5side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_0s.L6.step10_a as L6_0side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_1s.L6.step10_a as L6_1side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_2s.L6.step10_a as L6_2side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_3s.L6.step10_a as L6_3side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_4s.L6.step10_a as L6_4side
+import Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop256_pad60_jit15.pyr_5s.L6.step10_a as L6_5side
 import step10_a as L6_6side
 #################################################################################################################################################################################################################################################################################################################################################################################################
 ###################
