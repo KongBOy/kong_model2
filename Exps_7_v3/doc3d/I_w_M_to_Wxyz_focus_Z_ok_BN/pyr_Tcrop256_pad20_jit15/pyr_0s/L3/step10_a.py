121519a798f221cb4a528b4b886cb056f14aad0b
KongBoy <s89334roy@yahoo.com.tw>
2022-05-10 22:42:38 +0800

    嘗試BN iter 搭配 batch_size 的 修正

--- Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok_BN/pyr_Tcrop256_pad20_jit15/pyr_0s/L3/step10_a.py
+++ Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok_BN/pyr_Tcrop256_pad20_jit15/pyr_0s/L3/step10_a.py
@@ -44,11 +44,12 @@ exp_dir 是 決定 result_dir 的 "上一層"資料夾 名字喔！ exp_dir要�
 
 use_db_obj = type8_blender_kong_doc3d_in_I_gt_W_ch_norm_v2
 use_loss_obj = [G_mae_s001_loss_info_builder.set_loss_target("UNet_z").copy(), G_mae_s001_loss_info_builder.set_loss_target("UNet_y").copy(), G_mae_s001_loss_info_builder.set_loss_target("UNet_x").copy()]  ### z, y, x 順序是看 step07_b_0b_Multi_UNet 來對應的喔
+batch_size = 10
 #############################################################
 ### 為了resul_analyze畫空白的圖，建一個empty的 Exp_builder
-empty = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_0side, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_0side.kong_model.model_describe) .set_train_args(epochs=  1, batch_size=10) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="為了resul_analyze畫空白的圖，建一個empty的 Exp_builder")
+empty = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_0side, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_0side.kong_model.model_describe) .set_train_args(epochs=  1, batch_size=batch_size) .set_train_iter_args(it_see_fq=900//batch_size, it_save_fq=(900 * 2)//batch_size, it_down_step="half", it_down_fq=900//batch_size).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="為了resul_analyze畫空白的圖，建一個empty的 Exp_builder")
 #############################################################
-ch032_0side = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_0side, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_0side.kong_model.model_describe) .set_train_args(epochs=  1, batch_size=10) .set_train_iter_args(it_see_fq=900, it_save_fq=900 * 2, it_down_step="half", it_down_fq=900).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="")
+ch032_0side = Exp_builder().set_basic("train", use_db_obj, ch032_pyramid_0side, use_loss_obj, exp_dir=exp_dir, code_exe_path=code_exe_path, describe_end=ch032_pyramid_0side.kong_model.model_describe) .set_train_args(epochs=  1, batch_size=batch_size) .set_train_iter_args(it_see_fq=900//batch_size, it_save_fq=(900 * 2)//batch_size, it_down_step="half", it_down_fq=900//batch_size).set_train_in_gt_use_range(use_in_range=Range(0, 1), use_gt_range=Range(0, 1)).set_result_name(result_name="")
 #############################################################
 if(__name__ == "__main__"):
     print("build exps cost time:", time.time() - start_time)
