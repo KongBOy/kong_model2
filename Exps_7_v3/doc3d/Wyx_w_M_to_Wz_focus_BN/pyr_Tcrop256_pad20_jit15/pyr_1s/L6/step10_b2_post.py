b59c55589ef6dc6374286596d95127e4091baceb
str_cvml <strong.85.wu@gmail.com>
2022-05-13 16:12:24 +0800

    doc3d 127.55

--- Exps_7_v3/doc3d/Wyx_w_M_to_Wz_focus_BN/pyr_Tcrop256_pad20_jit15/pyr_1s/L6/step10_b2_post.py
+++ Exps_7_v3/doc3d/Wyx_w_M_to_Wz_focus_BN/pyr_Tcrop256_pad20_jit15/pyr_1s/L6/step10_b2_post.py
@@ -32,3 +32,4 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"ch032_1side_5.{train}"])
 # sb.run(cmd_python_step10_a + [f"ch032_1side_6.{train}"])
 # sb.run(cmd_python_step10_a + [f"ch032_1side_7.{train}"])
+# sb.run(cmd_python_step10_a + [f"ch032_1side_7_ep10.{compress_all}"])
