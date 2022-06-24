afd5e622a7ab86a54c0f6959ff43e545c53e6cab
KongBoy <s89334roy@yahoo.com.tw>
2022-06-24 10:03:31 +0800

    加入 I_w_M_to_W 的 Mae+Sob_k9 good的

---
+++ Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok_to_Cxy_focus/Separ/step10_b2_gather_metric.py
@@ -0,0 +1,85 @@
+#############################################################################################################################################################################################################
+#############################################################################################################################################################################################################
+### 把 kong_model2 加入 sys.path
+import os
+code_exe_path = os.path.realpath(__file__)                   ### 目前執行 step10_b.py 的 path
+code_exe_path_element = code_exe_path.split("\\")            ### 把 path 切分 等等 要找出 kong_model 在第幾層
+kong_layer = code_exe_path_element.index("kong_model2")      ### 找出 kong_model2 在第幾層
+kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer + 1])  ### 定位出 kong_model2 的 dir
+import sys                                                   ### 把 kong_model2 加入 sys.path
+sys.path.append(kong_model2_dir)
+# print(__file__.split("\\")[-1])
+# print("    code_exe_path:", code_exe_path)
+# print("    code_exe_path_element:", code_exe_path_element)
+# print("    kong_layer:", kong_layer)
+# print("    kong_model2_dir:", kong_model2_dir)
+###############################################################################################################################################################################################################
+# 按F5執行時， 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～ 才可 import step10_a.py 喔！
+code_exe_dir = os.path.dirname(code_exe_path)   ### 目前執行 step10_b.py 的 dir
+if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～
+    os.chdir(code_exe_dir)
+# print("current_path:", os.getcwd())
+###############################################################################################################################################################################################################
+### 所有 指令 統一寫這邊
+from step10_c_exp_command import *
+######################################################################################################################
+import subprocess as sb
+
+#################################
+### 前change 後fix
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k03__MaeSob_k05_EroM                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k03_EroM__MaeSob_k05_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k03_EroMore__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k03_Tv_s001__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k03_Tv_s001_EroM__MaeSob_k05_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k03_Tv_s001_EroMore__MaeSob_k05_EroM .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05__MaeSob_k05_EroM                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroMore__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_Tv_s001__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_Tv_s001_EroM__MaeSob_k05_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_Tv_s001_EroMore__MaeSob_k05_EroM .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k09__MaeSob_k05_EroM                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k09_EroM__MaeSob_k05_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k09_EroMore__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k09_Tv_s001__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k09_Tv_s001_EroM__MaeSob_k05_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k09_Tv_s001_EroMore__MaeSob_k05_EroM .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k11__MaeSob_k05_EroM                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k11_EroM__MaeSob_k05_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k11_EroMore__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k11_Tv_s001__MaeSob_k05_EroM         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k11_Tv_s001_EroM__MaeSob_k05_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k11_Tv_s001_EroMore__MaeSob_k05_EroM .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+#################################
+### 前fix 後change
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_EroMore         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_Tv_s001         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_Tv_s001_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_Tv_s001_EroMore .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroMore         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_Tv_s001         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_Tv_s001_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_Tv_s001_EroMore .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_EroMore         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_Tv_s001         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_Tv_s001_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_Tv_s001_EroMore .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11                 .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_EroM            .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_EroMore         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_Tv_s001         .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_Tv_s001_EroM    .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_Tv_s001_EroMore .{Gather_test_SSIM_LD % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
---
+++ Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok_to_Cxy_focus/Separ/step11.py
@@ -0,0 +1,71 @@
+#############################################################################################################################################################################################################
+#############################################################################################################################################################################################################
+### 把 kong_model2 加入 sys.path
+import os
+code_exe_path = os.path.realpath(__file__)                   ### 目前執行 step10_b.py 的 path
+code_exe_path_element = code_exe_path.split("\\")            ### 把 path 切分 等等 要找出 kong_model 在第幾層
+kong_layer = code_exe_path_element.index("kong_model2")      ### 找出 kong_model2 在第幾層
+kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer + 1])  ### 定位出 kong_model2 的 dir
+import sys                                                   ### 把 kong_model2 加入 sys.path
+sys.path.append(kong_model2_dir)
+# print(__file__.split("\\")[-1])
+# print("    code_exe_path:", code_exe_path)
+# print("    code_exe_path_element:", code_exe_path_element)
+# print("    kong_layer:", kong_layer)
+# print("    kong_model2_dir:", kong_model2_dir)
+###############################################################################################################################################################################################################
+# 按F5執行時， 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～ 才可 import step10_a.py 喔！
+code_exe_dir = os.path.dirname(code_exe_path)   ### 目前執行 step10_b.py 的 dir
+if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～
+    os.chdir(code_exe_dir)
+# print("current_path:", os.getcwd())
+###############################################################################################################################################################################################################
+from step10_a import *
+from Exps_7_v3.doc3d.DewarpNet_result.step10_a import Model_run, Google_down
+#################################################################################################################################################################################################################################################################################################################################################################################################
+#################################
+##### 前fix 後change
+### 分析 EroM
+combine1b_fix_I_w_M_to_W__change_W_w_M_t_C__EroM_type_analyze = [
+    [Model_run                                          , Google_down                                        , empty                                              , empty                                              , ],
+    [p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03        , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05        , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09        , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11        , ],
+    [p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_EroM   , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroM   , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_EroM   , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_EroM   , ],
+    [p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_EroMore, p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroMore, p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_EroMore, p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_EroMore, ],
+]
+
+### 分析TV
+combine2a_fix_I_w_M_to_W__change_W_w_M_t_C__wo_wiTv_s001_woEroM = [
+    [Model_run  , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11         , ],
+    [Google_down, p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_Tv_s001 , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_Tv_s001 , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_Tv_s001 , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_Tv_s001 , ],
+]
+combine2b_fix_I_w_M_to_W__change_W_w_M_t_C__wo_wiTv_s001_wiEroM = [
+    [Model_run  , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_EroM         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_EroM         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_EroM         , ],
+    [Google_down, p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_Tv_s001_EroM , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_Tv_s001_EroM , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_Tv_s001_EroM , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_Tv_s001_EroM , ],
+]
+combine2c_fix_I_w_M_to_W__change_W_w_M_t_C__wo_wiTv_s001_wiEroMore = [
+    [Model_run  , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_EroMore         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroMore         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_EroMore         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_EroMore         , ],
+    [Google_down, p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k03_Tv_s001_EroMore , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_Tv_s001_EroMore , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k09_Tv_s001_EroMore , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k11_Tv_s001_EroMore , ],
+]
+#################################
+##### 前change 後fix
+### 分析 EroM
+combine3b_fix_I_w_M_to_W__change_W_w_M_t_C__EroM_type_analyze = [
+    [Model_run                                          , Google_down                                        , empty                                              , empty                                              , ],
+    [p20_wiColorJ_L5_MaeSob_k03__MaeSob_k05_EroM        , p20_wiColorJ_L5_MaeSob_k05__MaeSob_k05_EroM        , p20_wiColorJ_L5_MaeSob_k09__MaeSob_k05_EroM        , p20_wiColorJ_L5_MaeSob_k11__MaeSob_k05_EroM        , ],
+    [p20_wiColorJ_L5_MaeSob_k03_EroM__MaeSob_k05_EroM   , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroM   , p20_wiColorJ_L5_MaeSob_k09_EroM__MaeSob_k05_EroM   , p20_wiColorJ_L5_MaeSob_k11_EroM__MaeSob_k05_EroM   , ],
+    [p20_wiColorJ_L5_MaeSob_k03_EroMore__MaeSob_k05_EroM, p20_wiColorJ_L5_MaeSob_k05_EroMore__MaeSob_k05_EroM, p20_wiColorJ_L5_MaeSob_k09_EroMore__MaeSob_k05_EroM, p20_wiColorJ_L5_MaeSob_k11_EroMore__MaeSob_k05_EroM, ],
+]
+
+### 分析TV
+combine4a_fix_I_w_M_to_W__change_W_w_M_t_C__wo_wiTv_s001_woEroM = [
+    [Model_run  , p20_wiColorJ_L5_MaeSob_k03__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k05__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k09__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k11__MaeSob_k05_EroM         , ],
+    [Google_down, p20_wiColorJ_L5_MaeSob_k03_Tv_s001__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k05_Tv_s001__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k09_Tv_s001__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k11_Tv_s001__MaeSob_k05_EroM , ],
+]
+combine4b_fix_I_w_M_to_W__change_W_w_M_t_C__wo_wiTv_s001_wiEroM = [
+    [Model_run  , p20_wiColorJ_L5_MaeSob_k03_EroM__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k05_EroM__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k09_EroM__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k11_EroM__MaeSob_k05_EroM         , ],
+    [Google_down, p20_wiColorJ_L5_MaeSob_k03_Tv_s001_EroM__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k05_Tv_s001_EroM__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k09_Tv_s001_EroM__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k11_Tv_s001_EroM__MaeSob_k05_EroM , ],
+]
+combine4c_fix_I_w_M_to_W__change_W_w_M_t_C__wo_wiTv_s001_wiEroMore = [
+    [Model_run  , p20_wiColorJ_L5_MaeSob_k03_EroMore__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k05_EroMore__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k09_EroMore__MaeSob_k05_EroM         , p20_wiColorJ_L5_MaeSob_k11_EroMore__MaeSob_k05_EroM         , ],
+    [Google_down, p20_wiColorJ_L5_MaeSob_k03_Tv_s001_EroMore__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k05_Tv_s001_EroMore__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k09_Tv_s001_EroMore__MaeSob_k05_EroM , p20_wiColorJ_L5_MaeSob_k11_Tv_s001_EroMore__MaeSob_k05_EroM , ],
+]
