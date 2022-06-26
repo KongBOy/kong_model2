0e1f4082720600526bf703c11859cd6e73bdf25c
KongBoy <s89334roy@yahoo.com.tw>
2022-06-27 02:45:50 +0800

    I_w_M_to_Wxyz_focus_to_Cxy_focus 的 Separate 更新 127.28

--- Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok_to_Cxy_focus/Separ/step09_3side_L5.py
+++ Exps_7_v3/doc3d/I_w_M_to_Wxyz_focus_Z_ok_to_Cxy_focus/Separ/step09_3side_L5.py
@@ -23,6 +23,7 @@ from step09_c_train_step import Train_step_I_w_M_to_W_to_C
 from step09_d_KModel_builder_combine_step789 import KModel_builder, MODEL_NAME
 
 ### Model builder， 跟loss沒關係， 只要model 取到自己要的即可
+from Exps_7_v3.doc3d.I_w_M_to_W_Comb.step09_2side_L5 import ch032_pyramid_1side_6__2side_6_woDiv as I_w_M_to_W_p20_2s_L5_woDiv_ch032
 from Exps_7_v3.doc3d.I_w_M_to_W_focus_Zok    .ch096.wiColorJ.Add2Loss.Sob_k05_s001_EroM_Mae_s001.pyr_Tcrop255_p20_j15.pyr_2s.L5.step09_2side_L5 import ch032_pyramid_1side_6__2side_6 as I_w_M_to_W_p20_2s_L5_woDiv
 from Exps_7_v3.doc3d.I_w_M_to_W_focus_Zok_div.ch032.wiColorJ.Add2Loss.Sob_k05_s001_EroM_Mae_s001.pyr_Tcrop255_p20_j15.pyr_2s.L5.step09_2side_L5 import ch032_pyramid_1side_6__2side_6 as I_w_M_to_W_p20_2s_L5_wiDiv
 from Exps_7_v3.doc3d.I_w_M_to_Wxyz_focus_Z_ok.pyr_Tcrop255_pad20_jit15.Mae_s001.pyr_2s.L5.step09_2side_L5                                       import ch032_pyramid_1side_6__2side_6 as I_w_M_to_W_p20_2s_L5_3UNet
@@ -41,6 +42,7 @@ start_time = time.time()
 ''' I_to_Wxyz 變動， W_to_Cxy固定用 W_w_Mgt_to_Cxy_focus 的 L5_ch064 '''
 ###############################################################################################################################################################################################
 ###############################################################################################################################################################################################
+ch032_pyramid_1side_6__2side_6_p20_L5_woDiv_ch032 = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_to_Wxyz_to_Cxy_general", I_to_Wx_Wy_Wz=I_w_M_to_W_p20_2s_L5_woDiv_ch032, W_to_Cx_Cy=W_w_M_to_Cxy_Tcrop255_p20_2s_L5).set_multi_model_separate_focus(I_to_W_separ=False, I_to_W_focus=True, W_to_C_separ=True, W_to_C_focus=True).set_gen_op( use_gen_op_p20 ).set_train_step( use_train_step_p20 )
 ch032_pyramid_1side_6__2side_6_p20_L5_woDiv = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_to_Wxyz_to_Cxy_general", I_to_Wx_Wy_Wz=I_w_M_to_W_p20_2s_L5_woDiv, W_to_Cx_Cy=W_w_M_to_Cxy_Tcrop255_p20_2s_L5).set_multi_model_separate_focus(I_to_W_separ=False, I_to_W_focus=True, W_to_C_separ=True, W_to_C_focus=True).set_gen_op( use_gen_op_p20 ).set_train_step( use_train_step_p20 )
 ch032_pyramid_1side_6__2side_6_p20_L5_wiDiv = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_to_Wxyz_to_Cxy_general", I_to_Wx_Wy_Wz=I_w_M_to_W_p20_2s_L5_wiDiv, W_to_Cx_Cy=W_w_M_to_Cxy_Tcrop255_p20_2s_L5).set_multi_model_separate_focus(I_to_W_separ=True , I_to_W_focus=True, W_to_C_separ=True, W_to_C_focus=True).set_gen_op( use_gen_op_p20 ).set_train_step( use_train_step_p20 )
 ch032_pyramid_1side_6__2side_6_p20_L5_3UNet = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_to_Wxyz_to_Cxy_general", I_to_Wx_Wy_Wz=I_w_M_to_W_p20_2s_L5_3UNet, W_to_Cx_Cy=W_w_M_to_Cxy_Tcrop255_p20_2s_L5).set_multi_model_separate_focus(I_to_W_separ=True , I_to_W_focus=True, W_to_C_separ=True, W_to_C_focus=True).set_gen_op( use_gen_op_p20 ).set_train_step( use_train_step_p20 )
