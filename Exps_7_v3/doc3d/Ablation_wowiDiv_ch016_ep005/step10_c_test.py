9ff1a7fde1954024a67b9d5984d2620ac0a5b007
KongBoy <s89334roy@yahoo.com.tw>
2022-07-18 09:33:48 +0800

    Ablation ep005 加入 W_w_M_to_C 的 1s6_2s5， 並且用  1s6_2s5, 6 和 I_w_M_to_W 的 1s5_2s4_3s3 搭配比較

--- Exps_7_v3/doc3d/Ablation_wowiDiv_ch016_ep005/step10_c_test.py
+++ Exps_7_v3/doc3d/Ablation_wowiDiv_ch016_ep005/step10_c_test.py
@@ -38,3 +38,6 @@ import subprocess as sb
 # sb.run(cmd_python_step10_a + [f"I_w_M_to_W__wiDiv_Sob_k09_Mae_ep001__W_w_M_to_C_woDiv_Sob_k09_Mae_ep001__ep002  .{test % ('test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA', 'knpy_save=False')}"])
 # sb.run(cmd_python_step10_a + [f"I_w_M_to_W__woDiv_Sob_k09_Mae_ep001__W_w_M_to_C_wiDiv_Sob_k09_Mae_ep001__ep002  .{test % ('test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA', 'knpy_save=False')}"])
 # sb.run(cmd_python_step10_a + [f"I_w_M_to_W__woDiv_Sob_k09_Mae_ep001__W_w_M_to_C_woDiv_Sob_k09_Mae_ep001__ep002  .{test % ('test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA', 'knpy_save=False')}"])
+
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__wiDiv_1s5_2s4_3s3_Sob_k09_Mae_ep001__W_w_M_to_C_wiDiv_1s6_2s5_Sob_k09_Mae_ep001__ep002  .{test % ('test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA', 'knpy_save=False')}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__wiDiv_1s5_2s4_3s3_Sob_k09_Mae_ep001__W_w_M_to_C_wiDiv_Sob_k09_Mae_ep001__ep002  .{test % ('test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA', 'knpy_save=False')}"])
