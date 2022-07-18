4c66f0d6c0a489839e4d0abc62835d43d0124dc1
KongBoy <s89334roy@yahoo.com.tw>
2022-07-18 16:53:32 +0800

    Ablation_wowiDiv整理了一下， 把 pyr 的 部分移到 Ablation2

---
+++ Exps_7_v3/doc3d/Ablation2_Pyr_ch016_ep005/step09_2side_L5.py
@@ -0,0 +1,93 @@
+#############################################################################################################################################################################################################
+#############################################################################################################################################################################################################
+### 把 kong_model2 加入 sys.path
+import os
+from tkinter import S
+code_exe_path = os.path.realpath(__file__)                   ### 目前執行 step10_b.py 的 path
+code_exe_path_element = code_exe_path.split("\\")            ### 把 path 切分 等等 要找出 kong_model 在第幾層
+kong_layer = code_exe_path_element.index("kong_model2")      ### 找出 kong_model2 在第幾層
+kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer + 1])  ### 定位出 kong_model2 的 dir
+import sys                                                   ### 把 kong_model2 加入 sys.path
+sys.path.append(kong_model2_dir)
+# print(__file__.split("\\")[-1])
+# print("    code_exe_path:", code_exe_path)
+# print("    code_exe_path_element:", code_exe_path_element)
+# print("    kong_layer:", kong_layer)
+# print("    kong_model2_dir:", kong_model2_dir)
+#############################################################################################################################################################################################################
+from step08_b_use_G_generate_0_util import Tight_crop, Color_jit
+from step09_d_KModel_builder_combine_step789 import KModel_builder, MODEL_NAME
+color_jit = Color_jit(do_ratio=0.6)
+
+### I_w_M_to_W
+from step08_b_use_G_generate_I_w_M_to_Wx_Wy_Wz_combine import I_w_M_to_W
+from step09_c_train_step import Train_step_I_w_M_to_W
+I_w_M_to_W_woDiv_use_gen_op     =            I_w_M_to_W(  separate_out=False, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale=  0) )
+I_w_M_to_W_woDiv_use_train_step = Train_step_I_w_M_to_W(  separate_out=False, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale= 15), color_jit=color_jit )
+I_w_M_to_W_wiDiv_use_gen_op     =            I_w_M_to_W(  separate_out=True , focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale=  0) )
+I_w_M_to_W_wiDiv_use_train_step = Train_step_I_w_M_to_W(  separate_out=True , focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale= 15), color_jit=color_jit )
+
+### W_w_M_to_C
+from step08_b_use_G_generate_W_w_M_to_Cx_Cy_combine import W_w_M_to_Cx_Cy
+from step09_c_train_step import Train_step_W_w_M_to_Cx_Cy
+W_w_M_to_C_woDiv_use_gen_op     =            W_w_M_to_Cx_Cy( separate_out=False, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale=  0) )
+W_w_M_to_C_woDiv_use_train_step = Train_step_W_w_M_to_Cx_Cy( separate_out=False, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale= 15) )
+W_w_M_to_C_wiDiv_use_gen_op     =            W_w_M_to_Cx_Cy( separate_out=True , focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale=  0) )
+W_w_M_to_C_wiDiv_use_train_step = Train_step_W_w_M_to_Cx_Cy( separate_out=True , focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale= 15) )
+
+
+### common
+use_hid_ch = 16
+
+import time
+start_time = time.time()
+###############################################################################################################################################################################################
+### Block1
+pyramid_1side_6__2side_5          = [2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2]
+pyramid_1side_6__2side_6          = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+pyramid_1side_5__2side_4__3side_3 = [3, 3, 3, 2, 1, 0, 1, 2, 3, 3, 3]
+
+#########################################################################################
+I_w_M_to_W_ch016_pyramid_1side_5__2side_4__3side_3__wiDiv = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch=use_hid_ch    , depth_level=5, out_ch=1, d_amount=3, bottle_divide=True, unet_acti="sigmoid", conv_block_num=pyramid_1side_5__2side_4__3side_3, ch_upper_bound= 2 ** 14).set_gen_op( I_w_M_to_W_wiDiv_use_gen_op ).set_train_step( I_w_M_to_W_wiDiv_use_train_step )
+
+W_w_M_to_C_ch016_pyramid_1side_6__2side_6_wiDiv = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch=use_hid_ch    , depth_level=5, out_ch=1, d_amount=2, bottle_divide=True, unet_acti="sigmoid", conv_block_num=pyramid_1side_6__2side_6, ch_upper_bound= 2 ** 14).set_gen_op( W_w_M_to_C_wiDiv_use_gen_op ).set_train_step( W_w_M_to_C_wiDiv_use_train_step )
+W_w_M_to_C_ch016_pyramid_1side_6__2side_5_wiDiv = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch=use_hid_ch    , depth_level=5, out_ch=1, d_amount=2, bottle_divide=True, unet_acti="sigmoid", conv_block_num=pyramid_1side_6__2side_5, ch_upper_bound= 2 ** 14).set_gen_op( W_w_M_to_C_wiDiv_use_gen_op ).set_train_step( W_w_M_to_C_wiDiv_use_train_step )
+from Exps_7_v3.doc3d.W_w_Mgt_to_Cx_Cy_focus_Z_ok.Sob_k03t13_EroM_Mae_Tv_EroM.pyr_Tcrop255_pad20_jit15.step09_2side_L5 import ch032_pyramid_1side_6__2side_6 as W_w_M_to_C_3UNet_ch032
+#########################################################################################
+###############################################################################################################################################################################################
+### 合起來也寫一起好了拉
+from step08_c_use_G_generate_I_w_M_to_Wx_Wy_Wz_focus_to_Cx_Cy_focus_combine import I_w_M_to_W_to_C
+from step09_c_train_step import Train_step_I_w_M_to_W_to_C
+use_gen_op_p20     =            I_w_M_to_W_to_C(  separate_out=True, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale=  0) )  ### 我目前的 multi_model 的 I_to_Wxyz_to_Cxy_general 是 全部都回傳 Wz_pre_w_M, Wy_pre_w_M, Wx_pre_w_M, Cx_pre_w_M, Cy_pre_w_M， 所以不管 wi/woDIV， Separate 全設 True 就對了 
+use_train_step_p20 = Train_step_I_w_M_to_W_to_C(  separate_out=True, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale= 15), color_jit=color_jit )  ### 我目前的 multi_model 的 I_to_Wxyz_to_Cxy_general 是 全部都回傳 Wz_pre_w_M, Wy_pre_w_M, Wx_pre_w_M, Cx_pre_w_M, Cy_pre_w_M， 所以不管 wi/woDIV， Separate 全設 True 就對了 
+### wiDiv wiDiv
+I_w_M_to_W_ch016_pyr_1s5_2s4_3s3_wiDiv__W_w_M_to_C_ch016_pyr_1s6_2s5_wiDiv = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_to_Wxyz_to_Cxy_general", I_to_Wx_Wy_Wz=I_w_M_to_W_ch016_pyramid_1side_5__2side_4__3side_3__wiDiv, W_to_Cx_Cy=W_w_M_to_C_ch016_pyramid_1side_6__2side_5_wiDiv).set_multi_model_separate_focus(I_to_W_separ=True  , I_to_W_focus=True, W_to_C_separ=True , W_to_C_focus=True).set_gen_op( use_gen_op_p20 ).set_train_step( use_train_step_p20 )
+I_w_M_to_W_ch016_pyr_1s5_2s4_3s3_wiDiv__W_w_M_to_C_ch016_pyr_1s6_2s6_wiDiv = KModel_builder().set_model_name(MODEL_NAME.multi_flow_unet).set_multi_model_builders(op_type="I_to_Wxyz_to_Cxy_general", I_to_Wx_Wy_Wz=I_w_M_to_W_ch016_pyramid_1side_5__2side_4__3side_3__wiDiv, W_to_Cx_Cy=W_w_M_to_C_ch016_pyramid_1side_6__2side_6_wiDiv).set_multi_model_separate_focus(I_to_W_separ=True  , I_to_W_focus=True, W_to_C_separ=True , W_to_C_focus=True).set_gen_op( use_gen_op_p20 ).set_train_step( use_train_step_p20 )
+###############################################################################################################################################################################################
+if(__name__ == "__main__"):
+    import numpy as np
+
+    print("build_model cost time:", time.time() - start_time)
+    data = np.zeros(shape=(1, 255, 255, 1))
+    use_model = W_w_M_to_C_ch016_pyramid_1side_6__2side_5_wiDiv
+    use_model = use_model.build()
+    result = use_model.generator(data)
+    print(result[0].shape)
+    print(result[1].shape)
+
+    from kong_util.tf_model_util import Show_model_weights
+    Show_model_weights(use_model.generator)
+    use_model.generator.summary()
+    print(use_model.model_describe)
+
+    import tensorflow as tf
+    import datetime
+    code_exe_dir = "\\".join(code_exe_path_element[:-1])
+    log_dir = f"{code_exe_dir}/use_Tensorboard_see_Graph/{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
+    tboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+
+    img_inputs = tf.keras.Input(shape=(255, 255, 1))
+    use_model.generator(img_inputs)
+    use_model.generator.compile(optimizer='adam', loss='mae', metrics=['accuracy'])
+    use_model.generator.fit    (data, data, epochs=1, callbacks=[tboard_callback])
+    print(f"tensorboard --logdir={log_dir}")
