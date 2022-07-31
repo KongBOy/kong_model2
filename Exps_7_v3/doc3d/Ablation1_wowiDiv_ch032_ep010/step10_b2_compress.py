54196bf4275acbd1d839a5b91821206b1a58f550
KongBoy <s89334roy@yahoo.com.tw>
2022-07-31 11:01:44 +0800

    加入 Ablation1 ch032

---
+++ Exps_7_v3/doc3d/Ablation1_wowiDiv_ch032_ep010/step10_b2_compress.py
@@ -0,0 +1,60 @@
+#############################################################################################################################################################################################################
+#############################################################################################################################################################################################################
+### 把 kong_model2 加入 sys.path
+import os
+code_exe_path = os.path.realpath(__file__)                   ### 目前執行 step10_b.py 的 path
+code_exe_path_element = code_exe_path.split("\\")            ### 把 path 切分 等等 要找出 kong_model 在第幾層
+kong_layer = code_exe_path_element.index("kong_model2")      ### 找出 kong_model2 在第幾層
+kong_model2_dir = "\\".join(code_exe_path_element[:kong_layer + 1])  ### 定位出 kong_model2 的 dir
+import sys                                                   ### 把 kong_model2 加入 sys.path
+sys.path.append(kong_model2_dir)
+# print(__file__.split("\\")[-1])
+# print("    code_exe_path:", code_exe_path)
+# print("    code_exe_path_element:", code_exe_path_element)
+# print("    kong_layer:", kong_layer)
+# print("    kong_model2_dir:", kong_model2_dir)
+###############################################################################################################################################################################################################
+# 按F5執行時， 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～ 才可 import step10_a.py 喔！
+code_exe_dir = os.path.dirname(code_exe_path)   ### 目前執行 step10_b.py 的 dir
+if(os.getcwd() != code_exe_dir):                ### 如果 不是在 step10_b.py 的資料夾， 自動幫你切過去～
+    os.chdir(code_exe_dir)
+# print("current_path:", os.getcwd())
+###############################################################################################################################################################################################################
+### 所有 指令 統一寫這邊
+from step10_c_exp_command import *
+######################################################################################################################
+import subprocess as sb
+
+
+### I_w_M_to_C 3UNet/wiDiv/woDiv
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__ch032_L5_2blk__3UNet__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__ch032_L5_2blk__wiDiv__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__ch032_L5_2blk__woD_L__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__ch032_L5_2blk__woD_M__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+### W_w_M_to_C 3UNet/wiDiv/woDiv
+# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch032_L5_2blk__3UNet__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch032_L5_2blk__wiDiv__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch032_L5_2blk__woD_L__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch032_L5_2blk__woD_M__Mae_s001_Sob_k09_s001                        .{compress_all}"])
+
+##### 一起訓練
+### 3UNet
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_3UNet__W_w_M_to_C_3UNet__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_3UNet__W_w_M_to_C_wiDiv__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_3UNet__W_w_M_to_C_woD_L__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_3UNet__W_w_M_to_C_woD_M__ep010 .{compress_all}"])
+### wiDiv
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_wiDiv__W_w_M_to_C_3UNet__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_wiDiv__W_w_M_to_C_wiDiv__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_wiDiv__W_w_M_to_C_woD_L__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_wiDiv__W_w_M_to_C_woD_M__ep010 .{compress_all}"])
+### woD_L
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_L__W_w_M_to_C_3UNet__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_L__W_w_M_to_C_wiDiv__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_L__W_w_M_to_C_woD_L__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_L__W_w_M_to_C_woD_M__ep010 .{compress_all}"])
+### woD_M
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_M__W_w_M_to_C_3UNet__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_M__W_w_M_to_C_wiDiv__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_M__W_w_M_to_C_woD_L__ep010 .{compress_all}"])
+# sb.run(cmd_python_step10_a + [f"I_w_M_to_W_woD_M__W_w_M_to_C_woD_M__ep010 .{compress_all}"])
