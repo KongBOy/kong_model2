6f7c931dc839896c18385d3c8b41805883426409
KongBoy <s89334roy@yahoo.com.tw>
2022-08-11 19:08:44 +0800

    Ablation3_ch016 加入 L6

--- Exps_7_v3/doc3d/Ablation3_Pyr_ch016_ep010_noFocus/step10_b1_train.py
+++ Exps_7_v3/doc3d/Ablation3_Pyr_ch016_ep010_noFocus/step10_b1_train.py
@@ -27,15 +27,21 @@ import subprocess as sb
 
 
 ### I_w_M_to_C 3UNet/wiDiv/woDiv %& FL/FM/NL/NM
-# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__ch016_L5__woD_L__Full_Less                        .{train}"])
-# sb.run(cmd_python_step10_a + [f"I_w_M_to_W__ch016_L5__woD_L__Full_Less_in_have_bg             .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W__ch016_L5__woD_L__Full_Less                        .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W__ch016_L5__woD_L__Full_Less_in_have_bg             .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W__ch016_L6__woD_L__Full_Less                        .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W__ch016_L6__woD_L__Full_Less_in_have_bg             .{train}"])
 
 ### W_w_M_to_C woDiv
-# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch016_L5__woD_L__Full_Less                        .{train}"])
-# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch016_L5__woD_L__Full_Less_in_have_bg             .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L5__woD_L__Full_Less                        .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L5__woD_L__Full_Less_in_have_bg             .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L6__woD_L__Full_Less                        .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L6__woD_L__Full_Less_in_have_bg             .{train}"])
 
 ##### 一起訓練
 ### 4. woD_L woD_L(記得 woD_L 的 seperate 要設 False)，第二個測這個
 # 這個是我意想不到竟然做得更好的結果， 我想看看他可以做得多好
-# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less            .{train}"])
-# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less_in_have_bg .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_L5_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less            .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_L5_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less_in_have_bg .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_L6_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less            .{train}"])
+# sb.run(cmd_python_step10_a + [f"exp_L6_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less_in_have_bg .{train}"])
--- Exps_7_v3/doc3d/Ablation3_Pyr_ch016_ep010_noFocus/step10_b2_post.py
+++ Exps_7_v3/doc3d/Ablation3_Pyr_ch016_ep010_noFocus/step10_b2_post.py
@@ -27,11 +27,15 @@ import subprocess as sb
 
 ### I_w_M_to_C woDiv 不需要看 LD/SSIM， 因為沒有rec
 ### W_w_M_to_C woDiv
-# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch016_L5__woD_L__Full_Less            .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
-# sb.run(cmd_python_step10_a + [f"W_w_M_to_C__ch016_L5__woD_L__Full_Less_in_have_bg .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L5__woD_L__Full_Less            .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L5__woD_L__Full_Less_in_have_bg .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L6__woD_L__Full_Less            .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_W_w_M_to_C__ch016_L6__woD_L__Full_Less_in_have_bg .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
 
 ##### 一起訓練
 ### 4. woD_L woD_L(記得 woD_L 的 seperate 要設 False)，第二個測這個
 # 這個是我意想不到竟然做得更好的結果， 我想看看他可以做得多好
-# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less            .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
-# sb.run(cmd_python_step10_a + [f"exp_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less_in_have_bg .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_L5_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less            .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_L5_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less_in_have_bg .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_L6_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less            .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
+# sb.run(cmd_python_step10_a + [f"exp_L6_I_w_M_to_W_ch016_woD_L_Full_Less__W_w_M_to_C_ch016_woD_L_Full_Less_in_have_bg .{test_Calculate_SSIM_LD_all_from_begin % 'test_Kong_Crop_p60_gt_DewarpNet_p60_then_Use_KModel5_FBA'}"])
