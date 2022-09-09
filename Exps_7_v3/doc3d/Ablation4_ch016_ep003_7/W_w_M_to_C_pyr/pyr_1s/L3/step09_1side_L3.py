1900a10cd7c23d4b99146255a79d8b66aa55c51e
KongBoy <s89334roy@yahoo.com.tw>
2022-09-09 16:06:35 +0800

    加入 Ablation4 ch016 ep003_7

---
+++ Exps_7_v3/doc3d/Ablation4_ch016_ep003_7/W_w_M_to_C_pyr/pyr_1s/L3/step09_1side_L3.py
@@ -0,0 +1,42 @@
+#############################################################################################################################################################################################################
+from step08_b_use_G_generate_W_w_M_to_Cx_Cy_combine import W_w_M_to_Cx_Cy
+from step08_b_use_G_generate_0_util import Tight_crop, Color_jit
+from step09_c_train_step import Train_step_W_w_M_to_Cx_Cy
+from step09_d_KModel_builder_combine_step789 import KModel_builder, MODEL_NAME
+
+color_jit = Color_jit(do_ratio=0.6)
+use_what_gen_op     =            W_w_M_to_Cx_Cy(  separate_out=False, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale=  0) )
+use_what_train_step = Train_step_W_w_M_to_Cx_Cy(  separate_out=False, focus=True, tight_crop=Tight_crop(pad_size=20, resize=(255, 255), jit_scale= 15) )
+
+import time
+start_time = time.time()
+###############################################################################################################################################################################################
+###############################################################################################################################################################################################
+########################################################### Block1
+### Block1
+#########################################################################################
+pyramid_1side_1 = [1, 0, 0, 0, 0, 0, 1]
+pyramid_1side_2 = [1, 1, 0, 0, 0, 1, 1]
+pyramid_1side_3 = [1, 1, 1, 0, 1, 1, 1]
+pyramid_1side_4 = [1, 1, 1, 1, 1, 1, 1]
+#########################################################################################
+ch032_pyramid_1side_1 = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch= 16, depth_level=3, out_ch=2, unet_acti="sigmoid", conv_block_num=pyramid_1side_1, ch_upper_bound= 2 ** 14).set_gen_op( use_what_gen_op ).set_train_step( use_what_train_step )
+ch032_pyramid_1side_2 = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch= 16, depth_level=3, out_ch=2, unet_acti="sigmoid", conv_block_num=pyramid_1side_2, ch_upper_bound= 2 ** 14).set_gen_op( use_what_gen_op ).set_train_step( use_what_train_step )
+ch032_pyramid_1side_3 = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch= 16, depth_level=3, out_ch=2, unet_acti="sigmoid", conv_block_num=pyramid_1side_3, ch_upper_bound= 2 ** 14).set_gen_op( use_what_gen_op ).set_train_step( use_what_train_step )
+ch032_pyramid_1side_4 = KModel_builder().set_model_name(MODEL_NAME.flow_unet2).set_unet3(out_conv_block=True, concat_before_down=True, kernel_size=3, padding="valid", hid_ch= 16, depth_level=3, out_ch=2, unet_acti="sigmoid", conv_block_num=pyramid_1side_4, ch_upper_bound= 2 ** 14).set_gen_op( use_what_gen_op ).set_train_step( use_what_train_step )
+#########################################################################################
+###############################################################################################################################################################################################
+
+if(__name__ == "__main__"):
+    import numpy as np
+
+    print("build_model cost time:", time.time() - start_time)
+    data = np.zeros(shape=(1, 512, 512, 1))
+    use_model = ch032_pyramid_1side_4
+    use_model = use_model.build()
+    result = use_model.generator(data)
+    print(result.shape)
+
+    from kong_util.tf_model_util import Show_model_weights
+    Show_model_weights(use_model.generator)
+    use_model.generator.summary()
